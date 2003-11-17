82a56bc61f09b593b41e833c3609c88b72a3ef25
Jonathan E. Guyer <guyer@nist.gov>
2003-11-17 16:18:19 +0000

    Cleaned up headers.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@11 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/boundaryConditions/fixedValue.py
+++ fipy/boundaryConditions/fixedValue.py
@@ -1,23 +1,41 @@
-"""
--*-Pyth-*-
+"""-*-Pyth-*-
 ###################################################################
- Alpha - Core code development for Alpha
+ PFM - Python-based phase field solver
 
  FILE: "fixedValue.py"
-                                   created: 11/13/03 {11:37:00 AM} 
-                               last update: 11/14/03 {5:07:34 PM} 
+                                   created: 11/15/03 {9:47:59 PM} 
+                               last update: 11/17/03 {11:05:24 AM} 
  Author: Jonathan Guyer
- E-mail: jguyer@his.com
-   mail: Alpha Cabal
-    www: http://alphatcl.sourceforge.net
+ E-mail: guyer@nist.gov
+ Author: Daniel Wheeler
+ E-mail: daniel.wheeler@nist.gov
+   mail: NIST
+    www: http://ctcms.nist.gov
  
-Copyright (c) 2003  Jonathan Guyer
+========================================================================
+This software was developed at the National Institute of Standards
+and Technology by employees of the Federal Government in the course
+of their official duties.  Pursuant to title 17 Section 105 of the
+United States Code this software is not subject to copyright
+protection and is in the public domain.  PFM is an experimental
+system.  NIST assumes no responsibility whatsoever for its use by
+other parties, and makes no guarantees, expressed or implied, about
+its quality, reliability, or any other characteristic.  We would
+appreciate acknowledgement if the software is used.
 
-See the file "license.terms" for information on usage and redistribution
-of this file, and for a DISCLAIMER OF ALL WARRANTIES.
- See the file "license.terms" for information on usage and  redistribution
- of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+This software can be redistributed and/or modified freely
+provided that any derivative works bear some notice that they are
+derived from it, and any modified versions bear some notice that
+they have been modified.
+========================================================================
  
+ Description: 
+
+ History
+
+ modified   by  rev reason
+ ---------- --- --- -----------
+ 2003-11-15 JEG 1.0 original
 ###################################################################
 """
 
--- fipy/meshes/cell.py
+++ fipy/meshes/cell.py
@@ -1,38 +1,43 @@
+"""-*-Pyth-*-
+###################################################################
+ PFM - Python-based phase field solver
+
+ FILE: "cell.py"
+                                   created: 11/10/03 {3:23:11 PM} 
+                               last update: 11/17/03 {11:11:02 AM} 
+ Author: Jonathan Guyer
+ E-mail: guyer@nist.gov
+ Author: Daniel Wheeler
+ E-mail: daniel.wheeler@nist.gov
+   mail: NIST
+    www: http://ctcms.nist.gov
+ 
+========================================================================
+This software was developed at the National Institute of Standards
+and Technology by employees of the Federal Government in the course
+of their official duties.  Pursuant to title 17 Section 105 of the
+United States Code this software is not subject to copyright
+protection and is in the public domain.  PFM is an experimental
+system.  NIST assumes no responsibility whatsoever for its use by
+other parties, and makes no guarantees, expressed or implied, about
+its quality, reliability, or any other characteristic.  We would
+appreciate acknowledgement if the software is used.
+
+This software can be redistributed and/or modified freely
+provided that any derivative works bear some notice that they are
+derived from it, and any modified versions bear some notice that
+they have been modified.
+========================================================================
+ 
+ Description: 
+
+ History
+
+ modified   by  rev reason
+ ---------- --- --- -----------
+ 2003-11-10 JEG 1.0 original
+###################################################################
 """
-#-----*-Pyth-*-
-####################################################################
-# PFM - Python-based phase field solver
-#
-# FILE: "cell.py"
-#                                   created: 11/10/03 {3:23:11 PM} 
-#                               last update: 11/10/03 {4:52:22 PM} 
-# Author: Jonathan Guyer
-# E-mail: guyer@nist.gov
-#   mail: NIST
-#    www: http://ctcms.nist.gov/
-# 
-#========================================================================
-#This software was developed at the National Institute of Standards
-#and Technology by employees of the Federal Government in the course
-#of their official duties.  Pursuant to title 17 Section 105 of the
-#United States Code this software is not subject to copyright
-#protection and is in the public domain.  PFM is an experimental
-#system.  NIST assumes no responsibility whatsoever for its use by
-#other parties, and makes no guarantees, expressed or implied, about
-#its quality, reliability, or any other characteristic.  We would
-#appreciate acknowledgement if the software is used.
-#
-#This software can be redistributed and/or modified freely
-#provided that any derivative works bear some notice that they are
-#derived from it, and any modified versions bear some notice that
-#they have been modified.
-#========================================================================
-# See the file "license.terms" for information on usage and  redistribution
-# of this file, and for a DISCLAIMER OF ALL WARRANTIES.
-# 
-####################################################################
-#----
-"""	
 
 class Cell:
     def __init__(self, faces, id):
