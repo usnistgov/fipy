3732c0b9875851d976f147d69d58285e13ec8a8b
Daniel Wheeler <daniel.wheeler@nist.gov>
2003-12-07 01:48:40 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@79 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/models/levelSet/levelSetEquation.py
@@ -0,0 +1,129 @@
+"""
+-*-Pyth-*-
+###################################################################
+ PFM - Python-based phase field solver
+
+ FILE: "levelSetEquation.py"
+                                   created: 11/12/03 {10:39:23 AM} 
+                               last update: 12/3/03 {4:39:19 PM} 
+ Author: Jonathan Guyer
+ E-mail: guyer@nist.gov
+ Author: Daniel Wheeler
+ E-mail: daniel.wheeler@nist.gov
+   mail: NIST
+    www: http://ctcms.nist.gov
+ 
+========================================================================
+This software was developed at the National Institute of Standards
+and Technology by employees of the Federal Government in the course
+of their official duties.  Pursuant to title 17 Section 105 of the
+United States Code this software is not subject to copyright
+protection and is in the public domain.  PFM is an experimental
+system.  NIST assumes no responsibility whatsoever for its use by
+other parties, and makes no guarantees, expressed or implied, about
+its quality, reliability, or any other characteristic.  We would
+appreciate acknowledgement if the software is used.
+
+This software can be redistributed and/or modified freely
+provided that any derivative works bear some notice that they are
+derived from it, and any modified versions bear some notice that
+they have been modified.
+========================================================================
+ 
+ Description: 
+
+ History
+
+ modified   by  rev reason
+ ---------- --- --- -----------
+ 2003-11-12 JEG 1.0 original
+###################################################################
+"""
+
+from equations.Equation import Equation
+import Numeric
+
+class LevelSetEquation(Equation):
+    """
+    Level set equation is implicit.
+    """    
+    def __init__(self,
+                 var,
+                 solver = 'default_solver',
+                 boundaryConditions = (),
+                 parameters = {}):
+        
+        mesh = var.getMesh()
+	
+	terms = ()
+
+	Equation.__init__(
+            self,
+            var,
+            terms,
+            solver)
+
+    def solve(self):
+        ## keep the old values
+        cells = self.mesh.getCells()
+
+        ## find all the cells with neighbours of opposite sign
+        zeroCells = self.getZeroCells(cells)
+
+        ## set the interface cells to have a value.
+        self.setZeroValues(zeroCells)
+
+        ## find bounding cells to setCells
+        boundingCells = self.getBoundingCells(zeroCells)
+
+        while(len(boundingCells) != 0):
+
+        ## get MinimumCell
+            minimumCell = self.getMinimumCell(boundingCells)
+
+        ## set value of minimum cell
+            self.setCellValue(minimumCell)
+
+        ## add the extra cell
+            boundingCells, zeroCells = self.updateBoundingCells(minimumCell, boundingCells, zeroCells)
+
+    def getZeroCells(self, cells):
+        zeroCells = ()
+        array = var.getArray()
+        for cell in cells:
+            id = cell.getId()
+            zeroCell = ()
+            for adjacentCell in cell.getAdjacentCells():                
+                adjacentId = adjacentCell.getId()
+                if array(array[id] * array[adjacentId]<0.):
+                    zeroCell = (cell,)
+             zeroCells += zeroCell            
+        return zeroCells
+
+    def setZeroCellValues(self, zeroCells):
+        varOld = var.getOld()
+        for cell in zeroCells:
+            minCell1 = cell.getMinimumCell(cell.getAdjacentCells())
+            minCell2 = cell.getMinimumCell(cell.getAdjacentCells()-minCell1)
+            value = varOld.getValue(cell)
+            gradx,grady = cell.getGradient(minCell1, minCell2)
+            value = value / Numeric.sqrt(gradx * gradx, grady * grady)
+            var.setValue(value, cell)
+
+    def getBoundingCells(self, zeroCells):
+        boundingCells = ()
+        for cell in cells:
+            boundingCell = ()
+            if cell !in zeroCells:
+                for adjacentCell in cell.getAdjacentCells():
+                    if adjacentCell in zeroCells:
+                        boundingCell = (cell,)
+            boundingCells += boundingCell
+
+    def getMinimumCell
+            
+
+            
+                    
+                
+    
