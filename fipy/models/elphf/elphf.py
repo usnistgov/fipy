3a0b87d4ea9929f70efdf68c8faa3f1dbae7b40f
Jonathan E. Guyer <guyer@nist.gov>
2003-12-16 16:18:10 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@95 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/models/elphf/elphf.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+## -*-Pyth-*-
+ # ###################################################################
+ #  PFM - Python-based phase field solver
+ # 
+ #  FILE: "elphf.py"
+ #                                    created: 12/12/03 {10:41:56 PM} 
+ #                                last update: 12/16/03 {10:37:05 AM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  See the file "license.terms" for information on usage and  redistribution
+ #  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ #  
+ # ###################################################################
+ ##
+
+from concentrationEquation import ConcentrationEquation
+from solvers.linearCGSSolver import LinearCGSSolver
+from solvers.linearLUSolver import LinearLUSolver
+from solvers.linearGMRESSolver import LinearGMRESSolver
+from boundaryConditions.fixedValue import FixedValue
+from boundaryConditions.fixedFlux import FixedFlux
+from iterators.iterator import Iterator
+
+def makeIterator(mesh, parameters, maxSweeps = 1):
+    equations = ()
+    for component in parameters['substitutionals']:
+	eq = ConcentrationEquation(
+	    var = component,
+	    diffusivity = parameters['diffusivity'],
+	    solver = LinearLUSolver(),
+# 	    solver = LinearGMRESSolver(
+# 		 tolerance = 1.e-15, 
+# 		 steps = 1000
+# 	    ),
+# 	   solver = LinearCGSSolver(
+# 		tolerance = 1.e-15, 
+# 		steps = 1000
+# 	   ),
+	    boundaryConditions=(
+# 		FixedValue(faces = mesh.getFacesLeft(),value = parameters['valueLeft']),
+# 		FixedValue(faces = mesh.getFacesRight(),value = parameters['valueRight']),
+		FixedFlux(faces = mesh.getFacesLeft(),value = 0.),
+		FixedFlux(faces = mesh.getFacesRight(),value = 0.),
+		FixedFlux(faces = mesh.getFacesTop(),value = 0.),
+		FixedFlux(faces = mesh.getFacesBottom(),value = 0.)
+	    ),
+	    parameters = parameters
+	)
+	equations += (eq,)
+	
+    return Iterator(equations = equations, maxSweeps = maxSweeps)
+
+
