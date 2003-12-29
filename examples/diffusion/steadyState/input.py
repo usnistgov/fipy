5e29ea6421e3e8fdfd60d36bcc71489d2840b178
Daniel Wheeler <daniel.wheeler@nist.gov>
2003-12-29 21:11:04 +0000

    added new directory for steady state diffusion test cases
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@134 d80e17d7-ff13-0410-a124-85740d801063

---
+++ examples/diffusion/steadyState/input.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  PFM - Python-based phase field solver
+ # 
+ #  FILE: "input.py"
+ #                                    created: 12/29/03 {3:23:47 PM}
+ #                                last update: 12/29/03 {11:10:21 AM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+from meshes.grid2D import Grid2D
+from equations.diffusionEquation import DiffusionEquation
+from solvers.linearPCGSolver import LinearPCGSolver
+from boundaryConditions.fixedValue import FixedValue
+from boundaryConditions.fixedFlux import FixedFlux
+from iterators.iterator import Iterator
+from variables.cellVariable import CellVariable
+from viewers.grid2DGistViewer import Grid2DGistViewer
+
+def getParameters(nx ,ny):
+
+    valueLeft = 0.
+    valueRight = 1.
+
+    mesh = Grid2D(1.,1.,nx,ny)
+
+    var = CellVariable(
+        name = "concentration",
+        mesh = mesh,
+        value = valueLeft)
+
+    eq = DiffusionEquation(
+        var,
+        transientCoeff = 0., 
+        diffusionCoeff = 1.,
+        solver = LinearPCGSolver(
+        tolerance = 1.e-15, 
+        steps = 1000
+        ),
+        boundaryConditions=(
+        FixedValue(mesh.getFacesLeft(),valueLeft),
+        FixedValue(mesh.getFacesRight(),valueRight),
+        FixedFlux(mesh.getFacesTop(),0.),
+        FixedFlux(mesh.getFacesBottom(),0.)
+        )
+        )
+
+    it = Iterator((eq,))
+
+    parameters = {
+        'valueLeft' : valueLeft,
+        'valueRight' : valueRight,
+        'timeStep' : 1.,
+        'tolerance' : 1e-8,
+        'steps' : 1,
+        'mesh' : mesh,
+        'var' : var,
+        'eq' : eq,
+        'it' : it
+        }
+        
+    return parameters
+
+if __name__ == '__main__':
+    parameters = getParameters(20,20)
+    it = parameters['it']
+    var = parameters['var']
+    viewer = Grid2DGistViewer(var)
+    it.timestep(1)
+    viewer.plot()
+    raw_input()
---
+++ examples/diffusion/steadyState/test.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  PFM - Python-based phase field solver
+ # 
+ #  FILE: "test.py"
+ #                                    created: 12/29/03 {3:23:47 PM}
+ #                                last update: 12/29/03 {11:10:21 AM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+"""Test steady-state diffusion solutions
+"""
+ 
+import unittest
+from tests.testBase import TestBase
+from input import getParameters
+
+class Test(TestBase):
+    """Generic steady-state diffusion class
+    
+    	Constructs a mesh, variable, equation, and iterator based
+	on the mesh dimensions specified by the child class
+    """
+    def setUp(self):
+        parameters = getParameters(self.nx, self.ny)
+        
+	self.steps = parameters['steps']
+	self.timeStep = parameters['timeStep']
+	self.tolerance = parameters['tolerance']
+        self.valueLeft = parameters['valueLeft']
+        self.valueRight = parameters['valueRight']
+        self.mesh = parameters['mesh']
+        self.var = parameters['var']
+        self.eq = parameters['eq']
+        self.it = parameters['it']
+
+    def getTestValues(self):
+	(lx,ly) = self.mesh.getPhysicalShape()
+	vl = self.valueLeft
+	vr = self.valueRight
+	x = self.mesh.getCellCenters()[:,0]
+	return vl + (vr - vl) * x / lx
+	
+class Test20x20(Test):
+    """Steady-state 1D diffusion on a 20x20 mesh
+    """
+    def setUp(self):
+	self.nx = 20
+	self.ny = 20
+	Test.setUp(self)	    
+	
+class Test50x50(Test):
+    """Steady-state 1D diffusion on a 50x50 mesh
+    """
+    def setUp(self):
+	self.nx = 50
+	self.ny = 50
+	Test.setUp(self)	    
+
+class  Test1D(Test):
+    """Steady-state 1D diffusion on a 50x1 mesh
+    """
+    def setUp(self):
+	self.nx = 50
+	self.ny = 1
+	Test.setUp(self)
+
+def suite():
+    theSuite = unittest.TestSuite()
+    theSuite.addTest(unittest.makeSuite(Test1D))
+    theSuite.addTest(unittest.makeSuite(Test20x20))
+    theSuite.addTest(unittest.makeSuite(Test50x50))
+    return theSuite
+    
+if __name__ == '__main__':
+    theSuite = suite()
+    unittest.TextTestRunner(verbosity=2).run(theSuite)
+
+            
+            
