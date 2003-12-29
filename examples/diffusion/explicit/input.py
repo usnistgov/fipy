6f3bc8a695164fc0943dfa09aeb0eb4d6b809bff
Daniel Wheeler <daniel.wheeler@nist.gov>
2003-12-29 21:31:55 +0000

    Added files for explicit diffusion test case.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@139 d80e17d7-ff13-0410-a124-85740d801063

---
+++ examples/diffusion/explicit/input.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  PFM - Python-based phase field solver
+ # 
+ #  FILE: "input.py"
+ #                                    created: 12/29/03 {3:23:47 PM}
+ #                                last update: 12/29/03 {11:10:21 AM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+from meshes.grid2D import Grid2D
+from equations.diffusionEquation import DiffusionEquation
+from solvers.linearPCGSolver import LinearPCGSolver
+from boundaryConditions.fixedValue import FixedValue
+from boundaryConditions.fixedFlux import FixedFlux
+from iterators.iterator import Iterator
+from variables.cellVariable import CellVariable
+from viewers.grid2DGistViewer import Grid2DGistViewer
+
+def getParameters(nx ,ny):
+
+    valueLeft = 0.
+    valueRight = 1.
+    timeStep = 0.2
+
+    mesh = Grid2D(1.,1.,nx,ny)
+
+    var = CellVariable(
+        name = "concentration",
+        mesh = mesh,
+        value = valueLeft)
+
+    eq = DiffusionEquation(
+        var,
+        transientCoeff = 1. / timeStep, 
+        diffusionCoeff = 1.,
+        solver = LinearPCGSolver(
+        tolerance = 1.e-15, 
+        steps = 1000
+        ),
+        boundaryConditions=(
+        FixedValue(mesh.getFacesLeft(),valueLeft),
+        FixedValue(mesh.getFacesRight(),valueRight),
+        FixedFlux(mesh.getFacesTop(),0.),
+        FixedFlux(mesh.getFacesBottom(),0.)
+        )
+        )
+
+    it = Iterator((eq,))
+
+    parameters = {
+        'valueLeft' : valueLeft,
+        'valueRight' : valueRight,
+        'timeStep' : timeStep,
+        'tolerance' : 1e-3,
+        'steps' : 10000,
+        'mesh' : mesh,
+        'var' : var,
+        'eq' : eq,
+        'it' : it
+        }
+        
+    return parameters
+
+if __name__ == '__main__':
+    parameters = getParameters(10,1)
+    it = parameters['it']
+    var = parameters['var']
+    steps = parameters['steps']
+    viewer = Grid2DGistViewer(var)
+    it.timestep(steps)
+    viewer.plot()
+    raw_input()
+
