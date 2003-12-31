16b897ee0d9f9d7ff843db2e33cb9a5fb315c840
Daniel Wheeler <daniel.wheeler@nist.gov>
2003-12-31 21:11:07 +0000

    Added a new anisotropy test case.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@163 d80e17d7-ff13-0410-a124-85740d801063

---
+++ examples/phase/anisotropy/test.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  PFM - Python-based phase field solver
+ # 
+ #  FILE: "testSteadyStateDiffusion.py"
+ #                                    created: 11/10/03 {3:23:47 PM}
+ #                                last update: 12/24/03 {10:18:38 AM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+"""Test steady-state diffusion solutions
+"""
+ 
+import unittest
+import os
+import cPickle
+from tests.testBase import TestBase
+from inputTemperature import AnisotropySystem
+import examples.phase.examples.anisotropy
+
+class TestAnisotropy(TestBase):
+    def setUp(self):
+        self.system = AnisotropySystem()
+        self.testFile = 'testAnisotropy.gz'
+        parameters = self.system.getParameters()
+
+	self.steps = parameters['steps']
+	self.tolerance = 1e-10
+
+        self.it = parameters['it']
+        self.var = parameters['var']
+        
+    def getTestValues(self):
+	filestream=os.popen('gunzip --fast -c < %s/%s'%(examples.phase.examples.anisotropy.__path__[0],self.testFile),'r')
+	
+	testData = cPickle.load(filestream)
+	filestream.close()
+
+	return testData
+
+def suite():
+    theSuite = unittest.TestSuite()
+    theSuite.addTest(unittest.makeSuite(TestAnisotropy))
+    return theSuite
+    
+if __name__ == '__main__':
+    theSuite = suite()
+    unittest.TextTestRunner(verbosity=2).run(theSuite)
+
+            
+            
