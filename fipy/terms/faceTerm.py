2e3020835995833dec132793bb6ef3868ad21b5e
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-01-05 19:33:57 +0000

    No changes
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@171 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/terms/faceTerm.py
+++ fipy/terms/faceTerm.py
@@ -5,7 +5,7 @@
  # 
  #  FILE: "faceTerm.py"
  #                                    created: 11/17/03 {10:29:10 AM} 
- #                                last update:  01/05/04 { 2:28:39 PM}
+ #                                last update:  01/05/04 { 2:33:46 PM}
  #  Author: Jonathan Guyer
  #  E-mail: guyer@nist.gov
  #  Author: Daniel Wheeler
@@ -19,7 +19,7 @@
  # of their official duties.  Pursuant to title 17 Section 105 of the
  # United States Code this software is not subject to copyright
  # protection and is in the public domain.  PFM is an experimental
- # system.  NIST assumes no responsibility whatsoever for its use by
+ # system.  NIST assumes no responsibility whatsever for its use by
  # other parties, and makes no guarantees, expressed or implied, about
  # its quality, reliability, or any other characteristic.  We would
  # appreciate acknowledgement if the software is used.
