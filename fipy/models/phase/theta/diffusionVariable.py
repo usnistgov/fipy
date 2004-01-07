8d5daf48ad04c01231d30189fd39ec559452c4a7
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-01-07 21:33:44 +0000

    Created the theta equation.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@184 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/models/phase/theta/diffusionVariable.py
@@ -0,0 +1,68 @@
+"""
+-*-Pyth-*-
+###################################################################
+ PFM - Python-based phase field solver
+
+ FILE: "diffusionVariable.py"
+                                   created: 11/12/03 {10:39:23 AM} 
+                               last update: 01/07/04 {11:57:57 AM}
+ Author: Jonathan Guyer
+ E-mail: guyer@nist.gov
+ Author: Daniel Wheeler
+ E-mail: daniel.wheeler@nist.gov
+   mail: NIST
+    www: http://ctcms.nist.gov
+ 
+========================================================================
+This software was developed at the National Institute of Standards
+and Technology by employees of the Federal Government in the course
+of their official duties.  Pursuant to title 17 Section 105 of the
+United States Code this software is not subject to copyright
+protection and is in the public domain.  PFM is an experimental
+system.  NIST assumes no responsibility whatsoever for its use by
+other parties, and makes no guarantees, expressed or implied, about
+its quality, reliability, or any other characteristic.  We would
+appreciate acknowledgement if the software is used.
+
+This software can be redistributed and/or modified freely
+provided that any derivative works bear some notice that they are
+derived from it, and any modified versions bear some notice that
+they have been modified.
+========================================================================
+ 
+ Description: 
+
+ History
+
+ modified   by  rev reason
+ ---------- --- --- -----------
+ 2003-11-12 JEG 1.0 original
+###################################################################
+"""
+
+from variables.faceVariable import FaceVariable
+
+class TransientVariable(CellVariable):
+
+    def __init__(self, phase = None, theta = None, parameters = None):
+
+        CellVariable.__init__(self, phase.getMesh())
+
+        self.parameters = parameters
+        self.phase = self.required(phase)
+        self.theta = self.required(theta)
+
+    def calcValue(self):
+
+        gamma = self.parameters['gamma']
+
+        
+        phaseFace = self.phase.getFaceValue()[:]
+        phaseSq = phaseFace * phaseFace
+        gradMag = self.theta.getFaceGradMag()[:]
+
+        IGamma = Numeric.where(gradMag > 1. / gamma, 1 / gradMag, gamma)
+
+        self.value = phaseSq * (self.parameters['s'] * IGamma + self.parameters['epsilon']**2)
+    
+
---
+++ fipy/models/phase/theta/transientVariable.py
@@ -0,0 +1,67 @@
+"""
+-*-Pyth-*-
+###################################################################
+ PFM - Python-based phase field solver
+
+ FILE: "transientVariable.py"
+                                   created: 11/12/03 {10:39:23 AM} 
+                               last update: 01/07/04 {11:27:37 AM}
+ Author: Jonathan Guyer
+ E-mail: guyer@nist.gov
+ Author: Daniel Wheeler
+ E-mail: daniel.wheeler@nist.gov
+   mail: NIST
+    www: http://ctcms.nist.gov
+ 
+========================================================================
+This software was developed at the National Institute of Standards
+and Technology by employees of the Federal Government in the course
+of their official duties.  Pursuant to title 17 Section 105 of the
+United States Code this software is not subject to copyright
+protection and is in the public domain.  PFM is an experimental
+system.  NIST assumes no responsibility whatsoever for its use by
+other parties, and makes no guarantees, expressed or implied, about
+its quality, reliability, or any other characteristic.  We would
+appreciate acknowledgement if the software is used.
+
+This software can be redistributed and/or modified freely
+provided that any derivative works bear some notice that they are
+derived from it, and any modified versions bear some notice that
+they have been modified.
+========================================================================
+ 
+ Description: 
+
+ History
+
+ modified   by  rev reason
+ ---------- --- --- -----------
+ 2003-11-12 JEG 1.0 original
+###################################################################
+"""
+
+from variables.cellVariable import CellVariable
+
+class TransientVariable(CellVariable):
+
+    def __init__(self, phase = None, theta = None, parameters = None):
+
+        CellVariable.__init__(self, phase.getMesh())
+
+        self.parameters = parameters
+        self.phase = self.required(phase)
+        self.theta = self.required(theta)
+
+    def calcValue(self):
+
+        smallValue = self.parameters['small Value']
+        epsion = self.parameters['epsilon']
+        
+        phaseMod = self.phase[:] + ( self.phase[:] < smallValue ) * smallValue
+        phaseSq = phaseMod * phaseMod
+        expo = - epsilon * self.parameters['beta'] * self.var.getGradMag()[:]
+        pFunc = 1. + Numerix.exp(expo) * (self.parameters['mu'] / epsilon - 1.)
+
+        self.value = self.parameters['tau'] * phaseSq * pFunc / self.parameters['timeStepDuration'] 
+    
+
