5ed77bd6033738f0d35ad9aafcf361673c8bc662
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-01-20 18:15:08 +0000

    removed a print statement
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@208 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/terms/powerLawConvectionTerm.py
+++ fipy/terms/powerLawConvectionTerm.py
@@ -50,8 +50,6 @@ class PowerLawConvectionTerm(ConvectionTerm):
 	    eps = 1e-3
 	    P  = self.P.getNumericValue()
 
-            print "P: ", type(P)
-
 	    P = Numeric.where(abs(P) < eps, eps, P)
 	    
 	    alpha = Numeric.where(                    P > 10.,                   (P - 1.) / P,   0.5)
