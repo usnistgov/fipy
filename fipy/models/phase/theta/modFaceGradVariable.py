e67f4b3f3f069f7a6c74a9f51359b08537f45e92
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-02-11 22:48:21 +0000

    Added a modFaceGradVariable, and split faceGradVariable to use another variable to profile in a better way.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@275 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/models/phase/theta/modFaceGradVariable.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+## -*-Pyth-*-
+ # ###################################################################
+ #  PyFiVol - Python-based finite volume PDE solver
+ # 
+ #  FILE: "faceGradVariable.py"
+ #                                    created: 12/18/03 {2:52:12 PM} 
+ #                                last update: 2/2/04 {2:52:19 PM}
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  See the file "license.terms" for information on usage and  redistribution
+ #  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ #  
+ # ###################################################################
+ ##
+
+import Numeric
+
+from fivol.variables.faceGradVariable import FaceGradVariable
+
+class ModFaceGradVariable(FaceGradVariable):
+    def __init__(self, var, modFunc):
+	FaceGradVariable.__init__(self, var)
+	self.var = self.requires(var)
+        self.modFunc = modFunc
+    
+    def mod(self, argument):
+        return self.modFunc(argument)
+    
---
+++ fipy/variables/faceGradContributionsVariable.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+## -*-Pyth-*-
+ # ###################################################################
+ #  PyFiVol - Python-based finite volume PDE solver
+ # 
+ #  FILE: "cellGradVariable.py"
+ #                                    created: 12/18/03 {2:28:00 PM} 
+ #                                last update: 2/3/04 {1:21:10 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  See the file "license.terms" for information on usage and  redistribution
+ #  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ #  
+ # ###################################################################
+ ##
+ 
+import Numeric
+
+from fivol.variables.vectorCellVariable import VectorCellVariable
+
+class FaceGradContributions(VectorCellVariable):
+    def __init__(self, var):
+        VectorCellVariable.__init__(self, var.getMesh())
+	self.var = self.requires(var)
+
+    def calcValue(self):
+        self.calcValue1()
+
+    def calcValue1(self):
+        self.value = self.mesh.getAreaProjections() * self.var.getFaceValue().transpose().getNumericValue()
+    
+
