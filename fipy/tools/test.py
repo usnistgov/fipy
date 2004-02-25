eaed6422aead3da9d7c01c3020dffb37446feab9
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-02-25 22:50:40 +0000

    Added pickle read and write and a test case.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@304 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/tools/test.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  PyFiVol - Python-based finite volume PDE solver
+ # 
+ #  FILE: "test.py"
+ #                                    created: 11/10/03 {3:23:47 PM}
+ #                                last update: 2/20/04 {4:05:53 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+"""Test steady-state diffusion solutions
+"""
+ 
+import unittest
+import fivol.tests.testProgram
+
+import fivol.tools.dump as dump
+from fivol.meshes.grid2D import Grid2D
+from fivol.examples.phase.theta.modularVariable import ModularVariable
+
+class TestDump(unittest.TestCase):
+    def setUp(self):
+        mesh = Grid2D(1.23, 4.5, 5, 10)
+        theta = ModularVariable(
+            mesh = mesh,
+            value = 100.0)
+
+        self.data = (theta, mesh) 
+        dump.write(self.data, 'tmpFile')
+        self.dataUnpickled = dump.read('tmpFile')
+
+    def assertEqual(self, first, second, msg = None):
+        if first == second:
+            pass
+        else:
+            raise self.failureException, (msg or '\n%s\n!=\n%s' % (first, second))
+
+    def testResult(self):
+        self.assertEqual(self.data[0], self.dataUnpickled[1])
+        self.assertEqual(self.data[1].getCellCenters(), self.dataUnpickled[1].getCellCenters())
+
+def suite():
+    theSuite = unittest.TestSuite()
+    theSuite.addTest(unittest.makeSuite(TestDump))
+    return theSuite
+    
+if __name__ == '__main__':
+    fivol.tests.testProgram.main(defaultTest='suite')
