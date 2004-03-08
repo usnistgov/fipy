28074551daec22f2058dfb96cb578005e5244878
Jonathan E. Guyer <guyer@nist.gov>
2004-03-08 21:53:23 +0000

    seting interpolation example
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@339 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/tests/blinky.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  PyFiVol - Python-based finite volume PDE solver
+ # 
+ #  FILE: "input.py"
+ #                                    created: 11/17/03 {10:29:10 AM} 
+ #                                last update: 3/8/04 {4:47:23 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-17 JEG 1.0 original
+ # ###################################################################
+ ##
+
+import Numeric
+
+from fivol.profiler.profiler import Profiler
+from fivol.profiler.profiler import calibrate_profiler
+
+from fivol.meshes.grid2D import Grid2D
+from fivol.viewers.grid2DGistViewer import Grid2DGistViewer
+from fivol.variables.cellVariable import CellVariable
+
+nx = 20
+dx = 0.01
+L = nx * dx
+
+mesh = Grid2D(
+    dx = dx,
+    dy = dx,
+    nx = nx,
+    ny = nx)
+
+var = CellVariable(
+    name = "test",
+    mesh = mesh)
+
+bottomLeft = mesh.getCells(lambda cell: cell.getCenter()[0] < L/2. and cell.getCenter()[1] < L/2.)
+bottomRight = mesh.getCells(lambda cell: cell.getCenter()[0] >= L/2. and cell.getCenter()[1] < L/2.)
+topLeft = mesh.getCells(lambda cell: cell.getCenter()[0] < L/2. and cell.getCenter()[1] >= L/2.)
+topRight = mesh.getCells(lambda cell: cell.getCenter()[0] >= L/2. and cell.getCenter()[1] >= L/2.)
+
+var.setValue(mesh.getCellCenters()[:,0] * mesh.getCellCenters()[:,1])
+
+if __name__ == '__main__':
+    viewer = Grid2DGistViewer(var = var, minVal = 0, maxVal = L * L)
+    viewer.plot()
+	
+    raw_input()
+    
+
+    # fudge = calibrate_profiler(10000)
+    # profile = Profiler('profile', fudge=fudge)
+
+##     def bottomRight(cell, cornerCell):
+## 	return L - cell.getCenter()[0] == cornerCell.getCenter()[1] and cell.getCenter()[1] == cornerCell.getCenter()[1]
+	
+##     print mesh.getCellCenters()
+    
+##     pt = (0.045, 0.045)
+##     
+##     d = mesh.getPointToCellDistances(pt)
+##     
+##     print d
+## 
+##     print Numeric.argmin(d)
+## 
+##     i = Numeric.argsort(d)
+##     
+##     print i
+##     
+##     print (var[i[0]] * var[i[1]] * (d[i[0]] + d[i[1]])) / (var[i[0]] * d[i[0]] + var[i[1]] * d[i[1]])
+##     
+##     print var(pt)
+##     
+##     print bottomRight
+    
+    [var.setValue(var((L - cell.getCenter()[0], cell.getCenter()[1])), [cell]) for cell in bottomRight]
+    [var.setValue(var((cell.getCenter()[0], L - cell.getCenter()[1])), [cell]) for cell in topLeft]
+    [var.setValue(var((L - cell.getCenter()[0], L - cell.getCenter()[1])), [cell]) for cell in topRight]
+##     for cell in bottomRight:
+## 	var.setValue(var((L - cell.getCenter()[0], cell.getCenter()[1])), [cell]) 
+
+##     print cornerCells
+##     print [mesh.getCells(lambda cell: L - cell.getCenter()[0] == cornerCell.getCenter()[0] and cell.getCenter()[1] == cornerCell.getCenter()[1]) for cornerCell in cornerCells]
+##     [var.setValue(var[cornerCell.getID()], mesh.getCells(lambda cell: L - cell.getCenter()[0] == cornerCell.getCenter()[1] and cell.getCenter()[1] == cornerCell.getCenter()[1])) for cornerCell in cornerCells]
+    viewer.plot()
+	
+    raw_input()
+    
+    # profile.stop()
+
