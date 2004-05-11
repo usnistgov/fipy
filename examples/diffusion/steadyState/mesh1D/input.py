1701dc8adc4a293e923f74ac9d79bcf8f0321451
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-05-11 19:13:21 +0000

    Simplified the steadyState input and test files.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@404 d80e17d7-ff13-0410-a124-85740d801063

---
+++ examples/diffusion/steadyState/mesh1D/input.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "input.py"
+ #                                    created: 12/29/03 {3:23:47 PM}
+ #                                last update: 4/2/04 {4:02:29 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+##from fipy.tools.profiler.profiler import Profiler
+##from fipy.tools.profiler.profiler import calibrate_profiler
+ 
+from fipy.meshes.grid2D import Grid2D
+from fipy.equations.diffusionEquation import DiffusionEquation
+from fipy.solvers.linearPCGSolver import LinearPCGSolver
+from fipy.boundaryConditions.fixedValue import FixedValue
+from fipy.boundaryConditions.fixedFlux import FixedFlux
+from fipy.iterators.iterator import Iterator
+from fipy.variables.cellVariable import CellVariable
+from fipy.viewers.grid2DGistViewer import Grid2DGistViewer
+
+nx = 50
+ny = 1
+
+dx = 1.
+dy = 1.
+
+valueLeft = 0.
+valueRight = 1.
+
+mesh = Grid2D(dx = dx, dy = dy, nx = nx, ny = ny)
+
+var = CellVariable(name = "concentration",
+                   mesh = mesh,
+                   value = valueLeft)
+
+viewer = Grid2DGistViewer(var, minVal =0., maxVal = 1.)
+
+eq = DiffusionEquation(var,
+                       transientCoeff = 0., 
+                       diffusionCoeff = 1.,
+                       solver = LinearPCGSolver(tolerance = 1.e-15, 
+                                                steps = 1000
+                                                ),
+                       boundaryConditions = (FixedValue(mesh.getFacesLeft(),valueLeft),
+                                             FixedValue(mesh.getFacesRight(),valueRight),
+                                             FixedFlux(mesh.getFacesTop(),0.),
+                                             FixedFlux(mesh.getFacesBottom(),0.)
+                                             )
+                       )
+
+it = Iterator((eq,))
+
+def run():
+    it.timestep()
+    return var.getNumericValue()
+
+def testResult():
+    Lx = nx * dx
+    x = mesh.getCellCenters()[:,0]
+    return valueLeft + (valueRight - valueLeft) * x / Lx
+
+if __name__ == '__main__':
+
+    run()
+    
+    viewer.plot()
+
+    raw_input("finished")
---
+++ examples/diffusion/steadyState/mesh1D/test.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "test.py"
+ #                                    created: 12/29/03 {3:23:47 PM}
+ #                                last update: 4/2/04 {4:05:59 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+"""Test steady-state diffusion solutions
+"""
+ 
+import unittest
+
+import fipy.tests.testProgram
+from fipy.tests.testBase import TestBase
+import input
+
+class Test(TestBase):
+    def testResult(self):
+        self.assertArrayWithinTolerance(input.run(), input.testResult())
+
+def suite():
+    theSuite = unittest.TestSuite()
+    theSuite.addTest(unittest.makeSuite(Test))
+    return theSuite
+
+if __name__ == '__main__':
+    fipy.tests.testProgram.main(defaultTest='suite')
