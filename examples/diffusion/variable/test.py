b5369aca37dce1d1e75c1dfb076a4e88a69d9a8a
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-06-16 19:28:45 +0000

    Changed the xplicit cases over to use doc test
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@452 d80e17d7-ff13-0410-a124-85740d801063

--- examples/diffusion/variable/test.py
+++ examples/diffusion/variable/test.py
@@ -45,80 +45,19 @@
 """
  
 import unittest
-
-import Numeric
-
-from fipy.tests.testBase import TestBase
 import fipy.tests.testProgram
+import doctest
 
-import input
-
-class TestVariableDiffusion(TestBase):
-    """
-    Variable diffusion test case. Diffusion
-    varies in space. Test is for the steady state profile.
-    Test case considers a bar with the following diffusion coefficient:
-    
-    1    when   0 < x < L/2
-    0.1  when   L/2 < x < 3L/4
-    1    when   3L/4 < x < L
-
-    where L is the length of the bar. The boundary conditions are a fixed value
-    of 0 on the left side and a fixed flux of 1 on the right side. A simple analytical
-    answer to this diffusion problem is given in ansFunc(). In order to obtain very
-    accurate numerical solutions it is necessary for cell centers to lie at exactly
-    L/4 and 3L/4. Thus the test case should give accurate results for meshes of length
-    2, 6, 10, 14, 18,...,50,... i.e. 4*n+2 where n is an integer.
-    """
-    
-    def setUp(self):
-        parameters = input.getParameters(self.nx, self.ny)
-        self.it = parameters['it']
-        self.steps = 1
-        self.var = parameters['var']
-        self.mesh = parameters['mesh']
-        self.Lx = parameters['L']
-        self.tolerance = 1e-8
-
-    def getTestValues(self):
-	x = self.mesh.getCellCenters()[:,0]
-	x = Numeric.array(x)
-	L = self.Lx
-	values = Numeric.where(x < 3. * L / 4., 10 * x - 9. * L / 4., x + 18. * L / 4.)
-	values = Numeric.where(     x < L / 4.,                    x,           values)
-	return values
-	    
-class TestVariableDiffusion2x1(TestVariableDiffusion):
-    """Variable 1D diffusion on a 1x2 mesh
-    gobeldegook
-    """
-    def setUp(self):
-	self.nx = 2
-	self.ny = 1
-	TestVariableDiffusion.setUp(self)
-	    
-class TestVariableDiffusion10x1(TestVariableDiffusion):
-    """Variable 1D diffusion on a 1x10 mesh
-    """
-    def setUp(self):
-	self.nx = 10
-	self.ny = 1
-	TestVariableDiffusion.setUp(self)
-	
-class TestVariableDiffusion50x1(TestVariableDiffusion):
-    """Variable 1D diffusion on a 1x50 mesh
-    """
-    def setUp(self):
-	self.nx = 50
-	self.ny = 1
-	TestVariableDiffusion.setUp(self)
+import mesh2x1.input
+import mesh10x1.input
+import mesh50x1.input
 
 def suite():
     theSuite = unittest.TestSuite()
     
-    theSuite.addTest(unittest.makeSuite(TestVariableDiffusion2x1))
-    theSuite.addTest(unittest.makeSuite(TestVariableDiffusion10x1))
-    theSuite.addTest(unittest.makeSuite(TestVariableDiffusion50x1))
+    theSuite.addTest(doctest.DocTestSuite(mesh2x1.input))
+    theSuite.addTest(doctest.DocTestSuite(mesh10x1.input))
+    theSuite.addTest(doctest.DocTestSuite(mesh50x1.input))
     
     return theSuite
     
