da06d0e021f71ac0fa0b7a2824dcf451f6a24e28
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-06-22 19:23:32 +0000

    Added a heirachy of test cases.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@476 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/test.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "test.py"
+ #                                    created: 11/10/03 {3:23:47 PM}
+ #                                last update: 6/10/04 {9:57:27 AM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+import fipy.tests.testProgram
+import unittest
+
+import models.test
+import tools.test
+import variables.test
+import meshes.numMesh.test
+
+def suite():
+    theSuite = unittest.TestSuite()
+
+    theSuite.addTest(models.test.suite())
+    theSuite.addTest(tools.test.suite())
+    theSuite.addTest(meshes.numMesh.test.suite())
+    theSuite.addTest(variables.test.suite())
+    
+    return theSuite
+    
+if __name__ == '__main__':
+    fipy.tests.testProgram.main(defaultTest='suite')
