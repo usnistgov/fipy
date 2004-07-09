da750fc2efc0efd25a74660d3f7d56b79180e649
Alex Mont <no@email>
2004-07-09 19:19:27 +0000

    Tri2D Test Case
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@526 d80e17d7-ff13-0410-a124-85740d801063

---
+++ examples/convection/exponential1DSource/tri2Dinput.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "input.py"
+ #                                    created: 12/16/03 {3:23:47 PM}
+ #                                last update: 4/2/04 {4:02:23 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+"""
+
+This example solves the steady-state convection-diffusion equation as described in
+`./examples/diffusion/convection/exponential1D/inpuy.py` but uses a constant source
+value such that,
+
+.. raw:: latex
+
+    $$ S_c = 1. $$
+
+Here the axes are reversed (`nx = 1`, `ny = 1000`) and
+
+.. raw:: latex
+
+    $$ \\vec{u} = (0, 10) $$
+
+The analytical solution test for this problem is given by:
+
+   >>> axis = 1
+   >>> x = mesh.getCellCenters()[:,axis]
+   >>> AA = -sourceCoeff * x / convCoeff[axis]
+   >>> BB = 1. + sourceCoeff * L / convCoeff[axis]
+   >>> import Numeric
+   >>> CC = 1. - Numeric.exp(-convCoeff[axis] * x / diffCoeff)
+   >>> DD = 1. - Numeric.exp(-convCoeff[axis] * L / diffCoeff)
+   >>> analyticalArray = AA + BB * CC / DD
+   >>> Numeric.allclose(analyticalArray, Numeric.array(var), rtol = 1e-6, atol = 1e-6) 
+   1
+   
+"""
+     
+from fipy.meshes.numMesh.tri2D import Tri2D
+from fipy.equations.stdyConvDiffScEquation import SteadyConvectionDiffusionScEquation
+from fipy.solvers.linearLUSolver import LinearLUSolver
+from fipy.iterators.iterator import Iterator
+from fipy.variables.cellVariable import CellVariable
+from fipy.terms.scSourceTerm import ScSourceTerm
+from fipy.viewers.pyxviewer import PyxViewer
+from fipy.terms.exponentialConvectionTerm import ExponentialConvectionTerm
+from fipy.boundaryConditions.fixedValue import FixedValue
+from fipy.boundaryConditions.fixedFlux import FixedFlux
+
+valueBottom = 0.
+valueTop = 1.
+L = 10.
+nx = 1
+ny = 1000
+diffCoeff = 1.
+convCoeff = (0., 10.)
+sourceCoeff = 0.
+
+
+
+mesh = Tri2D(L / ny, L / ny, nx, ny)
+
+var = CellVariable(
+    name = "solution variable",
+    mesh = mesh,
+    value = valueBottom)
+
+boundaryConditions = (
+    FixedValue(mesh.getFacesTop(), valueTop),
+    FixedValue(mesh.getFacesBottom(), valueBottom),
+    FixedFlux(mesh.getFacesRight(), 0.),
+    FixedFlux(mesh.getFacesLeft(), 0.)
+    )
+
+        
+eq = SteadyConvectionDiffusionScEquation(
+    var = var,
+    diffusionCoeff = diffCoeff,
+    convectionCoeff = convCoeff,
+    sourceCoeff = sourceCoeff,
+    solver = LinearLUSolver(tolerance = 1.e-15, steps = 2000),
+    convectionScheme = ExponentialConvectionTerm,
+    boundaryConditions = boundaryConditions
+    )
+
+it = Iterator((eq,))
+
+it.timestep()
+
+if __name__ == '__main__':
+    viewer = PyxViewer(var)
+    print var
+    viewer.plot()
+    raw_input('finished')
---
+++ examples/diffusion/variable/mesh10x1/tri2Dinput.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+## 
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "input.py"
+ #                                    created: 12/29/03 {3:23:47 PM}
+ #                                last update: 4/2/04 {4:06:26 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+"""
+
+This example is a 1D steady state diffusion test case as in
+`./examples/diffusion/variable/mesh2x1/input.py` with then
+number of cells set to `nx = 10`.
+
+A simple analytical answer can be used to test the result:
+
+   >>> x = mesh.getCellCenters()[:,0]
+   >>> values = Numeric.where(x < 3. * L / 4., 10 * x - 9. * L / 4., x + 18. * L / 4.)
+   >>> values = Numeric.where(x < L / 4., x, values)
+   >>> Numeric.allclose(values, Numeric.array(var), atol = 1e-8, rtol = 1e-8)
+   1
+
+"""
+
+import Numeric
+
+from fipy.boundaryConditions.fixedValue import FixedValue
+from fipy.boundaryConditions.fixedFlux import FixedFlux
+from fipy.equations.diffusionEquation import DiffusionEquation
+from fipy.iterators.iterator import Iterator
+from fipy.meshes.numMesh.tri2D import Tri2D
+from fipy.solvers.linearPCGSolver import LinearPCGSolver
+from fipy.variables.cellVariable import CellVariable
+from fipy.viewers.pyxviewer import PyxViewer
+
+nx = 10
+ny = 1
+
+valueLeft = 0.
+fluxRight = 1.
+timeStepDuration = 1. 
+
+L = 10.
+
+dx = L / nx
+dy = 1.
+
+mesh = Tri2D(dx, dy, nx, ny)
+    
+var = CellVariable(
+    name = "solution variable",
+    mesh = mesh,
+    value = valueLeft)
+
+x = mesh.getFaceCenters()[:,0]
+middleFaces = Numeric.logical_or(x < L / 4.,x >= 3. * L / 4.)
+diffCoeff = Numeric.where(middleFaces, 1., 0.1)
+
+eq = DiffusionEquation(
+    var,
+    transientCoeff = 0. / timeStepDuration, 
+    diffusionCoeff = diffCoeff,
+    solver = LinearPCGSolver(
+    tolerance = 1.e-15, 
+    steps = 1000
+    ),
+    boundaryConditions=(
+    FixedValue(mesh.getFacesLeft(),valueLeft),
+    FixedFlux(mesh.getFacesRight(),fluxRight),
+    FixedFlux(mesh.getFacesTop(),0.),
+    FixedFlux(mesh.getFacesBottom(),0.)
+    )
+    )
+
+it = Iterator((eq,))
+
+it.timestep()
+
+if __name__ == '__main__':
+    viewer = PyxViewer(var)
+    viewer.plot()
+    raw_input('finished')
