554f0e233ec578479f5886bd4af34559732c67b1
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-07-14 18:51:23 +0000

    Upwind convection term
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@547 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/terms/upwindConvectionTerm.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+## -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "upwindConvectionTerm.py"
+ #                                    created: 12/5/03 {2:50:05 PM} 
+ #                                last update: 4/2/04 {4:00:49 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  See the file "license.terms" for information on usage and  redistribution
+ #  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ #  
+ # ###################################################################
+ ##
+
+import Numeric
+
+from fipy.terms.convectionTerm import ConvectionTerm
+from fipy.variables.faceVariable import FaceVariable
+from fipy.tools.dimensions.physicalField import PhysicalField
+
+from fipy.tools.inline import inline
+
+class PowerLawConvectionTerm(ConvectionTerm):
+    class Alpha(FaceVariable):
+	def __init__(self, P):
+	    FaceVariable.__init__(self, mesh = P.getMesh())
+	    self.P = self.requires(P)
+	    
+	def _calcValuePy(self, eps, P):
+	    P = Numeric.where(abs(P) < eps, eps, P)
+	    
+	    alpha = Numeric.where(P > 0., Numeric.ones(len(P)), Numeric.zeros(len(P)))
+
+	    self.value = PhysicalField(value = alpha)
+
+	def _calcValueIn(self, eps, P):
+
+	    inline.runInlineLoop1("""
+		if (P(i) > 0) {
+		    P(i) = eps;
+		}
+		
+		alpha(i) = 0.5;
+		
+		if (P(i) > 10.) {
+		    alpha(i) = (P(i) - 1.) / P(i);
+		} else if (10. >= P(i) && P(i) > eps) {
+		    double	tmp = (1. - P(i) / 10.);
+		    double	tmpSqr = tmp * tmp;
+		    alpha(i) = ((P(i) - 1.) + tmpSqr*tmpSqr*tmp) / P(i);
+		} else if (eps >= P(i) && P(i) >= -10) {
+		    double	tmp = (1. + P(i) / 10.);
+		    double	tmpSqr = tmp * tmp;
+		    alpha(i) = (tmpSqr*tmpSqr*tmp - 1.) / P(i);
+		} else {	// P(i) < -10.
+		    alpha(i) = -1. / P(i);
+		}
+	    """,
+	    alpha = self.value.value, eps = eps, P = P,
+	    ni = len(self.mesh.getFaces())
+	    )
+
+
+	def calcValue(self):	    
+	    eps = 1e-3
+	    P  = self.P.getNumericValue()
+	    
+	    inline.optionalInline(self._calcValueIn, self._calcValuePy, eps, P)
