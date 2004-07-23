0ca0ec266bbe1b2808a6af2604527fc2043b7a23
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-07-23 01:04:21 +0000

    Added lots of documentation and other doc tests.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@575 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/models/levelSet/advection/higherOrderAdvectionTerm.py
+++ fipy/models/levelSet/advection/higherOrderAdvectionTerm.py
@@ -43,7 +43,7 @@
 
 """
 
-The `AdvectionTerm` object constructs the b vector contribution for
+The `HigherOrderAdvectionTerm` object constructs the `b` vector contribution for
 the advection term given by
 
 .. raw:: latex
@@ -56,12 +56,34 @@ from the advection equation given by:
 
     $$ \\frac{\\partial \\phi}{\\partial t} + u | \\nabla \\phi | = 0$$
 
-The construction of the gradient magnitude term requires upwinding.
+The construction of the gradient magnitude term requires upwinding as in the standard
+`AdvectionTerm`. The higher order terms are incorperated as follows.
 The formula used here is given by:
 
 .. raw:: latex
 
-    $$ u_P | \\nabla \\phi |_P = \\max \\left( u_P , 0 \\right) \\left[  \\sum_A \\min \\left( \\frac{ \\phi_A - \\phi_P } { d_{AP}}, 0 \\right)^2 \\right]^{1/2} +  \\min \\left( u_P , 0 \\right) \\left[  \\sum_A \\max \\left( \\frac{ \\phi_A - \\phi_P } { d_{AP}}, 0 \\right)^2 \\right]^{1/2} $$
+    $$ u_P | \\nabla \\phi |_P = \\max \\left( u_P , 0 \\right) \\left[  \\sum_A \\min \\left( D_{AP}, 0 \\right)^2 \\right]^{1/2} +  \\min \\left( u_P , 0 \\right) \\left[  \\sum_A \\max \\left( D_{AP}, 0 \\right)^2 \\right]^{1/2} $$
+
+where,
+
+.. raw:: latex
+
+    $$ D_{AP} = \\frac{ \\phi_A - \\phi_P } { d_{AP}} - \\frac{ d_{AP} } {2} m \\left(L_A, L_P \\right) $$
+
+and
+
+.. raw:: latex
+
+    $$ m\\left(x, y\\right) = x \;\; \\text{if} \;\; |x| \le |y| \;\; xy \ge 0 $$
+    $$ m\\left(x, y\\right) = y \;\; \\text{if} \;\; |x| > |y|   \;\; xy \ge 0 $$
+    $$ m\\left(x, y\\right) = 0 \;\; \\text{if} \;\; xy < 0 $$
+    
+also,
+
+.. raw:: latex
+
+    $$ L_A = \\frac{\\phi_{AA} + \\phi_P - 2 \\phi_A}{d_{AP}^2} $$
+    $$ L_P = \\frac{\\phi_{A} + \\phi_{PP} - 2 \\phi_P}{d_{AP}^2} $$
 
 Here are some simple test cases for this problem:
 
@@ -108,8 +130,71 @@ Somewhat less trivial test case:
    >>> answer = -vel * Numeric.array((2, Numeric.sqrt(2**2 + 6**2), 1, 0))
    >>> Numeric.allclose(b, answer, atol = 1e-10)
    1
-   
+
+For the above test cases the `HigherOrderAdvectionTerm` gives the
+same result as the `AdvectionTerm`. The following test imposes a quadratic
+field. The higher order term can resolve this field correctly.
+
+.. raw:: latex
+
+    $$ \\phi = x^2 $$
+
+The returned vector `b` should have the value:
+
+.. raw:: latex
+
+    $$ -|\\nabla \\phi| = -\\left|\\frac{\\partial \\phi}{\\partial x}\\right| = - 2 |x| $$
+
+Build the test case in the following way,
+
+   >>> mesh = Grid2D(dx = 1., dy = 1., nx = 5, ny = 1)
+   >>> vel = 1.
+   >>> coeff = CellVariable(mesh = mesh, value = mesh.getCellCenters()[:,0]**2)
+   >>> L, b = AdvectionTerm(vel, mesh = mesh).buildMatrix(coeff)
+
+The first order term is not accurate. The first and last element are ignored because they
+don't have any neighbors for higher order evaluation
+
+   >>> Numeric.allclose(b[1:-1], -2 * mesh.getCellCenters()[:,0][1:-1])
+   0
+
+The higher order term is spot on.
+
+   >>> L, b = HigherOrderAdvectionTerm(vel, mesh = mesh).buildMatrix(coeff)
+   >>> Numeric.allclose(b[1:-1], -2 * mesh.getCellCenters()[:,0][1:-1])
+   1
+
+The `HigherOrderAdvectionTerm` will also resolve a circular field with
+more accuracy,
+
+.. raw:: latex
+
+    $$ \\phi = \\left( x^2 + y^2 \\right)^{1/2} $$
+
+Build the test case in the following way,
+
+   >>> mesh = Grid2D(dx = 1., dy = 1., nx = 10, ny = 10)
+   >>> vel = 1.
+   >>> x = mesh.getCellCenters()[:,0]
+   >>> y = mesh.getCellCenters()[:,1]
+   >>> r = Numeric.sqrt(x**2 + y**2)
+   >>> coeff = CellVariable(mesh = mesh, value = r)
+   >>> L, b = AdvectionTerm(1., mesh = mesh).buildMatrix(coeff)
+   >>> error = Numeric.reshape(Numeric.reshape(b, (10,10))[2:-2,2:-2] + 1, (36,))
+   >>> print error[Numeric.argmax(error)]
+   0.123105625618
+
+The maximum error is large (about 12%) for the first order advection.
+
+   >>> L, b = HigherOrderAdvectionTerm(1., mesh = mesh).buildMatrix(coeff)
+   >>> error = Numeric.reshape(Numeric.reshape(b, (10,10))[2:-2,2:-2] + 1, (36,))
+   >>> print error[Numeric.argmax(error)]
+   0.0201715476597
+
+The maximum error is 2% when using a higher order contribution.
+
 """
+__docformat__ = 'restructuredtext'
 
 import MA
 import Numeric
