1154fd892899f8e8e4795341cb36274e58aa1601
Jonathan E. Guyer <guyer@nist.gov>
2004-07-26 18:51:26 +0000

    Viewer for vector fields
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@603 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/viewers/gistVectorViewer.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+## -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "gistViewer.py"
+ #                                    created: 11/10/03 {2:48:25 PM} 
+ #                                last update: 7/13/04 {4:20:47 PM} { 2:45:36 PM}
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  See the file "license.terms" for information on usage and  redistribution
+ #  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+import Numeric
+
+import gist
+
+from fipy.viewers.gistViewer import GistViewer
+
+class GistVectorViewer(GistViewer):
+    
+    def __init__(self, var = None, title = '', grid = 1):
+	self.var = var
+	GistViewer.__init__(self, title, grid)
+	
+    def plot(self, fileName = None):
+        gist.window(self.id, wait = 1)
+	gist.pltitle(self.title)
+        gist.animate(1)
+        gist.palette(self.palette)
+## 	gist.gridxy(self.grid)
+	
+	centers = self.var.getMesh().getFaceCenters()
+	
+	gist.plmesh(Numeric.array([centers[:,1],centers[:,1]]), Numeric.array([centers[:,0],centers[:,0]]))
+
+	print self.var
+	
+	vx = Numeric.array(self.var[:,0])
+	vy = Numeric.array(self.var[:,1])
+	
+        gist.plv(Numeric.array([vy,vy]), Numeric.array([vx,vx]),scale=0.01)
+
+        if fileName is not None:
+            
+            gist.hcp_file(fileName)
+            gist.hcp()
+
+        gist.fma()
+        
+    def getArray(self):
+        pass
+        
