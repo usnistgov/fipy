7b0127dae4ac2871f87c92ed4f49f751f5a5076b
Jonathan E. Guyer <guyer@nist.gov>
2004-07-30 23:46:16 +0000

    Attempt to fix weave's obnoxious rebuilding
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@634 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/tools/vector.py
+++ fipy/tools/vector.py
@@ -6,7 +6,7 @@
  # 
  #  FILE: "tools.py"
  #                                    created: 11/17/03 {5:05:47 PM} 
- #                                last update: 7/27/04 {1:55:19 PM} 
+ #                                last update: 7/27/04 {3:27:17 PM} 
  #  Author: Jonathan Guyer
  #  E-mail: guyer@nist.gov
  #  Author: Daniel Wheeler
@@ -75,8 +75,7 @@ def _putAddPy(vector, ids, additionVector):
 
 def _putAddIn(vector, ids, additionVector):
     inline.runInlineLoop1("""
-	int	j = ids(i);
-	vector(j) += additionVector(i);
+	vector(ids(i)) += additionVector(i);
     """, 
     vector = vector, ids = ids, additionVector = additionVector,
     ni = len(ids))
