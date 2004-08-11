b7bff98bdbed2cd54a7974728aa8a80720f76746
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-08-11 21:20:47 +0000

    Created expanding circle test cases
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@689 d80e17d7-ff13-0410-a124-85740d801063

--- examples/levelSet/surfactant/expandingCircle.py
+++ examples/levelSet/surfactant/expandingCircle.py
@@ -56,22 +56,30 @@ The solution for these set of equations is given by:
 
 .. raw:: latex
 
-    $$ r = \\sqrt{2 r_0 \\theta_0 t + r_0^2} $$
+    $$ r = \\sqrt{2 k r_0 \\theta_0 t + r_0^2} $$
     $$ \\theta = \\frac{r_0 \\theta_0}{\\sqrt{2 k r_0 \\theta_0 t + r_0^2}} $$
     
-The result can be tested with the following code:
-
+The following tests can be performed. First test for global
+conservation of surfactant:
 
    >>> surfactantBefore = Numeric.sum(surfactantVariable * mesh.getCellVolumes())
+   >>> totalTime = 0
    >>> for step in range(steps):
+   ...     velocity.setValue(surfactantVariable.getInterfaceValue() * k)
+   ...     argmax = Numeric.argmax(velocity)
+   ...     timeStepDuration = cfl * dx / velocity[argmax]
    ...     it.timestep(dt = timeStepDuration)
+   ...     totalTime += timeStepDuration
    >>> surfactantEquation.solve()
    >>> surfactantAfter = Numeric.sum(surfactantVariable * mesh.getCellVolumes())
    >>> Numeric.allclose(surfactantBefore, surfactantAfter)
    1
-   >>> areas = (distanceVariable.getCellInterfaceAreas() < 1e-6) * 1e+10 + distanceVariable.getCellInterfaceAreas()
-   >>> answer = initialSurfactantValue * initialRadius / (initialRadius +  distanceToTravel)
-   >>> coverage = surfactantVariable * mesh.getCellVolumes() / areas
+
+Next test for the correct local value of surfactant: 
+
+   >>> finalRadius = Numeric.sqrt(2 * k * initialRadius * initialSurfactantValue * totalTime + initialRadius**2)
+   >>> answer = initialSurfactantValue * initialRadius / finalRadius
+   >>> coverage = surfactantVariable.getInterfaceValue()
    >>> error = 0.
    >>> size = 0
    >>> for i in range(len(coverage)):
@@ -79,7 +87,22 @@ The result can be tested with the following code:
    ...         error += (coverage[i] / answer - 1.)**2
    ...         size += 1            
    >>> print Numeric.sqrt(error / size)
-   0.00813776069241
+   0.0148159203754
+
+Test for the correct position of the interface:
+
+   >>> x = mesh.getCellCenters()[:,0]
+   >>> y = mesh.getCellCenters()[:,1]
+   >>> radius = Numeric.sqrt((x - L / 2)**2 + (y - L / 2)**2)
+   >>> solution = radius - distanceVariable
+   >>> erro = 0.
+   >>> size = 0
+   >>> for i in range(len(coverage)):
+   ...     if coverage[i] > 1e-3:
+   ...         error += (solution[i] / finalRadius - 1.)**2
+   ...         size += 1            
+   >>> print Numeric.sqrt(error / size)
+   0.0149871164507
 
 """
 __docformat__ = 'restructuredtext'
@@ -98,20 +121,15 @@ from fipy.iterators.iterator import Iterator
 from fipy.solvers.linearPCGSolver import LinearPCGSolver
 from fipy.solvers.linearLUSolver import LinearLUSolver
 from fipy.boundaryConditions.fixedValue import FixedValue
-
+from fipy.variables.cellVariable import CellVariable
 
 L = 1.
 nx = 50
-velocity = 1.
 cfl = 0.1
-velocity = 1.
-distanceToTravel = L / 10.
 initialRadius = L / 4.
 k = 1
-
 dx = L / nx
-timeStepDuration = cfl * dx / velocity
-steps = int(distanceToTravel / dx / cfl)
+steps = 50
 
 mesh = Grid2D(dx = dx, dy = dx, nx = nx, ny = nx)
 
@@ -131,13 +149,15 @@ surfactantVariable = SurfactantVariable(
     distanceVariable = distanceVariable
     )
 
-velocity = surfactantVariable.getInterfaceValue() * k
-
-print 'velocity',velocity
+velocity = CellVariable(
+    name = 'velocity',
+    mesh = mesh,
+    value = 1.,
+    )
 
 advectionEquation = AdvectionEquation(
     distanceVariable,
-    advectionCoeff = ,
+    advectionCoeff = velocity,
     solver = LinearPCGSolver(
         tolerance = 1.e-15, 
         steps = 1000),
@@ -154,39 +174,32 @@ extensionEquation = ExtensionEquation(
     distanceVariable,
     velocity)
 
-it = Iterator((extensionEquation, surfactantEquation, advectionEquation))
+it = Iterator((extensionEquation, advectionEquation, surfactantEquation))
 
 if __name__ == '__main__':
     
     distanceViewer = Grid2DGistViewer(var = distanceVariable, palette = 'rainbow.gp', minVal = -initialRadius, maxVal = initialRadius)
     surfactantViewer = Grid2DGistViewer(var = surfactantVariable, palette = 'rainbow.gp', minVal = -1., maxVal = 100.)
+    velocityViewer = Grid2DGistViewer(var = velocity, palette = 'rainbow.gp', minVal = 0., maxVal = 2.)
     distanceViewer.plot()
     surfactantViewer.plot()
+    velocityViewer.plot()
 
-    print 'total surfactant before:',Numeric.sum(surfactantVariable * mesh.getCellVolumes())
-    
+    totalTime = 0
+
     for step in range(steps):
+        print 'step',step
+        velocity.setValue(surfactantVariable.getInterfaceValue() * k)
+
+        argmax = Numeric.argmax(velocity)
+        timeStepDuration = cfl * dx / velcity[argmax]
+        
         it.timestep(dt = timeStepDuration)
+
+        totalTime += timeStepDuration
+        
+        velocityViewer.plot()
         distanceViewer.plot()
         surfactantViewer.plot()
-    surfactantEquation.solve()
 
-
-    print 'total surfactant after:',Numeric.sum(surfactantVariable * mesh.getCellVolumes())
-
-    areas = (distanceVariable.getCellInterfaceAreas() < 1e-6) * 1e+10 + distanceVariable.getCellInterfaceAreas()
-    answer = initialSurfactantValue * initialRadius / (initialRadius +  distanceToTravel)
-    coverage = surfactantVariable * mesh.getCellVolumes() / areas
-
-    error = 0.
-    size = 0
-    for i in range(len(coverage)):
-        if coverage[i] > 1e-3:
-            error += (coverage[i] / answer - 1.)**2
-            size += 1
-            
-    error = Numeric.sqrt(error / size)
-    
-    print 'error:', error
-    
     raw_input('finished')
