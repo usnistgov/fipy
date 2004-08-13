e977dfe5834b18922dc9aff1c3beaddb5d53c99c
Alex Mont <no@email>
2004-08-13 15:36:16 +0000

    changed 1e.-10 to 1e.-100
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@710 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/meshes/numMesh/adaptiveMesh.py
+++ fipy/meshes/numMesh/adaptiveMesh.py
@@ -72,7 +72,7 @@ def orderVertices(vertexCoords, vertices):
     coordinates = Numeric.take(vertexCoords, vertices)
     centroid = Numeric.add.reduce(coordinates) / coordinates.shape[0]
     coordinates = coordinates - centroid
-    coordinates = Numeric.where(coordinates == 0, 1.e-10, coordinates) ## to prevent division by zero
+    coordinates = Numeric.where(coordinates == 0, 1.e-100, coordinates) ## to prevent division by zero
     angles = Numeric.arctan(coordinates[:, 1] / coordinates[:, 0]) + Numeric.where(coordinates[:, 0] < 0, pi, 0) ## angles go from -pi / 2 to 3*pi / 2
     sortorder = Numeric.argsort(angles)
     return Numeric.take(vertices, sortorder)
