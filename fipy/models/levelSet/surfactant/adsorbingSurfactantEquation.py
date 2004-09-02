dcdad6fa6f8be9265d0bf93361b2472e9c4d1fc1
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-09-02 21:42:41 +0000

    Added some new equations
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@766 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/models/levelSet/surfactant/adsorbingSurfactantEquation.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+## 
+ # -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "adsorbingSurfactantEquation.py"
+ #                                    created: 8/31/04 {10:39:23 AM} 
+ #                                last update: 8/31/04 {4:00:26 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-12 JEG 1.0 original
+ # ###################################################################
+ ##
+
+"""
+
+The `AdsorbingSurfactantEquation` object solves the
+`SurfactantEquation but with an adsorbing species from some bulk
+value. The equation that describes the surfactant adsorbing is given
+by,
+
+.. raw:: latex
+
+    $$ \\dot{\\theta} = J v \\theta + k c (1 - \\theta) $$
+
+This last term in this equation accounts for Langmuir type adsorption
+from the bulk. It assumes a vacant proportion of surface sites. The adsorption term
+is added to the source in teh follwoing way,
+
+.. raw:: latex
+
+    $$ S_c = k c \;\; \\text{and} \;\; S_p = k c $$
+
+The following is a test case:
+
+   >>> from fipy.models.levelSet.distanceFunction.distanceVariable import DistanceVariable
+   >>> from fipy.models.levelSet.surfactant.surfactantVariable import SurfactantVariable
+   >>> from fipy.meshes.grid2D import Grid2D
+   >>> dx = .5
+   >>> dy = 2.3
+   >>> dt = 0.25
+   >>> k = 0.56
+   >>> initialValue = 0.1
+   >>> c = 0.2
+   >>> mesh = Grid2D(dx = dx, dy = dy, nx = 5, ny = 1)
+   >>> distanceVar = DistanceVariable(mesh = mesh, value = (-dx*3/2, -dx/2, dx/2, 3*dx/2 ,5*dx/2))
+   >>> var = SurfactantVariable(value = (0, 0, initialValue, 0 ,0), distanceVariable = distanceVar)
+   >>> bulkVar = CellVariable(mesh = mesh, value = (c , c, c, c, c))
+   >>> eqn = AdsorbingSurfactantEquation(var, distanceVar, bulkVar, k)
+   >>> eqn.solve(dt = dt)
+   >>> answer = (initialValue + dt * k * c) / (1 + dt * k * c)
+   >>> Numeric.allclose(var.getInterfaceValue(), Numeric.array((0, 0, answer, 0, 0)))
+   1
+   
+"""
+
+import Numeric
+
+from fipy.variables.cellVariable import CellVariable
+from surfactantEquation import SurfactantEquation
+from fipy.terms.spSourceTerm import SpSourceTerm
+from fipy.terms.scSourceTerm import ScSourceTerm
+
+ 
+class AdsorptionCoeff(CellVariable):
+    def __init__(self, distanceVariable, bulkVar, rateConstant):
+        CellVariable.__init__(self, mesh = distanceVariable.getMesh())
+
+        self.distanceVariable = self.requires(distanceVariable)
+        self.bulkVar = self.requires(bulkVar)
+        self.rateConstant = rateConstant
+        self.dt = 0
+
+    def _calcValue(self):
+        self.value = self.dt * Numeric.array(self.bulkVar) * self.rateConstant * self.multiplier()
+
+    def updateDt(self, dt):
+        self.dt = dt
+        self.markStale()
+
+class SpAdsorptionCoeff(AdsorptionCoeff):
+    def multiplier(self):
+        return self.distanceVariable.getCellInterfaceFlag()
+    
+class ScAdsorptionCoeff(AdsorptionCoeff):
+    def multiplier(self):
+        return self.distanceVariable.getCellInterfaceAreas() / self.mesh.getCellVolumes() 
+ 
+class AdsorbingSurfactantEquation(SurfactantEquation):
+    def __init__(self,
+                 var,
+                 distanceVar,
+                 bulkVar,
+                 rateConstant):
+        
+        SurfactantEquation.__init__(self, var, distanceVar)
+        
+        self.spCoeff = SpAdsorptionCoeff(distanceVar, bulkVar, rateConstant)
+        self.scCoeff = ScAdsorptionCoeff(distanceVar, bulkVar, rateConstant)
+
+        self.terms += (
+            SpSourceTerm(self.spCoeff, self.var.getMesh()),
+            ScSourceTerm(self.scCoeff, self.var.getMesh())
+            )
+
+    def solve(self, dt):
+        self.dt = dt
+        self.scCoeff.updateDt(dt)
+        self.spCoeff.updateDt(dt)
+        SurfactantEquation.solve(self, dt)
+        
+def _test(): 
+    import doctest
+    return doctest.testmod()
+    
+if __name__ == "__main__": 
+    _test() 
---
+++ fipy/models/levelSet/surfactant/surfactantBulkDiffusionEquation.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+
+## -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "surfactantBulkDiffusionEquation.py"
+ #                                    created: 8/31/04 {10:39:23 AM} 
+ #                                last update: 8/31/04 {4:00:40 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  PFM is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-12 JEG 1.0 original
+ # ###################################################################
+ ##
+
+"""
+
+The `MetalIonDiffusionEquation` solves the diffusion of the metal
+species with a source term at the electrolyte interface. The governing
+equation is given by,
+
+.. raw:: latex
+
+    $$ \\frac{\\partial c}{\\partial t} = \\nabla \\cdot D \\nabla  c $$
+
+where,
+
+.. raw:: latex
+
+    $$ D = D_c \\;\\; \\text{when} \\;\\; \\phi > 0 $$
+    $$ D = 0   \\;\\; \\text{when} \\;\\; \\phi \\le 0 $$
+
+The velocity of the interface generally has a linear dependence on ion
+concentration. The following boundary condition applies at the zero
+level set,
+
+.. raw:: latex
+
+    $$ D \\hat{n} \\cdot \\nabla c = \\frac{v(c)}{\\Omega} \;\; \\text{at} \;\; \\phi = 0$$ 
+
+where
+
+.. raw:: latex
+
+    $$ v(c) = c V_0 $$
+
+The test case below is for a 1D steady state problem. The solution is
+given by:
+
+.. raw:: latex
+
+    $$ c(x) = \\frac{c^{\\infty}}{\Omega D / V_0 + L}\\left(x - L\\right) + c^{\\infty} $$
+
+This is the test case,
+
+   >>> import Numeric
+   >>> from fipy.meshes.grid2D import Grid2D
+   >>> nx = 11
+   >>> dx = 1.
+   >>> mesh = Grid2D(nx = nx, ny = 1, dx = dx, dy = 1)
+   >>> from fipy.variables.cellVariable import CellVariable
+   >>> ionVar = CellVariable(mesh = mesh, value = 1)
+   >>> from fipy.models.levelSet.distanceFunction.distanceVariable import DistanceVariable
+   >>> disVar = DistanceVariable(mesh = mesh, value = Numeric.arange(11) - 0.99)
+
+   >>> v = 1.
+   >>> diffusion = 1.
+   >>> omega = 1.
+   >>> cinf = 1.
+   >>> from fipy.boundaryConditions.fixedValue import FixedValue
+   >>> eqn = MetalIonDiffusionEquation(ionVar,
+   ...                                 distanceVariable = disVar,
+   ...                                 depositionRate = v * ionVar,
+   ...                                 diffusionCoeff = diffusion,
+   ...                                 metalIonAtomicVolume = omega,
+   ...                                 boundaryConditions = (
+   ...                                     FixedValue(mesh.getFacesRight(), cinf),))
+   >>> for i in range(10):
+   ...     eqn.solve(dt = 1000)
+   >>> L = (nx - 1) * dx - dx / 2
+   >>> gradient = cinf / (omega * diffusion / v + L)
+   >>> answer = gradient * (mesh.getCellCenters()[:,0] - L - dx * 3 / 2) + cinf
+   >>> answer[0] = 1
+   >>> Numeric.allclose(answer, Numeric.array(ionVar))
+   1
+   
+"""
+
+__docformat__ = 'restructuredtext'
+
+
+from fipy.equations.matrixEquation import MatrixEquation
+from fipy.terms.transientTerm import TransientTerm
+from fipy.terms.spSourceTerm import SpSourceTerm
+from fipy.terms.implicitDiffusionTerm import ImplicitDiffusionTerm
+from metalIonSourceVariable import MetalIonSourceVariable
+from metalIonDiffusionVariable import MetalIonDiffusionVariable
+from fipy.solvers.linearPCGSolver import LinearPCGSolver
+
+class MetalIonDiffusionEquation(MatrixEquation):
+    
+    def __init__(self,
+                 var,
+                 distanceVariable = None,
+                 depositionRate = 1,
+                 diffusionCoeff = 1,
+                 transientCoeff = 1,
+                 metalIonAtomicVolume = 1,
+                 solver = LinearPCGSolver(tolerance = 1.e-15,
+                                          steps = 1000),
+                 boundaryConditions = ()):
+        """
+        
+        A `MetalIonDiffusionEquation` is instantiated with the
+        following arguments,
+
+        `var` - The metal ion concentration variable.
+
+        `distanceVariable` - A `DistanceVariable` object
+
+        `depositionRate` - A float or a `CellVariable` representing the interface deposition rate.
+
+        `diffusionCoeff` - A float or a `FaceVariable`.
+
+        `transientCoeff` - In general 1 is used.
+
+        `metalIonAtomicVolume` - Atomic volume of the metal ions.
+
+        `solver` - A given solver.
+
+        `boundaryConditions` - A tuple of `BoundaryCondition` objects.
+
+        """
+        
+        mesh = var.getMesh()
+
+	terms = (
+            
+	    TransientTerm(transientCoeff, mesh),
+            
+	    ImplicitDiffusionTerm(MetalIonDiffusionVariable(distanceVariable,
+                                                            diffusionCoeff),
+                                  mesh,
+                                  boundaryConditions),
+            
+            SpSourceTerm(MetalIonSourceVariable(ionVar = var,
+                                                distanceVariable = distanceVariable,
+                                                depositionRate = depositionRate,
+                                                metalIonAtomicVolume = metalIonAtomicVolume),
+                         mesh),
+            
+            )
+
+	MatrixEquation.__init__(
+            self,
+            var,
+            terms,
+            solver)
+
+def _test(): 
+    import doctest
+    return doctest.testmod()
+    
+if __name__ == "__main__": 
+    _test() 
