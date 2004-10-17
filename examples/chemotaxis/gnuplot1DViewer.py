b16c7fcb7b2068f5f03614999fcf6de6cbf5b703
Daniel Wheeler <daniel.wheeler@nist.gov>
2004-10-17 21:56:24 +0000

    Added a 1D viewer for the chemotaxis example only.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@904 d80e17d7-ff13-0410-a124-85740d801063

---
+++ examples/chemotaxis/gnuplot1DViewer.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+## -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "gnuplot1DViewer.py"
+ #                                    created: 9/14/04 {2:48:25 PM} 
+ #                                last update: 9/14/04 {10:30:12 PM} { 2:45:36 PM}
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #  Author: Daniel Wheeler
+ #  E-mail: daniel.wheeler@nist.gov
+ #    mail: NIST
+ #     www: http://ctcms.nist.gov
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  FiPy is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  See the file "license.terms" for information on usage and  redistribution
+ #  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+"""
+
+The `GnuplotViewer` plots a 1D Numeric array using a front end python
+wrapper available to download (Gnuplot.py_).
+
+.. _Gnuplot.py: http://gnuplot-py.sourceforge.net/
+
+If one would like more specific styles for a plot, it is probably best
+to create an inherited class and change the `gnuplotCommands` method.
+
+Different style script demos_ are available at the Gnuplot_ site.
+
+.. _Gnuplot: http://gnuplot.sourceforge.net/
+.. _demos: http://gnuplot.sourceforge.net/demo/
+
+`GnuplotViewer` requires Gnuplot_ version 4.0.
+
+"""
+__docformat__ = 'restructuredtext'
+
+import Numeric
+import Gnuplot
+
+class Gnuplot1DViewer:
+    
+    def __init__(self, vars = (), title = '', varTitles = (), xlabel = '', ylabel = ''):
+        """
+
+        Argument list:
+
+        `var` - The variable to be plotted
+
+        """
+        
+        self.vars = vars
+        self.varTitles = varTitles
+        self.plotter = Gnuplot.Gnuplot()
+        self.plotter('set key on top')
+        self.plotter('show key')
+        self.plotter.xlabel(xlabel)
+        self.plotter.ylabel(ylabel)
+        self.plotter.title(title)
+        
+    def plot(self, fileName = None):
+
+        data = ()
+        i = 0
+        for var in self.vars:
+            data += (Gnuplot.Data(var.getMesh().getCellCenters()[:,0],
+                                  var,
+                                  title = self.varTitles[i],
+                                  with = 'lines'),)
+            i += 1
+            
+        self.plotter.plot(data[0], data[1], data[2])
+
+        if fileName is not None:
+            self.plotter.hardcopy(fileName, enhanced=1, color=1)
+        
+        
