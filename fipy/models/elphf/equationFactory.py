01f836ad2f2d8eb71854dca12adacfb882bf904e
Jonathan E. Guyer <guyer@nist.gov>
2004-12-10 02:29:53 +0000

    abstract base class for equation factories
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/branch-additiveTerms@1072 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/models/elphf/equationFactory.py
@@ -0,0 +1,43 @@
+## -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - a finite volume PDE solver in Python
+ # 
+ #  FILE: "equationFactory.py"
+ #                                    created: 12/8/04 {4:46:00 PM} 
+ #                                last update: 12/8/04 {4:47:12 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #    mail: NIST
+ #     www: http://www.ctcms.nist.gov/fipy/
+ #  
+ # ========================================================================
+ # This document was prepared at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this document is not subject to copyright
+ # protection and is in the public domain.  equationFactory.py
+ # is an experimental work.  NIST assumes no responsibility whatsoever
+ # for its use by other parties, and makes no guarantees, expressed
+ # or implied, about its quality, reliability, or any other characteristic.
+ # We would appreciate acknowledgement if the document is used.
+ # 
+ # This document can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  See the file "license.terms" for information on usage and  redistribution
+ #  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2004-12-08 JEG 1.0 original
+ # ###################################################################
+ ##
+
+class EquationFactory:
+    pass
