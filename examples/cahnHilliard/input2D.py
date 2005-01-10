41f1b7bb7161c9d025732d04d93d2976a3da9c15
Daniel Wheeler <daniel.wheeler@nist.gov>
2005-01-10 20:14:13 +0000

    Still working on this.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@1132 d80e17d7-ff13-0410-a124-85740d801063

--- examples/cahnHilliard/input2D.py
+++ examples/cahnHilliard/input2D.py
@@ -48,7 +48,9 @@ This example solves the Cahn-Hilliard equation given by:
 
 .. raw:: latex
 
-    $$ \\frac{\\partial \\phi}{\\partial t} = \\nabla \\cdot D \\nabla \\left( \\frac{\\partial f}{\\partial \\phi} - \\epsilon^2 \\nabla^2 \\phi \\right) $$
+    $$ \\frac{\\partial \\phi}{\\partial t} = \\nabla \\cdot D \\nabla
+    \\left( \\frac{\\partial f}{\\partial \\phi} - \\epsilon^2
+    \\nabla^2 \\phi \\right) $$
 
 where the free energy functional is given by,
 
@@ -56,67 +58,26 @@ where the free energy functional is given by,
 
     $$ f = \\frac{a^2}{2} \\phi^2 (1 - \\phi)^2 $$
 
-The solution to the 1D problem over an infinite domain is given by,
-
-.. raw:: latex
-
-    $$ \\phi(x) = \\frac{1}{1 + \exp{\\left(-\\frac{a}{\\epsilon} x \\right)}} $$
-
-Evolve the solution to equilibrium,
-
-   >>> for step in range(steps):
-   ...     it.timestep(dt = 10)
-
-Calculate the answer,
-
-   >>> a = Numeric.sqrt(parameters['asq'])
-   >>> answer = 1 / (1 + Numeric.exp(-a * (mesh.getCellCenters()[:,0] - L / 2) / parameters['epsilon']))
-
-Compare with the numerical solution,
-
-   >>> Numeric.allclose(Numeric.array(var), answer, atol = 1e-2)
-   1
-   >>> Numeric.allclose(Numeric.array(var), answer, atol = 1e-3)
-   1
-   >>> Numeric.allclose(Numeric.array(var), answer, atol = 1e-4)
-   0
-   >>> Numeric.allclose(Numeric.array(var), answer, atol = 1e-5)
-   0
 """
 __docformat__ = 'restructuredtext'
 
-import Numeric
-
-from fipy.boundaryConditions.fixedValue import FixedValue
-from fipy.boundaryConditions.fixedFlux import FixedFlux
-from fipy.boundaryConditions.nthOrderBoundaryCondition import NthOrderBoundaryCondition
-from fipy.iterators.iterator import Iterator
-from fipy.meshes.grid2D import Grid2D
-from fipy.solvers.linearPCGSolver import LinearPCGSolver
-from fipy.solvers.linearLUSolver import LinearLUSolver
-from fipy.variables.cellVariable import CellVariable
-from fipy.viewers.grid2DGistViewer import Grid2DGistViewer
-from fipy.models.cahnHilliard.cahnHilliardEquation import CahnHilliardEquation
-from fipy.equations.nthOrderDiffusionEquation import NthOrderDiffusionEquation
-
-L = 1.
+L = 1000.
 nx = 100
 ny = 100
-steps = 100
+
+steps = 1000
 dx = L / nx
 dy = L / ny
 
-parameters={
-    'asq' : 1.0,
-    'epsilon' : 1,
-    'diffusionCoeff' : 1}
-         
+asq = 1.0
+epsilon = 1
+diffusionCoeff = 1
 
+from fipy.meshes.grid2D import Grid2D
 mesh = Grid2D(dx, dy, nx, ny)
 
-##a = Numeric.sqrt(parameters['asq'])
-##answer = 1 / (1 + Numeric.exp(a * (mesh.getCellCenters()[:,0] - L / 2) / parameters['epsilon']))
 import random
+from fipy.variables.cellVariable import CellVariable
 var = CellVariable(
     name = "phase field",
     mesh = mesh,
@@ -124,65 +85,46 @@ var = CellVariable(
 
 ##var.setValue(1, cells = mesh.getCells(lambda cell: cell.getCenter()[0] > L / 2))
 
-eqch= CahnHilliardEquation(
-    var,
-    parameters = parameters,
-    solver = LinearPCGSolver(tolerance = 1e-15,steps = 100),
-    boundaryConditions=(
-    FixedFlux(mesh.getFacesRight(), 0),
-    FixedFlux(mesh.getFacesLeft(), 0),
-    NthOrderBoundaryCondition(mesh.getFacesLeft(), 0, 2),
-    NthOrderBoundaryCondition(mesh.getFacesRight(), 0, 2),
-    NthOrderBoundaryCondition(mesh.getFacesTop(), 0, 2),
-    NthOrderBoundaryCondition(mesh.getFacesBottom(), 0, 2)))
+faceVar = var.getArithmeticFaceValue()
+doubleWellDerivative = asq * ( 1 - 6 * faceVar * (1 - faceVar))
 
-it = Iterator((eqch,))
+from fipy.terms.nthOrderDiffusionTerm import NthOrderDiffusionTerm
+from fipy.terms.transientTerm import TransientTerm
+diffTerm2 = NthOrderDiffusionTerm(coeffs = (diffusionCoeff * doubleWellDerivative,))
+diffTerm4 = NthOrderDiffusionTerm(coeffs = (diffusionCoeff, -epsilon**2))
+eqch = TransientTerm() - diffTerm2 - diffTerm4
+
+from fipy.solvers.linearPCGSolver import LinearPCGSolver
+from fipy.solvers.linearLUSolver import LinearLUSolver
+solver = LinearLUSolver(tolerance = 1e-15,steps = 1000)
+
+from fipy.boundaryConditions.fixedValue import FixedValue
+from fipy.boundaryConditions.fixedFlux import FixedFlux
+from fipy.boundaryConditions.nthOrderBoundaryCondition import NthOrderBoundaryCondition
+BCs = (FixedFlux(mesh.getFacesRight(), 0),
+       FixedFlux(mesh.getFacesLeft(), 0),
+       NthOrderBoundaryCondition(mesh.getFacesLeft(), 0, 3),
+       NthOrderBoundaryCondition(mesh.getFacesRight(), 0, 3),
+       NthOrderBoundaryCondition(mesh.getFacesTop(), 0, 3),
+       NthOrderBoundaryCondition(mesh.getFacesBottom(), 0, 3))
 
 if __name__ == '__main__':
+    from fipy.viewers.grid2DGistViewer import Grid2DGistViewer
     viewer = Grid2DGistViewer(var, minVal=0., maxVal=1.0, palette = 'rainbow.gp')
     viewer.plot()
-    dexp=-5
 
-    a = Numeric.sqrt(parameters['asq'])
-    answer = 1 / (1 + Numeric.exp(-a * (mesh.getCellCenters()[:,0]) / parameters['epsilon']))
-    
-    for step in range(steps):      
-        dt=Numeric.exp(dexp)
-        dt = min(10,dt)
-        dexp=dexp+.5
-##        print 'in loop ',step,dt
+    dexp=-5
+    import Numeric
+    for step in range(steps):
+        dt = Numeric.exp(dexp)
+        dt = min(10, dt)
+        dexp += 0.5
         print 'step:',step,dt
-        it.timestep(dt = dt)
-        diff = abs(answer - Numeric.array(var))
-        maxarg = Numeric.argmax(diff)
-        print 'maximum error:',diff[maxarg]
-        print 'element id:',maxarg
-        print 'value at element ',maxarg,' is ',var[maxarg]
-        
-        
+        var.updateOld()
+        eqch.solve(var, boundaryConditions = BCs, solver = solver, dt = dt)
+
         viewer.plot()
         
-        
-##    a = Numeric.sqrt(parameters['asq'])
-##    answer = 1 / (1 + Numeric.exp(a * (mesh.getCellCenters()[:,0] - L / 2) / parameters['epsilon']))
-
-##    from fipy.viewers.gist1DViewer import Gist1DViewer
-##    view1Dvar = Gist1DViewer(vars = (var,), minVal = 0., maxVal = 1.)
-##    view1Ddiff = Gist1DViewer(vars = (var - answer,), minVal = -1e-3, maxVal = 1e-3)
-
-##    view1Dvar.plot()
-##    view1Ddiff.plot()
-    
-##    diff = answer - Numeric.array(var)
-##    maxarg = Numeric.argmax(diff)
-##    print diff[maxarg]
-##    print maxarg
-##    aa = Numeric.argsort(diff)
-##    print diff[aa[nx-1]],diff[aa[nx-2]],aa[nx-1],aa[nx-2]
-##    print diff[800:840]
-##    print answer[800:840]
-
-    
     raw_input('finished')
 
 
