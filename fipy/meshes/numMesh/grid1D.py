088f9429180e02d1bfb050117ab553e7c92ac40c
Jonathan E. Guyer <guyer@nist.gov>
2005-03-07 21:22:33 +0000

    True 1D mesh class
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@1209 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/meshes/numMesh/grid1D.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+## -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "grid1D.py"
+ #                                    created: 11/10/03 {3:30:42 PM} 
+ #                                last update: 3/7/05 {8:43:02 AM} 
+ #  Author: Jonathan Guyer <guyer@nist.gov>
+ #  Author: Daniel Wheeler <daniel.wheeler@nist.gov>
+ #  Author: James Warren   <jwarren@nist.gov>
+ #    mail: NIST
+ #     www: http://www.ctcms.nist.gov/fipy/
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  FiPy is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2003-11-10 JEG 1.0 original
+ # ###################################################################
+ ##
+
+"""1D rectangular Mesh
+"""
+
+import Numeric
+
+from fipy.meshes.numMesh.mesh1D import Mesh1D
+from fipy.meshes.numMesh.face import Face
+from fipy.tools import vector
+from fipy.tools.dimensions.physicalField import PhysicalField
+
+class Grid1D(Mesh1D):
+    """
+    Creates a 1D grid mesh. Vertices and cells are numbered 
+    in the usual way.
+    """
+    def __init__(self, dx = 1., nx = 1):
+        self.nx = nx
+        
+        self.dx = PhysicalField(value = dx)
+        scale = PhysicalField(value = 1, unit = self.dx.getUnit())
+        self.dx /= scale
+        
+        self.numberOfVertices = self.nx + 1
+        
+        vertices = self.createVertices()
+        faces = self.createFaces()
+        cells = self.createCells()
+        Mesh1D.__init__(self, vertices, faces, cells)
+        
+        self.setScale(value = scale)
+        
+    def createVertices(self):
+        x = Numeric.arange(self.numberOfVertices) * self.dx
+        return Numeric.transpose(Numeric.array((x,)))
+    
+    def createFaces(self):
+        return Numeric.arange(self.numberOfVertices)
+
+    def createCells(self):
+        """
+        cells = (f1, f2) going left to right.
+        f1 etc. refer to the faces
+        """
+        self.numberOfFaces = self.nx + 1
+        f1 = Numeric.arange(self.nx)
+        f2 = f1 + 1
+        return Numeric.transpose(Numeric.array((f1, f2)))
+
+    def getDim(self):
+        return 1
+        
+    def getFacesLeft(self):
+        """Return face on left boundary of Grid1D as list.
+        """
+        return [Face(self, 0)]
+        
+    def getFacesRight(self):
+        """Return face on right boundary of Grid1D as list.
+        """
+        return [Face(self, self.numberOfFaces - 1)]
+        
+    def getScale(self):
+        return self.scale['length']
+        
+    def getPhysicalShape(self):
+        """Return physical dimensions of Grid1D.
+        """
+        return PhysicalField(value = (self.nx * self.dx * self.getScale(),))
+
+    def getMeshSpacing(self):
+        return Numeric.array((self.dx,))
+    
+    def getShape(self):
+        return (self.nx,)
+    
+## pickling
+
+    def __getstate__(self):
+        dict = {
+            'dx' : self.dx,            
+            'nx' : self.nx}
+        return dict
+
+    def __setstate__(self, dict):
+        self.__init__(dx = dict['dx'], nx = dict['nx'])
+
---
+++ fipy/meshes/numMesh/mesh1D.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+## 
+ # -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "mesh1D.py"
+ #                                    created: 11/10/03 {2:44:42 PM} 
+ #                                last update: 3/7/05 {8:42:47 AM} 
+ #  Author: Jonathan Guyer <guyer@nist.gov>
+ #  Author: Daniel Wheeler <daniel.wheeler@nist.gov>
+ #  Author: James Warren   <jwarren@nist.gov>
+ #    mail: NIST
+ #     www: http://www.ctcms.nist.gov/fipy/
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  FiPy is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  See the file "license.terms" for information on usage and  redistribution
+ #  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ #  
+ # ###################################################################
+ ##
+
+"""Generic mesh class using Numeric to do the calculations
+
+    Meshes contain cells, faces, and vertices.
+
+    This is built for a non-mixed element mesh.
+"""
+
+import Numeric
+import MA
+
+from fipy.meshes.numMesh.mesh import Mesh
+from fipy.tools import vector
+
+class Mesh1D(Mesh):
+    def calcFaceAreas(self):
+        self.faceAreas = Numeric.ones(self.numberOfFaces, 'd')
+
+    def calcFaceCenters(self):
+        faceVertexIDs = MA.filled(self.faceVertexIDs, 0)
+        faceVertexCoords = Numeric.take(self.vertexCoords, faceVertexIDs)
+        if self.faceVertexIDs.mask() == None:
+            faceVertexCoordsMask = Numeric.zeros(Numeric.shape(faceVertexCoords))
+        else:
+            faceVertexCoordsMask = Numeric.reshape(Numeric.repeat(self.faceVertexIDs.mask().flat, self.dim), Numeric.shape(faceVertexCoords))
+            
+        self.faceCenters = MA.array(data = faceVertexCoords, mask = faceVertexCoordsMask)
+        
+    def calcFaceNormals(self):
+        self.faceNormals = Numeric.transpose(Numeric.array((Numeric.ones(self.numberOfFaces, 'd'),)))
+
+    def calcFaceTangents(self):
+        self.faceTangents1 = Numeric.zeros(self.numberOfFaces, 'd')
+        self.faceTangents2 = Numeric.zeros(self.numberOfFaces, 'd')
+
+    def calcHigherOrderScalings(self):
+	self.scale['area'] = 1.
+	self.scale['volume'] = self.scale['length']
+
+    def translate(self, vector):
+        newCoords = self.vertexCoords + vector
+        newmesh = Mesh1D(newCoords, Numeric.array(self.faceVertexIDs), Numeric.array(self.cellFaceIDs))
+        return newmesh
+
+    def dilate(self, factor):
+        newCoords = self.vertexCoords * factor
+        newmesh = Mesh1D(newCoords, Numeric.array(self.faceVertexIDs), Numeric.array(self.cellFaceIDs))
+        return newmesh
+
+    def meshAdd(self, other, smallNumber):
+        a = self.getAddedMeshValues(other, smallNumber)
+        return Mesh1D(a[0], a[1], a[2])
