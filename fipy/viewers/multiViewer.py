8705de072509ce0f1088af3cb734cb6beb75f364
Jonathan E. Guyer <guyer@nist.gov>
2006-03-08 20:11:58 +0000

    Wrapper for multiple viewers
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@1713 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/viewers/multiViewer.py
@@ -0,0 +1,59 @@
+## -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - a finite volume PDE solver in Python
+ # 
+ #  FILE: "multiViewer.py"
+ #                                    created: 12/17/05 {6:28:03 PM} 
+ #                                last update: 12/17/05 {6:33:32 PM} 
+ #  Author: Jonathan Guyer
+ #  E-mail: guyer@nist.gov
+ #    mail: NIST
+ #     www: http://www.ctcms.nist.gov/fipy/
+ #  
+ # ========================================================================
+ # This document was prepared at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this document is not subject to copyright
+ # protection and is in the public domain.  multiViewer.py
+ # is an experimental work.  NIST assumes no responsibility whatsoever
+ # for its use by other parties, and makes no guarantees, expressed
+ # or implied, about its quality, reliability, or any other characteristic.
+ # We would appreciate acknowledgement if the document is used.
+ # 
+ # This document can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  See the file "license.terms" for information on usage and  redistribution
+ #  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ #  
+ #  Description: 
+ # 
+ #  History
+ # 
+ #  modified   by  rev reason
+ #  ---------- --- --- -----------
+ #  2005-12-17 JEG 1.0 original
+ # ###################################################################
+ ##
+
+from fipy.viewers.viewer import Viewer
+
+class _MultiViewer(Viewer):
+    def __init__(self, viewers):
+        if type(viewers) not in [type([]), type(())]:
+            viewers = [viewers]
+        self.viewers = viewers
+
+    def setLimits(self, limits):
+        for viewer in self.getViewers():
+            viewer.setLimits(limits)
+            
+    def plot(self):
+        for viewer in self.getViewers():
+            viewer.plot()
+            
+    def getViewers(self):
+        return self.viewers
