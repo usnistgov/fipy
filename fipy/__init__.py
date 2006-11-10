0cd2ccc17eb6c2ccab74c1ae3ae2a62d67f09413
Jonathan E. Guyer <guyer@nist.gov>
2006-11-10 21:26:34 +0000

    Changed "iterators" to "steppers"
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@1947 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/__init__.py
+++ fipy/__init__.py
@@ -3,6 +3,7 @@ __version__ = "1.2"
 from boundaryConditions import *
 from meshes import *
 from solvers import *
+from steppers import *
 from terms import *
 from tools import *
 from variables import *
