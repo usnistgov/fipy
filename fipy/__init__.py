d1356836733337fc35979361c57e1c57081f7eda
Jonathan E. Guyer <guyer@nist.gov>
2007-03-15 17:32:25 +0000

    Fix for ticket:83
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@2050 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/__init__.py
+++ fipy/__init__.py
@@ -1,6 +1,12 @@
 import os
 execfile(os.path.join(__path__[0], '__version__.py'))
 
+f = file(os.path.join(os.path.split(__path__[0])[0], 'README.txt'))
+__doc__ = f.read()
+f.close()
+del f
+__docformat__ = 'restructuredtext'
+
 from boundaryConditions import *
 from meshes import *
 from solvers import *
