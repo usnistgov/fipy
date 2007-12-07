e678b861548545f10469bc1855544549ca460fd0
Jonathan E. Guyer <guyer@nist.gov>
2007-12-07 21:36:34 +0000

    Cleanup and fixing from prototype code
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/variableMeshes@2448 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/meshes/numMesh/movingMesh.py
+++ fipy/meshes/numMesh/movingMesh.py
@@ -1,11 +1,48 @@
 #!/usr/bin/env python
 
+## -*-Pyth-*-
+ # #############################################################################
+ # FiPy - a finite volume PDE solver in Python
+ # 
+ # FILE: "movingMesh.py"
+ #                                     created: 11/25/07 {6:52:31 PM}
+ #                                 last update: 12/6/07 {5:55:29 PM}
+ # Author: Jonathan Guyer
+ # E-mail: <jguyer@his.com>
+ #   mail: Alpha Cabal
+ #    www: <http://alphatcl.sourceforge.net>
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  FiPy is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are 
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ # 
+ # History
+ # 
+ # modified   by  rev reason
+ # ---------- --- --- -----------
+ # 1941-11-23 JEG 1.0 original
+ # 
+ # #############################################################################
+ ##
 
 from fipy.tools import numerix, vector
 
-from fipy.meshes.numMesh.mesh import Mesh
-from fipy.meshes.numMesh.mesh1D import Mesh1D
-from fipy.meshes.numMesh.mesh2D import Mesh2D
+from mesh import Mesh
+from mesh1D import Mesh1D
+from mesh2D import Mesh2D
 
 from fipy.variables.cellVariable import CellVariable
 
@@ -58,17 +95,21 @@ def MovingMesh(mesh):
 
                 monitor = 0.
                 lagrangianVar = CellVariable(mesh=lmesh, value=0.)
-##                 magGradSqrt = lagrangianVar.getGrad().getMag().sqrt()
-                magGrad = lagrangianVar.getGrad().getMag()
+                magGradSqrt = lagrangianVar.getGrad().getMag().sqrt()
+##                 magGrad = lagrangianVar.getGrad().getMag()
                 for var in monitorVariables:
                     lagrangianVar.setValue(var.getValue())
 ##                     monitor += ((1.-beta) * (magGradSqrt.getCellVolumeAverage() 
 ##                                              * lmesh.getNumberOfCells())
 ##                                 + beta * magGradSqrt)
-                    monitor += ((1.-beta) * (magGrad.getCellVolumeAverage() 
-                                             * lmesh.getNumberOfCells())
-                                + beta * magGrad)
-                
+##                     monitor += ((1.-beta) * (magGrad.getCellVolumeAverage() 
+##                                              * lmesh.getNumberOfCells())
+##                                 + beta * magGrad)
+##                     monitor += ((1.-beta) * magGrad.getCellVolumeAverage() 
+##                                 + beta * magGrad)
+                    monitor += ((1.-beta) * magGradSqrt.getCellVolumeAverage() 
+                                + beta * magGradSqrt)
+
                 
                 from fipy.terms.transientTerm import TransientTerm
                 from fipy.terms.diffusionTerm import DiffusionTerm
@@ -109,12 +150,16 @@ def MovingMesh(mesh):
 
                 displacmentLInfNorm = max(displacementMag)
 
-                print "max displacement:", displacmentLInfNorm
+                print sweep, "max displacement:", displacmentLInfNorm
                 
                 if displacmentLInfNorm < 1e-8:
                     break
                     
                 self.vertexCoords[:] = newVertexCoords
+                
+                from fipy.terms.implicitSourceTerm import ImplicitSourceTerm
+                from fipy.terms.upwindConvectionTerm import UpwindConvectionTerm
+                from fipy.solvers import LinearLUSolver
                 
                 for var in interpolants: #self.getSubscribedVariables():
 ##                     var = var()
@@ -127,114 +172,7 @@ def MovingMesh(mesh):
                         while res > 1e-12:
                             res = updateEq.sweep(var=var, solver=LinearLUSolver(tolerance=1.e-15, iterations=2000))
     ##                         res = updateEq.sweep(var=var, solver=LinearCGSSolver())
-                            print "%s move residual: %g" % (var.name, res)
+                            print "%d: %s move residual: %g" % (sweep, var.name, res)
 
     return _MovingMesh(mesh)
     
-
-from fipy import *
-
-from fipy.meshes.numMesh.grid2D import Grid2D as NonUniformGrid2D
-from fipy.meshes.numMesh.grid1D import Grid1D as NonUniformGrid1D
-
-dx = 5e-5 # cm
-## nx = 40
-## dx = 2e-5 # cm
-nx = 40
-L = nx * dx
-ny = 40
-dy = dx
-
-mesh1 = Grid2D(nx=nx, ny=ny, dx=dx, dy=dy)
-mesh2 = NonUniformGrid2D(nx=nx, ny=ny, dx=dx, dy=dy)
-mesh3 = MovingMesh(mesh1)
-mesh4 = NonUniformGrid1D(nx=nx, dx=dx)
-mesh = MovingMesh(mesh2)
-
-
-x, y = mesh.getCellCenters()
-## x,  = mesh.getCellCenters()
-
-phase = CellVariable(name="phase", mesh=mesh, hasOld=1)
-phase.setValue(1.)
-phase.setValue(0., where=(x > L/2) & (y > L/2))
-
-Lv = 2350 # J / cm**3
-Tm = 1728. # K
-T = Variable(value=Tm)
-enthalpy = Lv * (T - Tm) / Tm # J / cm**3
-
-delta = 1.5 * dx
-sigma = 3.7e-5 # J / cm**2
-beta = 0.33 # cm / (K s)
-kappa = 6 * sigma * delta # J / cm
-W = 6 * sigma / delta # J / cm**3
-Mphi = Tm * beta / (6. * Lv * delta) # cm**3 / (J s)
-
-
-
-mPhi = -((1 - 2 * phase) * W + 30 * phase * (1 - phase) * enthalpy)
-dmPhidPhi = 2 * W - 30 * (1 - 2 * phase) * enthalpy
-S1 = dmPhidPhi * phase * (1 - phase) + mPhi * (1 - 2 * phase)
-S0 = mPhi * phase * (1 - phase) - S1 * phase
-eq = TransientTerm(coeff=1/Mphi) == ImplicitDiffusionTerm(coeff=kappa) \
-                        + S0 + ImplicitSourceTerm(coeff = S1)
-
-
-## rho = CellVariable(mesh=mesh, 
-##                    value=1 + 10. * exp(-50 * (y - 1./2 - 1./4 * sin(2*pi*x))**2))
-
-## t = 0.75
-## rho = CellVariable(mesh=mesh, 
-##                    name="rho",
-##                    value=1 + 10. * exp(-50 * abs((x - 1./2 - 1./4 * cos(2*pi*t))**2
-##                                                  + (y - 1./2 - 1./4 * sin(2*pi*t))**2
-##                                                  - (1./10)**2)))
-
-## TSVViewer(vars=(rho, rho.getGrad().dot(rho.getGrad()), mesh.getCellVolumes())).plot("test0.txt")
-## TSVViewer(vars=(phase, mesh.getCellVolumes())).plot("test%d-%d.txt" % (0, 0))
-
-print "0-0", phase.getCellVolumeAverage()
-
-timeStep = 1e-6
-for step in range(10):
-    
-    TSVViewer(vars=(phase, mesh.getCellVolumes())).plot("test%d-before.txt" % step)
-
-    mesh.move(monitorVariables=(phase,), interpolants=(phase,), beta=0.99)
-
-    TSVViewer(vars=(phase, mesh.getCellVolumes())).plot("test%d-after.txt" % step)
-
-    print step, phase.getCellVolumeAverage()
-        
-    phase.updateOld()
-    res = 1e+10
-    while res > 1e-9:
-        res = eq.sweep(var=phase, dt=timeStep)
-        print "solve residual:", res
-
-
-T.setValue(T() - 1)
-
-velocity = beta * abs(Tm - T()) # cm / s
-timeStep = .1 * dx / velocity # s
-elapsed = 0
-while elapsed < 0.1 * L / velocity:
-    step += 1
-    
-    TSVViewer(vars=(phase, mesh.getCellVolumes())).plot("test%d-before.txt" % step)
-
-    mesh.move(monitorVariables=(phase,), interpolants=(phase,), beta=0.99)
-
-    TSVViewer(vars=(phase, mesh.getCellVolumes())).plot("test%d-after.txt" % step)
-
-    print step, phase.getCellVolumeAverage()
-
-    
-    phase.updateOld()
-    res = 1e+10
-    while res > 1e-9:
-        res = eq.sweep(var=phase, dt=timeStep)
-        print "solve residual:", res
-    elapsed += timeStep
-
