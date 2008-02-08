c259fec395954c512be49577b2dde3483b089b29
Jonathan E. Guyer <guyer@nist.gov>
2008-02-08 18:32:55 +0000

    cleaning up tests
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/version-1_2@2464 d80e17d7-ff13-0410-a124-85740d801063

--- grid2DTest.py
+++
@@ -1,9 +0,0 @@
-from fipy import *
-
-mesh3 = Grid2D(dx=(0.25, 0.75, 1.), dy=(0.1, 1.8, 0.1))
-
-var3 = VectorFaceVariable(mesh=mesh3, value=mesh3._getOrientedFaceNormals())
-x = mesh3.getCellCenters()[...,0]
-var4 = CellVariable(mesh=mesh3, value=x)
-
-TSVViewer(vars=(var4.getFaceGrad(), var3)).plot("Grid2D.txt")
