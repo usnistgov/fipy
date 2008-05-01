1a8713320ff6038f8e9e8662cc637389182aa27b
Jonathan E. Guyer <guyer@nist.gov>
2008-05-01 19:11:03 +0000

    Depending on the viewers available, this example could display a viewer even when `__name__ != '__main__'`
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@2507 d80e17d7-ff13-0410-a124-85740d801063

--- examples/diffusion/anisotropy.py
+++ examples/diffusion/anisotropy.py
@@ -6,7 +6,7 @@
  # 
  #  FILE: "circle.py"
  #                                    created: 4/6/06 {11:26:11 AM}
- #                                last update: 10/5/07 {10:49:43 AM}
+ #                                last update: 5/1/08 {3:25:37 PM}
  #  Author: Jonathan Guyer <guyer@nist.gov>
  #  Author: Daniel Wheeler <daniel.wheeler@nist.gov>
  #  Author: James Warren   <jwarren@nist.gov>
@@ -115,7 +115,8 @@ Make the equation, viewer and solve.
 
     >>> eqn = TransientTerm() == DiffusionTerm((gamma,))
 
-    >>> viewer = make(var, limits={'datamin' : 0.0, 'datamax' : 0.001})
+    >>> if __name__ == '__main__':
+    ...     viewer = make(var, limits={'datamin' : 0.0, 'datamax' : 0.001})
 
     >>> mass = float(numerix.sum(mesh.getCellVolumes() * var))
     >>> time = 0
