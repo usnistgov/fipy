6daa8402e7826ab84c9743035bc5a499f51e6812
Jonathan E. Guyer <guyer@nist.gov>
2008-11-21 02:02:52 +0000

    typo
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@2833 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/solvers/test.py
+++ fipy/solvers/test.py
@@ -36,8 +36,8 @@ from fipy.tests.lateImportTest import _LateImportTestSuite
 import fipy.tests.testProgram
 
 def _suite():
-    return _LateImportTestSuite(testModuleNames = (,
-    ), base = __name__)
+    return _LateImportTestSuite(testModuleNames = (), 
+                                base = __name__)
     
 if __name__ == '__main__':
     fipy.tests.testProgram.main(defaultTest='_suite')
