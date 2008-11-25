0a358e3c659b4c72456c16f7497964950ec027dc
Daniel Wheeler <daniel.wheeler@nist.gov>
2008-11-25 17:08:11 +0000

    removed comments
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@2858 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/meshes/numMesh/gmshImport.py
+++ fipy/meshes/numMesh/gmshImport.py
@@ -412,15 +412,10 @@ class MshFile:
                 self.deletegeofile = False
                 geofile = arg
                 
-##            import sys
-##            if sys.platform == 'win32':
-##                self.mshfile = 'tmp.msh'
-##            else:
             (f, self.mshfile) = tempfile.mkstemp('.msh')
 
             os.system('gmsh ' + geofile + ' -2 -v 0 -format msh -o ' + self.mshfile)
 
-##            if sys.platform != 'win32':
             os.close(f)
 
             if self.deletegeofile:
