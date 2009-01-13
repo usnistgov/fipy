1482ebbcb8a99f52022160ffd7fcc1696f2d06bd
Daniel Wheeler <daniel.wheeler@nist.gov>
2009-01-13 22:49:45 +0000

    - Added Angus Hendrick's changes to read the inline environment flag - Changed AMINISTRATA to reflect bash shell scripting
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@2950 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/tools/inline/__init__.py
+++ fipy/tools/inline/__init__.py
@@ -3,4 +3,14 @@ import sys
 if '--inline' in sys.argv[1:]:
     inlineFlagOn = True
 else:
-    inlineFlagOn = False
+    import os
+
+    if os.environ.has_key('FIPY_INLINE'):
+        if os.environ['FIPY_INLINE'].lower() == 'true':
+            inlineFlagOn = True
+        elif os.environ['FIPY_INLINE'].lower() == 'false':
+            inlineFlagOn = False
+        else:
+            raise ImportError, 'Unknown setting for FIPY_INLINE: %s, should be true or false' % os.environ['FIPY_INLINE']
+    else:
+        inlineFlagOn = False
