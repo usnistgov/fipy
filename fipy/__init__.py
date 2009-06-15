a82a86ec38b3f32298a1e62faec59728d374ac52
Jonathan E. Guyer <guyer@nist.gov>
2009-06-15 18:15:44 +0000

    attempt at mpi-aware raw_input()
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/parallel@3079 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/__init__.py
+++ fipy/__init__.py
@@ -15,3 +15,23 @@ from tools import *
 from variables import *
 from viewers import *
 from models import *
+
+try:
+    from PyTrilinos import Epetra
+    
+    if Epetra.PyComm().NumProc() > 1:
+        raw_input_original = raw_input
+        def mpi_raw_input(prompt):
+            import sys
+            Epetra.PyComm().Barrier()
+            sys.stdout.flush()
+            if Epetra.PyComm().MyPID() == 0:
+                sys.stdout.write(prompt)
+                sys.stdout.flush()
+                return sys.stdin.readline()
+            else:
+                return ""
+        raw_input = mpi_raw_input
+
+except ImportError:
+    pass
