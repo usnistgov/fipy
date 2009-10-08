7f5f3cba542370dbbbe52fa64baba9ac7e83d1e7
Jonathan E. Guyer <guyer@nist.gov>
2009-10-08 19:39:30 +0000

    All of daemon is now an object (to allow easier override)
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/mayavi@3246 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/viewers/mayaviViewer/mayaviDaemon.py
+++ fipy/viewers/mayaviViewer/mayaviDaemon.py
@@ -25,42 +25,45 @@ this script to point to other data which you can edit.
 # License: BSD Style.
 
 # Standard imports.
-from mmap import mmap
 import os
-from os.path import join, abspath, dirname
 import sys
 
 # Enthought library imports
 from enthought.mayavi.scripts import mayavi2
 from enthought.mayavi.sources.vtk_file_reader import VTKFileReader
-from enthought.mayavi.modules.outline import Outline
 from enthought.pyface.timer.api import Timer
 
 
 ######################################################################
-# `Pollster` class.
-class Pollster(object):
+class MayaviDaemon(object):
     """Given a file name and a mayavi2 data reader object, this class
     polls the file for any changes and automatically updates the
     mayavi pipeline.
     """
-    def __init__(self, fname, cd, fd):
+    def __init__(self, cellfname, facefname, lockfname):
         """Initialize the object.
 
         Parameters:
         -----------
-        fname -- filename to poll.
-        cd -- the MayaVi cell source object to update.
-        fd -- the MayaVi face source object to update.
+        cellfname -- VTK file holding cell data.
+        facefname -- VTK file holding face data.
+        lockfname -- filename to poll.
         """
-        self.fname = fname
-        self.celldata = cd
-        self.facedata = fd
+        self.cellfname = cellfname
+        self.facefname = facefname
+        self.lockfname = lockfname
+        
+        self.setup_data()
+        self.view_data()
+
+        # Poll the lock file.
+        self.timer = Timer(1000, self.poll_file)
+
 
     def poll_file(self):
-        if os.path.isfile(self.fname):
+        if os.path.isfile(self.lockfname):
             self.update_pipeline()
-            os.unlink(self.fname)
+            os.unlink(self.lockfname)
 
     def update_pipeline(self):
         """Override this to do something else if needed.
@@ -79,66 +82,52 @@ class Pollster(object):
         # Propagate the changes in the pipeline.
         fd.data_changed = True
 
+    def setup_data(self):
+        """Given a VTK file name `fname`, this creates a mayavi2 reader
+        for it and adds it to the pipeline.  It returns the reader
+        created.
+        """
+        # 'mayavi' is always defined on the interpreter.
+        mayavi.new_scene()
+        
+        self.celldata = VTKFileReader()
+        self.celldata.initialize(self.cellfname)
+        self.cellsource = mayavi.add_source(self.celldata)
+        
+        self.facedata = VTKFileReader()
+        self.facedata.initialize(self.facefname)
+        self.facesource = mayavi.add_source(self.facedata)
 
-                       
-def setup_data(cellfname, facefname):
-    """Given a VTK file name `fname`, this creates a mayavi2 reader
-    for it and adds it to the pipeline.  It returns the reader
-    created.
-    """
-    # 'mayavi' is always defined on the interpreter.
-    mayavi.new_scene()
-    
-    cd = VTKFileReader()
-    cd.initialize(cellfname)
-    cs = mayavi.add_source(cd)
-    
-    fd = VTKFileReader()
-    fd.initialize(facefname)
-    fs = mayavi.add_source(fd)
-    
-    return cs, cd, fs, fd
+    def view_data(self):
+        """Sets up the mayavi pipeline for the visualization.
+        """
+        # 'mayavi' is always defined on the interpreter.
+        
+        from enthought.mayavi import mlab
+        o = mlab.pipeline.outline(self.cellsource)
+        s = mlab.pipeline.surface(self.cellsource) #,extent=extent,vmin=datamin,vmax=datamax)
+        s.module_manager.scalar_lut_manager.show_scalar_bar = True
+        p = mlab.pipeline.cell_to_point_data(self.cellsource)
+        v = mlab.pipeline.vectors(p)
 
-def view_data(cs, fs):
-    """Sets up the mayavi pipeline for the visualization.
-    """
-    # 'mayavi' is always defined on the interpreter.
-#     o = Outline()
-#     mayavi.add_module(o)
-    
-    from enthought.mayavi import mlab
-    o = mlab.pipeline.outline(cs)
-    s = mlab.pipeline.surface(cs) #,extent=extent,vmin=datamin,vmax=datamax)
-    s.module_manager.scalar_lut_manager.show_scalar_bar = True
-    p = mlab.pipeline.cell_to_point_data(cs)
-    v = mlab.pipeline.vectors(p)
-
-    s = mlab.pipeline.surface(fs) #,extent=extent,vmin=datamin,vmax=datamax)
-#     s.module_manager.scalar_lut_manager.show_scalar_bar = True
-    p = mlab.pipeline.cell_to_point_data(fs)
-    v = mlab.pipeline.vectors(p)
+        s = mlab.pipeline.surface(self.facesource) #,extent=extent,vmin=datamin,vmax=datamax)
+    #     s.module_manager.scalar_lut_manager.show_scalar_bar = True
+        p = mlab.pipeline.cell_to_point_data(self.facesource)
+        v = mlab.pipeline.vectors(p)
 
 @mayavi2.standalone
 def main():
     # Change this to suit your needs.  Edit the file after running this
     # script and the pipeline should be updated automatically.
 
-    vtkcellfname = sys.argv[1]
-    vtkfacefname = sys.argv[2]
-    vtklockfname = sys.argv[3]
+    cellfname = sys.argv[1]
+    facefname = sys.argv[2]
+    lockfname = sys.argv[3]
     
-    
-    cs, cd, fs, fd = setup_data(vtkcellfname, vtkfacefname)
-    view_data(cs, fs)
-
-    # Poll the file.
-    p = Pollster(vtklockfname, cd, fd)
-    timer = Timer(1000, p.poll_file)
-
-    mayavi.fipytimer = timer
+    mayavi.fipydaemon = MayaviDaemon(cellfname, facefname, lockfname)
     
     # To stop polling the file do:
-    #timer.Stop()
+    #mayavi.fipydaemon.timer.Stop()
 
 if __name__ == '__main__':
     main()
