1a012f1124f944644b651d355cb3a48c44fdb908
Jonathan E. Guyer <guyer@nist.gov>
2010-03-16 17:40:12 +0000

    more adjustments to layout, particularly in sidebar
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/git-sphinx@3452 d80e17d7-ff13-0410-a124-85740d801063

--- documentation/conf.py
+++ documentation/conf.py
@@ -144,12 +144,12 @@ html_theme_path = ['_themes']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_static/logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -166,7 +166,8 @@ html_last_updated_fmt = '%b %d, %Y'
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
-html_sidebars = {'index': ['indexsidebar.html', 'searchbox.html']}
+html_sidebars = {'index': ['index.html', 'searchbox.html', 'contact.html'],
+                 '**': ['localtoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html', 'contact.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
