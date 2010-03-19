6ef1d41d7020197e4976219b4742cb2d98365950
Jonathan E. Guyer <guyer@nist.gov>
2010-03-19 20:37:34 +0000

    test "failures" illuminated (or caused) by sphinxification
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/version-2_0@3491 d80e17d7-ff13-0410-a124-85740d801063

--- examples/phase/anisotropyOLD.py
+++ examples/phase/anisotropyOLD.py
@@ -226,7 +226,7 @@ the data and compares it with the `phase` variable.
 >>> import os
 >>> testData = loadtxt(os.path.splitext(__file__)[0] + '.gz')
 >>> print phase.allclose(testData)
-1   
+1
 """
 __docformat__ = 'restructuredtext'
 
