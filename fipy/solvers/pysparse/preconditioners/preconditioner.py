7a336297d3431fbe8ad0e8f19332ccec088bd7b1
James O'Beirne <james.obeirne@gmail.com>
2010-06-02 15:56:08 +0000

    adding Pysparse preconditioners
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/pysparse_precon@3556 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/solvers/pysparse/preconditioners/preconditioner.py
@@ -0,0 +1,25 @@
+class Preconditioner:
+    """
+    Base preconditioner class
+
+    .. attention :: This class is abstract. Always
+    create one of its subclasses.
+    """
+
+    def __init__(self):
+        """
+        Create a `Preconditioner` object.
+        """
+        if self.__class__ is Preconditioner:
+            raise NotImplementedError, \
+                  "can't instantiate abstract base class"
+
+    def _applyToMatrix(self, matrix):
+        """
+        Returns the function used for PySparse
+        preconditioning.
+        """
+        raise NotImplementedError
+        
+
+
---
+++ fipy/solvers/pysparse/preconditioners/ssorPreconditioner.py
@@ -0,0 +1,15 @@
+from fipy.solvers.pysparse.preconditioners.preconditioner import Preconditioner
+from pysparse import precon
+
+class SsorPreconditioner(Preconditioner):
+    """
+    SSOR preconditioner for PySparse.
+    Really just a wrapper class for pysparse.precon.jacobi.
+    """
+    def _applyToMatrix(self, A):
+        """
+        Returns (preconditioning matrix, resulting matrix)
+        """
+        A = A.to_sss()
+        return precon.ssor(A), A
+
