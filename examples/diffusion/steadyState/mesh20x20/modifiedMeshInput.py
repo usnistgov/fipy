3409e21744a7caf6f0a87a762aaac809ed57c983
James O'Beirne <james.obeirne@gmail.com>
2010-06-15 13:54:31 +0000

    absorbed GEO file into code.
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/gmshImport@3578 d80e17d7-ff13-0410-a124-85740d801063

--- examples/diffusion/steadyState/mesh20x20/modifiedMeshInput.py
+++ examples/diffusion/steadyState/mesh20x20/modifiedMeshInput.py
@@ -67,7 +67,24 @@ valueLeft = 0.
 valueRight = 1.
 
 import os.path
-mesh = GmshImporter2D(os.path.join(os.path.split(__file__)[0], 'modifiedMesh.geo'))
+mesh = GmshImporter2D("""
+    cellSize = 0.5;
+
+    Point(2) = {0, 0, 0, cellSize};
+    Point(3) = {20, 0, 0, cellSize};
+    Point(4) = {20, 20, 0, cellSize};
+    Point(5) = {0, 20, 0, cellSize};
+
+    Line(6) = {2, 3};
+    Line(7) = {3, 4};
+    Line(8) = {4, 5};
+    Line(9) = {5, 2};
+
+    Line Loop(10) = {6, 7, 8, 9};
+
+    Plane Surface(11) = {10};
+""")
+
 
 ##    "%s/%s" % (sys.__dict__['path'][0], "examples/diffusion/steadyState/mesh20x20/modifiedMesh.msh"))
 
