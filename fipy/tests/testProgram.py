6e5c81cf65ef81c6faf6df2b6769f57a2a3e2dff
Daniel Wheeler <daniel.wheeler@nist.gov>
2010-08-05 22:23:59 +0000

    added the 'pysparse', 'trilinos' and 'no-pysparse' command line arguments when running the tests outside of setup.py
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/divorcePysparse@3784 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/tests/testProgram.py
+++ fipy/tests/testProgram.py
@@ -43,7 +43,7 @@ class _TestProgram(unittest.TestProgram):
 ##        numMesh = 0
         try:
             options, args = getopt.getopt(argv[1:], 'hHvq',
-                                          ['help','verbose','quiet','inline', 'Trilinos', 'Pysparse'])
+                                          ['help','verbose','quiet','inline', 'Trilinos', 'Pysparse', 'pysparse', 'trilinos', 'no-pysparse'])
             for opt, value in options:
                 if opt in ('-h','-H','--help'):
                     self.usageExit()
