067a12685f68334827fe7eab022ed0287d30a167
Daniel Wheeler <daniel.wheeler@nist.gov>
2010-11-02 21:27:41 +0000

    added test to make sure pysparse actually stores the zeros
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/efficiency@3937 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/matrices/pysparseMatrix.py
+++ fipy/matrices/pysparseMatrix.py
@@ -359,6 +359,26 @@ class _PysparseMeshMatrix(_PysparseMatrix):
         if (not hasattr(self, 'cache')) or (self.cache is False):
             del self.matrix
 
+    def _test(self):
+        """
+        Tests
+        
+        >>> m = _PysparseMatrix(size=3, storeZeros=True)
+        >>> m.addAt((1., 0., 2.), (0, 2, 1), (1, 2, 0))
+        >>> print numerix.allequal(m.matrix.keys(), [(0, 1), (1, 0), (2, 2)]) or not hasattr(m.matrix, 'storeZeros')
+        True
+        >>> print numerix.allequal(m.matrix.values(), [1., 2., 0.]) or not hasattr(m.matrix, 'storeZeros')
+        True
+        >>> m = _PysparseMatrix(size=3, storeZeros=False)
+        >>> m.addAt((1., 0., 2.), (0, 2, 1), (1, 2, 0))
+        >>> print m.matrix.keys()
+        [(0, 1), (1, 0)]
+        >>> print m.matrix.values()
+        [1.0, 2.0]
+
+        """
+        pass
+        
 class _PysparseIdentityMatrix(_PysparseMatrix):
     """
     Represents a sparse identity matrix for pysparse.
