272bb828817ce8f17782e2228fb94f8174a44a77
Daniel Wheeler <daniel.wheeler@nist.gov>
2011-01-05 17:13:44 +0000

    removing reactiveWetting from file name
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/reactiveWetting@4074 d80e17d7-ff13-0410-a124-85740d801063

--- examples/reactiveWetting/reactiveWetting1DLiquidVapor.py
+++ examples/reactiveWetting/reactiveWetting1DLiquidVapor.py
@@ -1,104 +1,142 @@
 #!/usr/bin/env python
 
-"""
+## 
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "reactiveWetting1DLiquidVapor.py"
+ #
+ #  Author: Jonathan Guyer <guyer@nist.gov>
+ #  Author: Daniel Wheeler <daniel.wheeler@nist.gov>
+ #  Author: James Warren   <jwarren@nist.gov>
+ #    mail: NIST
+ #     www: http://www.ctcms.nist.gov/fipy/
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  FiPy is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ # ###################################################################
+ ##
 
-from fipy import *
+r"""
 
-factor = 1
+>>> from fipy import *
 
-Lx = 1.
-nx = 100
-dx = Lx / nx
+>>> factor = 1
 
-viscosity = 1e-3
-epsilon = 1e-2
+>>> Lx = 1.
+>>> nx = 100
+>>> dx = Lx / nx
 
-temperature = 1.854236326217e-01
-vbar = 8.149038895253e-01
-alpha = 7.0
+>>> viscosity = 1e-3
+>>> epsilon = 1e-2
 
-cfl = 0.1
-tolerance = 1e-3
-variance = 0.01
-dt = 1e-5
+>>> temperature = 1.854236326217e-01
+>>> vbar = 8.149038895253e-01
+>>> alpha = 7.0
 
-liquidDensity = 1.0
-vaporDensity = 0.01
-averageDensity = (liquidDensity + vaporDensity) / 2.0
+>>> cfl = 0.1
+>>> tolerance = 1e-3
+>>> variance = 0.01
+>>> dt = 1e-5
 
-mesh = Grid1D(nx=nx, dx=dx)
+>>> liquidDensity = 1.0
+>>> vaporDensity = 0.01
+>>> averageDensity = (liquidDensity + vaporDensity) / 2.0
 
-density = CellVariable(mesh=mesh, hasOld=True, name='density')
-density[:] = averageDensity * (1  + variance * (2 * numerix.random.random(mesh.getNumberOfCells()) - 1))
+>>> mesh = Grid1D(nx=nx, dx=dx)
 
-velocity = CellVariable(mesh=mesh, hasOld=True, name='velocity')
-velocity.constrain(0, mesh.getExteriorFaces())
+>>> density = CellVariable(mesh=mesh, hasOld=True, name='density')
+>>> density[:] = averageDensity * (1  + variance * (2 * numerix.random.random(mesh.getNumberOfCells()) - 1))
 
-potential = CellVariable(mesh=mesh, hasOld=False, name='potential')
-ap = CellVariable(mesh=mesh, value=1e+20)
-faceDensity = density.getArithmeticFaceValue()
+>>> velocity = CellVariable(mesh=mesh, hasOld=True, name='velocity')
+>>> velocity.constrain(0, mesh.getExteriorFaces())
 
-ConvectionTerm = CentralDifferenceConvectionTerm
+>>> potential = CellVariable(mesh=mesh, hasOld=False, name='potential')
+>>> ap = CellVariable(mesh=mesh, value=1e+20)
+>>> faceDensity = density.getArithmeticFaceValue()
 
-potentialDerivative = -2. * temperature / density / (1 - vbar * density)**2
-potentialConst = temperature * (numerix.log(density / (1 - vbar * density)) + alpha + vbar * density / (1 - vbar * density)**2)
+>>> ConvectionTerm = CentralDifferenceConvectionTerm
 
-apCoeff =  mesh._getFaceAreas() * mesh._getCellDistances() / ap.getArithmeticFaceValue()
-densityCoeff = apCoeff * faceDensity
+>>> potentialDerivative = -2. * temperature / density / (1 - vbar * density)**2
+>>> potentialConst = temperature * (numerix.log(density / (1 - vbar * density)) + alpha + vbar * density / (1 - vbar * density)**2)
 
-tensorAverage = (density * potential.getLeastSquaresGrad()).getArithmeticFaceValue()
+>>> apCoeff =  mesh._getFaceAreas() * mesh._getCellDistances() / ap.getArithmeticFaceValue()
+>>> densityCoeff = apCoeff * faceDensity
 
-velocityCorrection = apCoeff * (-tensorAverage)
+>>> tensorAverage = (density * potential.getLeastSquaresGrad()).getArithmeticFaceValue()
 
-densityEqn = TransientTerm(var=density) + VanLeerConvectionTerm(coeff=velocity.getFaceValue() - velocityCorrection, var=density) \
+>>> velocityCorrection = apCoeff * (-tensorAverage)
+
+>>> densityEqn = TransientTerm(var=density) + VanLeerConvectionTerm(coeff=velocity.getFaceValue() - velocityCorrection, var=density) \
              + DiffusionTerm(coeff=-densityCoeff * faceDensity, var=potential)
 
 velocityEqn = TransientTerm(coeff=density, var=velocity) + ConvectionTerm(coeff=[[1]] * faceDensity * velocity.getFaceValue(), var=velocity) \
               - DiffusionTerm(coeff=2 * viscosity, var=velocity) \
               + ConvectionTerm(coeff=density * [[1]], var=potential) - ImplicitSourceTerm(coeff=density.getGrad()[0], var=potential)
 
-potentialEqn = - ImplicitSourceTerm(coeff=potentialDerivative, var=density) + DiffusionTerm(coeff=epsilon, var=density) \
+>>> potentialEqn = - ImplicitSourceTerm(coeff=potentialDerivative, var=density) + DiffusionTerm(coeff=epsilon, var=density) \
                + ImplicitSourceTerm(1., var=potential) - potentialConst
 
-coupledEqn = densityEqn & velocityEqn & potentialEqn
+>>> coupledEqn = densityEqn & velocityEqn & potentialEqn
 
-viewers = Viewer(density), Viewer(velocity), Viewer(potential)
-for viewer in viewers:
-    viewer.plot()
+>>> viewers = Viewer(density), Viewer(velocity), Viewer(potential)
+>>> for viewer in viewers:
+...     viewer.plot()
 
-for timestep in xrange(1000):
-
-    residual = 1.
-    dt *= 1.1
-    
-    density.updateOld()
-    velocity.updateOld()
-
-    sweep = 0
-
-    while residual > tolerance and sweep < 100:
-
-
-
-        dt = min(dt * 1.1, dx / max(abs(velocity)) * cfl)
-
-        print 'sweep',sweep,'residual',residual, 'dt',dt
-
-        coupledEqn.cacheMatrix()
-        residual = coupledEqn.sweep(dt=dt)
-        ap[:] = coupledEqn.getMatrix().takeDiagonal()[mesh.getNumberOfCells(): 2 * mesh.getNumberOfCells()]
-
-##        print 'residual',residual
-##        raw_input('stopped')
-        if sweep == 0:
-            initialResidual = residual
-
-        residual = residual / initialResidual
-
-        sweep += 1
-
-    for viewer in viewers:
-        viewer.plot()
+>>> for timestep in xrange(1000):
+... 
+...     residual = 1.
+...     dt *= 1.1
+...     
+...     density.updateOld()
+...     velocity.updateOld()
+... 
+...     sweep = 0
+... 
+...     while residual > tolerance and sweep < 100:
+...
+... 
+... 
+...         dt = min(dt * 1.1, dx / max(abs(velocity)) * cfl)
+... 
+...         print 'sweep',sweep,'residual',residual, 'dt',dt
+... 
+...         coupledEqn.cacheMatrix()
+...         residual = coupledEqn.sweep(dt=dt)
+...         ap[:] = coupledEqn.getMatrix().takeDiagonal()[mesh.getNumberOfCells(): 2 * mesh.getNumberOfCells()]
+... 
+... ##        print 'residual',residual
+... ##        raw_input('stopped')
+...         if sweep == 0:
+...             initialResidual = residual
+... 
+...         residual = residual / initialResidual
+... 
+...         sweep += 1
+... 
+...     for viewer in viewers:
+...         viewer.plot()
 
 
 """
+
+__docformat__ = 'restructuredtext'
+
+if __name__ == '__main__':
+    import fipy.tests.doctestPlus
+    exec(fipy.tests.doctestPlus._getScript())
