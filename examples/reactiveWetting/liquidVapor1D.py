a0b71fb163d27fa7449b3d2c45ab0377a4cf9338
Daniel Wheeler <daniel.wheeler@nist.gov>
2011-01-07 22:37:17 +0000

    The script is has most of the documentation, but hasn't been run yet.
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/reactiveWetting@4081 d80e17d7-ff13-0410-a124-85740d801063

--- examples/reactiveWetting/liquidVapor1D.py
+++ examples/reactiveWetting/liquidVapor1D.py
@@ -34,104 +34,273 @@
 
 r"""
 
+This example solves a single-component, liquid-vapor, van der Waals system as
+described by Wheeler et *al.* [PhysRevE.82.051601]_. The free energy for this
+system takes the form,
+
+.. math:: :label: eq:reactiveWetting:liquidVapor1D:freeEnergy
+   
+   f = - \frac{e \rho^2}{m^2} + \frac{R T}{m} \left( \ln \frac{\rho}{m - \bar{v} \rho} \right)
+
+where :math:`\rho` is the density. This free energy supports a two phase in
+equilibrium with densities given by :math:`\rho^l` and :math:`\rho^v` in the
+liquid and vapor phases, respectively. These densities can be determined by
+solving the following system of equations,
+
+.. math::
+   :label: eq:reactiveWetting:liquidVapor1D:pressureEquilibrium
+   
+   P \left( \rho^l \right) = P \left( \rho^v \right)
+
+and
+
+.. math::
+   :label: eq:reactiveWetting:liquidVapor1D:chemicalPotentialEquilibrium
+   
+   \mu \left( \rho^l \right) = \mu \left( \rho^v \right)
+
+where :math:`\mu` is the chemical potential,
+
+.. math::
+   :label: eq:reactiveWetting:liquidVapor1D:chemicalPotential
+
+   \mu = \frac{\partial f}{\partial \rho}
+
+and :math:`P` is the pressure,
+
+.. math::
+   :label: eq:reactiveWetting:liquidVapor1D:pressure
+
+   P = \rho \mu - f
+
+One choice of thermodynamic parameters that yields a relatively physical two phase system is
+
+>>> molarWeight = 0.118
+>>> ee = -0.455971
+>>> gasConstant = 8.314
+>>> temperature = 650.
+>>> vbar = 1.3e-05
+
+with equilibrium density values of
+
+>>> liquidDensity = 7354.3402662299995
+>>> vaporDensity = 82.855803327810008
+
+The equilibrium densities can be verified by substitution into Eqs.
+:eq:`eq:reactiveWetting:liquidVapor1D:pressureEquilibrium` and
+:eq:`eq:reactiveWetting:liquidVapor1D:chemicalPotentialEquilibrium`. Firstly,
+Eqs. :eq:`eq:reactiveWetting:liquidVapor1D:freeEnergy`,
+:eq:`eq:reactiveWetting:liquidVapor1D:chemicalPotential` and
+:eq:`eq:reactiveWetting:liquidVapor1D:pressure` need to be defined as python
+functions,
+
 >>> from fipy import *
 
->>> factor = 1
+>>> def f(rho):
+...     return ee * rho**2 / molarWeight**2 + gasConstant * temperature * rho / molarWeight * \
+...            numerix.log(rho / (molarWeight - vbar * rho))
 
->>> Lx = 1.
+>>> def mu(rho):
+...     return 2 * ee * rho / molarWeight**2 + gasConstant * temperature / molarWeight * \
+...            (numerix.log(rho / (molarWeight - vbar * rho)) + molarWeight / (molarWeight - vbar * rho))
+
+>>> def P(rho):
+...     return rho * mu(rho) - f(rho)
+
+The equilibrium densities values can then be verified.
+
+>>> print numerix.allclose(mu(liquidDensity), mu(vaporDensity))
+True
+
+>>> print numerix.allclose(P(liquidDensity), P(vaporDensity))
+True
+
+In order to derive governing equations, the free energy functional must aslo be defined.
+
+.. math::
+   
+   F = \int \left[ f + \frac{\epsilon T}{2} \left( \partial_j \rho \right)^2 \right] dV
+
+Using standard dissipation laws, we can now write down the governing equations
+for mass and momentum conservation,
+
+.. math::
+   :label: eq:reactiveWetting:liquidVapor1D:continuity
+   
+   \frac{\partial \rho}{\partial t} + \partial_j \left(\rho u_j \right) = 0
+
+and
+
+.. math::
+   :label: eq:reactiveWetting:liquidVapor1D:momentum
+   
+   \frac{\partial \left( \rho u_i\right) }{\partial t} + \partial_j\left( \rho
+   u_iu_j \right) = \partial_j\left( \nu \left[ \partial_j u_i + \partial_i u_j
+   \right] \right) - \rho \partial_i \mu^{NC}
+
+where the non-classical potential, :math:`\mu^{NC}` is given by,
+
+.. math::
+  :label: eq:reactiveWetting:liquidVapor1D:nonClassicalPotential
+
+   \mu^{NC} = \frac{\delta F}{\delta \rho} = \mu - \epsilon T \partial_j^2 \rho
+   
+As usual, to proceed, we need to define a mesh
+
+>>> Lx = 1e-6
 >>> nx = 100
 >>> dx = Lx / nx
-
->>> viscosity = 1e-3
->>> epsilon = 1e-2
-
->>> temperature = 1.854236326217e-01
->>> vbar = 8.149038895253e-01
->>> alpha = 7.0
-
->>> cfl = 0.1
->>> tolerance = 1e-3
->>> variance = 0.01
->>> dt = 1e-5
-
->>> liquidDensity = 1.0
->>> vaporDensity = 0.01
->>> averageDensity = (liquidDensity + vaporDensity) / 2.0
-
 >>> mesh = Grid1D(nx=nx, dx=dx)
 
->>> density = CellVariable(mesh=mesh, hasOld=True, name='density')
->>> density[:] = averageDensity * (1  + variance * (2 * numerix.random.random(mesh.getNumberOfCells()) - 1))
+and the independent variables.
 
->>> velocity = CellVariable(mesh=mesh, hasOld=True, name='velocity')
->>> velocity.constrain(0, mesh.getExteriorFaces())
+>>> density = CellVariable(mesh=mesh, hasOld=True, name='$\rho$')
+>>> velocity = CellVariable(mesh=mesh, hasOld=True, name='$u$')
 
->>> potential = CellVariable(mesh=mesh, hasOld=False, name='potential')
->>> ap = CellVariable(mesh=mesh, value=1e+20)
->>> faceDensity = density.getArithmeticFaceValue()
+The system of equations will be solved in a fully coupled manner using a block
+matrix. Defining :math:`\mu^{NC}` as an independent variable makes it easier to
+script the equations without using higher order terms.
+
+>>> potentialNC = CellVariable(mesh=mesh, hasOld=True, name='$\mu^{NC}$')
+
+In order to solve the equations numerically, an interpolation method must be used
+to prevent the velocity and density fields decoupling. The following velocity
+correction equation (expressed in discretized form) prevents decoupling from
+occuring,
+
+.. math::
+   :label: eq:reactiveWetting:liquidVapor1D:correction
+   
+   u_{i,f}^c = \frac{A_f d_f}{\overline{d}_f} \left( \overline{ \rho \partial_i
+   \mu^{NC} }_f - \overline{\rho}_f \partial_{i,f} \mu^{NC} \right)
+
+where :math:`A_f` is the face area, :math:`d_f` is the distance between the
+adjacent cell centers and :math:`\overline{a}_f` is the momentum conservation
+equation's matrix diagonal. The overbar refers to an averaged value between the
+two adjacent cells to the face. The notation :math:`\partial_{i,f}` refers to a
+derivative evaluated directly at the face (not averaged). The variable
+:math:`u_i^c` is used to modify the velocity used in
+Eq. :eq:`eq:reactiveWetting:liquidVapor1D:continuity` such that,
+
+.. math::
+   :label: eq:reactiveWetting:liquidVapor1D:continuityCorrected
+   
+   \frac{\partial \rho}{\partial t} + \partial_j \left(\rho \left[u_j + u_i^c
+   \right] \right) = 0
+
+Equation :eq:`eq:reactiveWetting:liquidVapor1D:continuityCorrected` can be
+scripted in the form,
+
+>>> matrixDiagonal = CellVariable(mesh=mesh, name='$a_f$')
+>>> correctionCoeff = mesh._getFaceAreas() * mesh._getCellDistances() / matrixDiagonal.getFaceValue()
+>>> densityEqn = TransientTerm(var=density) \
+...              + VanLeerConvectionTerm(coeff=velocity.getFaceValue() + correctionCoeff \
+...                                            * (density * potentialNC.getGrad()).getFaceValue(), \
+...                                      var=density) \
+...              - DiffusionTerm(coeff=correctionCoeff * faceDensity**2, var=potentialNC)
+
+where the first term on the LHS of
+Eq. :eq:`eq:reactiveWetting:liquidVapor1D:correction` is calculated in an
+explicit manner in the ``VanLeerConvectionTerm`` and the second term is
+calculated implicitly as a ``DiffusionTerm`` with :math:`\mu^{NC}` as the
+independent variable.
+
+In order to write Eq. :eq:`eq:reactiveWetting:liquidVapor1D:momentum` as a
+:term:`FiPy` expression, the last term must be rewritten such that,
+
+.. math::
+
+   \rho \partial_i \mu^{NC} = \partial_i \left( \rho \mu^{NC} \right) - \mu^{NC} \partial_i \rho
+
+which results in
 
 >>> ConvectionTerm = CentralDifferenceConvectionTerm
+>>> velocityEqn = TransientTerm(coeff=density, var=velocity) \
+...               + ConvectionTerm(coeff=[[1]] * density.getFaceValue() * velocity.getFaceValue(), var=velocity) \
+...               = DiffusionTerm(coeff=2 * viscosity, var=velocity) \
+...               - ConvectionTerm(coeff=density.getFaceValue() * [[1]], var=potentialNC) \
+...               + ImplicitSourceTerm(coeff=density.getGrad()[0], var=potentialNC)
 
->>> potentialDerivative = -2. * temperature / density / (1 - vbar * density)**2
->>> potentialConst = temperature * (numerix.log(density / (1 - vbar * density)) + alpha + vbar * density / (1 - vbar * density)**2)
+The only required bnoundary condition eliminates flow in or out of the domain.
 
->>> apCoeff =  mesh._getFaceAreas() * mesh._getCellDistances() / ap.getArithmeticFaceValue()
->>> densityCoeff = apCoeff * faceDensity
+>>> velocity.constrain(0, mesh.getExteriorFaces())
 
->>> tensorAverage = (density * potential.getLeastSquaresGrad()).getArithmeticFaceValue()
+As previously stated, the :math:`\mu^{NC}` variable will be solved implicitly. To
+do this the Eq. eq:`eq:reactiveWetting:liquidVapor1D:nonClassicalPotential` needs
+to be linearized in :math:`\rho` such that
 
->>> velocityCorrection = apCoeff * (-tensorAverage)
+.. math::
+   :label: eq:reactiveWetting:liquidVapor1D:chemicalPotentialEquation
 
->>> densityEqn = TransientTerm(var=density) + VanLeerConvectionTerm(coeff=velocity.getFaceValue() - velocityCorrection, var=density) \
-             + DiffusionTerm(coeff=-densityCoeff * faceDensity, var=potential)
+   \mu^{NC} = \mu^* + \left( \frac{\partial \mu}{\partial \rho} \right)^* \left( \rho - \rho^* \right) - \epsilon T \partial_j^2 \rho
 
-velocityEqn = TransientTerm(coeff=density, var=velocity) + ConvectionTerm(coeff=[[1]] * faceDensity * velocity.getFaceValue(), var=velocity) \
-              - DiffusionTerm(coeff=2 * viscosity, var=velocity) \
-              + ConvectionTerm(coeff=density * [[1]], var=potential) - ImplicitSourceTerm(coeff=density.getGrad()[0], var=potential)
+The :math:`^*` superscript denotes the current held value. In :term:`FiPy`, can be written as,
 
->>> potentialEqn = - ImplicitSourceTerm(coeff=potentialDerivative, var=density) + DiffusionTerm(coeff=epsilon, var=density) \
-               + ImplicitSourceTerm(1., var=potential) - potentialConst
+>>> potentialDerivative = 2 * ee / molarWeight + gasConstant * temperature / density / (molarWeight - vbar * density)**2
 
->>> coupledEqn = densityEqn & velocityEqn & potentialEqn
+and :math:`\mu^*` is simply,
+
+>>> potential = mu(density)
+
+Eq. :eq:`eq:reactiveWetting:liquidVapor1D:chemicalPotentialEquation` can then be
+written as
+
+>>> potententialNCEq = ImplicitSourceTerm(coeff=1, var=potentialNC) \
+...                    == potential \
+...                    + ImplicitSourceTerm(coeff=potentialDerivative, var=density) \
+...                    - potentialDerivative * density \
+...                    - DiffusionTerm(coeff=epsilon * temperature, var=density)
+
+All three equations have now been defined and can now be combined together,
+
+>>> coupledEq = densityEqn & velocityEq & potentialEq
+
+The system will be solved as a phase separation problem with an initial density
+close to the average density, but with some small amplitude noise. Under these
+circumstances, the final condition should be two separate phases of roughly equal
+volume. Define an initial condition for the density, such that
+
+>>> density[:] = (liquidDensity + vaporDensity) / 2 * \
+...    (1  + 0.01 * (2 * numerix.random.random(mesh.getNumberOfCells()) - 1))
 
 >>> viewers = Viewer(density), Viewer(velocity), Viewer(potential)
 >>> for viewer in viewers:
 ...     viewer.plot()
 
->>> for timestep in xrange(1000):
+Some control parameters need to be defined. The ``cfl`` parameter limits the size
+of the time step so that ``dt = cfl * dx / max(velocity)``. 
+
+>>> cfl = 0.1
+>>> tolerance = 1e-3
+>>> globalTolerance = 1e-3
+>>> dt = 1e-10
+>>> globalResidual = 1.
+>>> timestep = 0
+>>> initialGlobalResidual = coupledEqn.justResidualVector(dt=1e+20)
+
+
+>>> while globalResidual > globalTolerance: 
 ... 
 ...     residual = 1.
+...     sweep = 0
 ...     dt *= 1.1
 ...     
 ...     density.updateOld()
 ...     velocity.updateOld()
-... 
-...     sweep = 0
-... 
-...     while residual > tolerance and sweep < 100:
 ...
-... 
+...     globalResidual = coupledEqn.justResidualVector(dt=1e+20) / initialGlobalResidual
+...     initialResiudal = coupledEqn.justResidualVector(dt=dt)
+...
+...     while residual > tolerance:
 ... 
 ...         dt = min(dt * 1.1, dx / max(abs(velocity)) * cfl)
 ... 
-...         print 'sweep',sweep,'residual',residual, 'dt',dt
-... 
 ...         coupledEqn.cacheMatrix()
-...         residual = coupledEqn.sweep(dt=dt)
+...         residual = coupledEqn.sweep(dt=dt) / initialResidual
 ...         ap[:] = coupledEqn.getMatrix().takeDiagonal()[mesh.getNumberOfCells(): 2 * mesh.getNumberOfCells()]
-... 
-... ##        print 'residual',residual
-... ##        raw_input('stopped')
-...         if sweep == 0:
-...             initialResidual = residual
-... 
-...         residual = residual / initialResidual
-... 
-...         sweep += 1
 ... 
 ...     for viewer in viewers:
 ...         viewer.plot()
-
 
 """
 
