938cf68a3a4d2519a22e6abf3495c13dcd6a5f0e
Jonathan E. Guyer <guyer@nist.gov>
2011-03-21 14:21:47 +0000

    James' `getsetDeprecated` decorator is lame
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/ufuncVariables@4311 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/tools/decorators.py
+++ fipy/tools/decorators.py
@@ -1,15 +1,47 @@
-
+import re
 import warnings
 
-def getsetDeprecated(fn):
-    """
-    Used to decorate old get/set functions which are now deprecated in favor of
-    their associated properties.
-    """
-    def new(*args, **kwargs):
-        temp = "Get/set methods are deprecated; use the associated property instead."
-        warnings.warn(temp, DeprecationWarning, stacklevel=3)
+from numpy.lib.utils import _Deprecate
 
-        return fn(*args, **kwargs)
-    return new
+class _GetSetDeprecated(_Deprecate):
+    def __init__(self, old_name=None, new_name=None, message=None):
+        _Deprecate.__init__(self, old_name=old_name, new_name=None, message=message)
+        self.new_property = new_name
+        
+    def __call__(self, func, *args, **kwargs):
+        old_name = self.old_name
+        new_name = self.new_name
+        message = self.message
 
+        if old_name is None:
+            try:
+                old_name = func.func_name
+            except AttributeError:
+                old_name = func.__name__
+                
+        self.old_name = old_name + "()"
+
+        if new_name is None:
+            RE = re.search("(_*)(get|set)(.)(.*)", old_name)
+            if RE is not None:
+                new_name = RE.group(1) + RE.group(3).lower() + RE.group(4)
+                if message is None:
+                    self.message = "Use the `%s` property instead." % new_name
+        
+        return _Deprecate.__call__(self, func=func, *args, **kwargs)
+
+def getsetDeprecated(*args, **kwargs):
+    if args:
+        fn = args[0]
+        args = args[1:]
+
+        return _GetSetDeprecated(*args, **kwargs)(fn)
+    else:
+        return _GetSetDeprecated(*args, **kwargs)
+
+def _test(): 
+    import doctest
+    return doctest.testmod()
+    
+if __name__ == "__main__": 
+    _test() 
