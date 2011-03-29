40f6bb0934e3898b9c5d41261e318e49e5fd744d
James O'Beirne <james.obeirne@gmail.com>
2011-03-29 16:50:26 +0000

    GmshExport works for Grid, Tri, and concatenated examples.
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/gmshExport@4338 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/meshes/gmshExport.py
+++ fipy/meshes/gmshExport.py
@@ -46,111 +46,138 @@ from fipy.tools import numerix
 class MeshExportError(Exception):
     pass
 
-def _getElementType(vertices, dimensions):
-    if(vertices == 3 and dimensions == 2):
-        return 2 ## triangle
-    elif(vertices == 4 and dimensions == 2):
-        return 3 ## quadrangle
-    elif(vertices == 4 and dimensions == 3):
-        return 4 ## tetrahedron
-    elif(vertices == 8 and dimensions == 3):
-        return 5 ## hexahedron
-    elif(vertices == 6 and dimensions == 3):
-        return 6 ## prism
-    elif(vertices == 5 and dimensions == 3):
-        return 7 ## pyramid
-    else:
-        raise MeshExportError, "Element type unsupported by Gmsh"
+class GmshExporter(object):
 
-def _orderVertices(vertexCoords, vertices):
-    coordinates = numerix.take(vertexCoords, vertices, axis=1)
-    centroid = numerix.add.reduce(coordinates, axis=1) / coordinates.shape[1]
-    coordinates = coordinates - centroid[..., numerix.newaxis]
-    coordinates = numerix.where(coordinates == 0, 1.e-10, coordinates) ## to prevent division by zero
-    angles = numerix.arctan(coordinates[1] / coordinates[0]) + numerix.where(coordinates[0] < 0, numerix.pi, 0) ## angles go from -pi / 2 to 3*pi / 2
-    sortorder = numerix.argsort(angles)
-    return numerix.take(vertices, sortorder)
-    
+    def __init__(self, mesh, filename):
+        self.mesh = mesh
+        self.filename = filename
+        self.outFile = open(self.filename, mode = 'w')
 
-def exportAsMesh(mesh, filename):
-    outFile = open(filename, mode = 'w')
-    ## do the nodes
-    outFile.write("$NOD\n")
-    coords = mesh.vertexCoords
-    dimensions, numNodes = coords.shape
-    outFile.write(str(numNodes))
-    outFile.write('\n')
-    for i in range(numNodes):
-        outFile.write(str(i + 1))
-        outFile.write(' ')
-        outFile.write(str(coords[0, i]))
-        outFile.write(' ') 
-        outFile.write(str(coords[1, i]))
-        outFile.write(' ')
-        if(dimensions == 2):
-            outFile.write("0 \n")
-        elif(dimensions == 3):
-            outFile.write(str(coords[2, i]))
-            outFile.write (" \n")
+    def _getElementType(self, vertices, dimensions):
+        if(vertices == 3 and dimensions == 2):
+            return 2 ## triangle
+        elif(vertices == 4 and dimensions == 2):
+            return 3 ## quadrangle
+        elif(vertices == 4 and dimensions == 3):
+            return 4 ## tetrahedron
+        elif(vertices == 8 and dimensions == 3):
+            return 5 ## hexahedron
+        elif(vertices == 6 and dimensions == 3):
+            return 6 ## prism
+        elif(vertices == 5 and dimensions == 3):
+            return 7 ## pyramid
         else:
-            raise MeshExportError, "Mesh has fewer than 2 or more than 3 dimensions"
-    outFile.write("$ENDNOD\n$ELM\n")
-    ## do the elements
-    faceVertexIDs = mesh.faceVertexIDs
-    cellFaceIDs = mesh.cellFaceIDs
-    numCells = cellFaceIDs.shape[1]
-    outFile.write(str(numCells))
-    outFile.write('\n')
-    for i in range(numCells):
-        ## build the vertex list
-        vertexList = []
-        for faceNum in cellFaceIDs[..., i]:
-            for vertexNum in faceVertexIDs[..., faceNum]:
-                if vertexNum not in vertexList:
-                    vertexList = vertexList + [vertexNum]
-        if(dimensions == 2):
-            vertexList = _orderVertices(coords, vertexList)
-        numVertices = len(vertexList)
-        elementType = _getElementType(numVertices, dimensions)
-        outFile.write(str(i + 1))
-        outFile.write(' ')
-        outFile.write(str(elementType))
-        outFile.write(" 1 1 ")
-        outFile.write(str(numVertices))
-        for a in vertexList:
-            outFile.write(' ')
-            outFile.write(str(a + 1))
-        outFile.write("\n")
-    outFile.write("$ENDNOD\n")
-    outFile.close()
+            raise MeshExportError, "Element type unsupported by Gmsh"
+
+    def _orderVertices(self, vertexCoords, vertices):
+        coordinates = numerix.take(vertexCoords, vertices, axis=1)
+        centroid = numerix.add.reduce(coordinates, axis=1) / coordinates.shape[1]
+        coordinates = coordinates - centroid[..., numerix.newaxis]
+        coordinates = numerix.where(coordinates == 0, 1.e-10, coordinates) ## to prevent division by zero
+        angles = numerix.arctan(coordinates[1] / coordinates[0]) + numerix.where(coordinates[0] < 0, numerix.pi, 0) ## angles go from -pi / 2 to 3*pi / 2
+        sortorder = numerix.argsort(angles)
+        return numerix.take(vertices, sortorder)
+        
+
+    def export(self):
+        coords = self.mesh.vertexCoords
+        dimensions, numNodes = coords.shape
+
+        self._writeMeshFormat()
+        self._writeNodes(coords, dimensions, numNodes)
+        self._writeElements(coords, dimensions, numNodes)
+        
+        self.outFile.close()
+
+    def _writeMeshFormat(self):
+        versionNumber = 2.2
+        sizeOfDouble = 8
+        lines = ["$MeshFormat\n",
+                 "%f 0 %d\n" % (versionNumber, sizeOfDouble),
+                 "$EndMeshFormat\n"]
+        self.outFile.writelines(lines)
+
+    def _writeNodes(self, coords, dimensions, numNodes):
+        self.outFile.write("$Nodes\n")
+        self.outFile.write(str(numNodes) + '\n')
+
+        for i in range(numNodes):
+            self.outFile.write("%s %s %s " % (str(i + 1),
+                                              str(coords[0, i]),
+                                              str(coords[1, i])))
+            if(dimensions == 2):
+                self.outFile.write("0 \n")
+            elif(dimensions == 3):
+                self.outFile.write(str(coords[2, i]))
+                self.outFile.write (" \n")
+            else:
+                raise MeshExportError, "Mesh has fewer than 2 or more than 3 dimensions" 
+
+        self.outFile.write("$EndNodes\n")
+
+    def _writeElements(self, coords, dimensions, numNodes):
+        self.outFile.write("$Elements\n")
+
+        faceVertexIDs = self.mesh.faceVertexIDs
+        cellFaceIDs = self.mesh.cellFaceIDs
+        numCells = cellFaceIDs.shape[1]
+        self.outFile.write(str(numCells) + '\n')
+
+        for i in range(numCells):
+            ## build the vertex list
+            vertexList = []
+            for faceNum in cellFaceIDs[..., i]:
+                """For more complicated meshes, some cells may have fewer
+                faces than others. If this is the case, ignore the
+                '--' entries."""
+                if type(faceNum) == numerix.ma.core.MaskedConstant:
+                    break
+                for vertexNum in faceVertexIDs[..., faceNum]:
+                    if vertexNum not in vertexList:
+                        vertexList.append(vertexNum)
+
+            if dimensions == 2:
+                vertexList = self._orderVertices(coords, vertexList)
+
+            numVertices = len(vertexList)
+            elementType = self._getElementType(numVertices, dimensions)
+            self.outFile.write("%s %s 0" % (str(i + 1), str(elementType)))
+
+            for a in vertexList:
+                self.outFile.write(" %s" % str(a + 1))
+            self.outFile.write("\n")
+
+        self.outFile.write("$EndElements\n") 
 
 if __name__ == "__main__":
     from fipy.meshes import Grid2D
     from fipy.meshes import Tri2D
     from fipy.meshes import Grid3D
     
-    import tempfile
+    import tempfile as tmp
     import subprocess
 
     a = Grid2D(dx = 1.0, dy = 1.0, nx = 10, ny = 10)
-    f = tempfile.NamedTemporaryFile(suffix=".msh")
-    f.close()
-    exportAsMesh(a, f.name)
-    subprocess.Popen(["gmsh", f.name])
+    a2 = Grid2D(dx = 1.0, dy = 1.0, nx = 10, ny = 10) + ([10], [0])
+    f1 = tmp.mktemp(".msh")
+    GmshExporter(a, f1).export()
+
     b = Tri2D(dx = 1.0, dy = 1.0, nx = 10, ny = 10)
-    f = tempfile.NamedTemporaryFile(suffix=".msh")
-    f.close()
-    exportAsMesh(b, f.name)
-    subprocess.Popen(["gmsh", f.name])
-#     fudge = calibrate_profiler(10000)
-#     profile = Profiler('profile', fudge=fudge)
+    f2 = tmp.mktemp(".msh")
+    GmshExporter(b, f2).export()
+
     c = Grid3D(dx = 1.0, dy = 1.0, nx = 20, ny = 20, nz = 20)
-    f = tempfile.NamedTemporaryFile(suffix=".msh")
-    f.close()
-    exportAsMesh(c, f.name)
-#     profile.stop()
-    subprocess.Popen(["gmsh", "-v", "0", f.name])
-    
-        
+    f3 = tmp.mktemp(".msh")
+    GmshExporter(c, f3).export()
+
+    d = a + a2
+    f4 = tmp.mktemp(".msh")
+    GmshExporter(d, f4).export()
+    subprocess.Popen(["gmsh", f4])
+ 
+    e = a + (b + ([0], [10]))
+    f5 = tmp.mktemp(".msh")
+    GmshExporter(e, f5).export()
+    subprocess.Popen(["gmsh", f5])
     
     
