9ab3cb7dfbd2a94130eb0995bd234c5a725cd26b
Daniel Wheeler <daniel.wheeler@nist.gov>
2011-03-31 21:01:27 +0000

    Updating the example to the future syntax.
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/riemann@4367 d80e17d7-ff13-0410-a124-85740d801063

--- examples/riemann/acoustics.py
+++ examples/riemann/acoustics.py
@@ -1,8 +1,8 @@
 from fipy import *
 
-L = 8.
-X0 = -4.
-nx = 3200
+L = 2.
+X0 = -1.
+nx = 800
 cfl = 10.0
 K = 4.
 rho = 1.
@@ -11,22 +11,29 @@ dx = L / nx
 m = Grid1D(nx=nx, dx=dx) + X0
 x, = m.getCellCenters()
 
-p = CellVariable(mesh=m, value=numerix.exp(-50 * (x - 0.3)**2) * numerix.cos(20 * (x - 0.3)))
-p[x > 0.3] = 0
-u = CellVariable(mesh=m)
+q = CellVariable(mesh=m, rank=1)
+q[0,:] = numerix.exp(-50 * (x - 0.3)**2) * numerix.cos(20 * (x - 0.3))
+q[0, x > 0.3] = 0.
 
-ConvectionTerm = CentralDifferenceConvectionTerm
+##p = CellVariable(mesh=m, value=numerix.exp(-50 * (x - 0.3)**2) * numerix.cos(20 * (x - 0.3)))
+##p[x > 0.3] = 0
+##u = CellVariable(mesh=m)
 
-TransientTerm(var=p)
+##ConvectionTerm = CentralDifferenceConvectionTerm
 
-eqn = (TransientTerm(var=p) + CentralDifferenceConvectionTerm(coeff=K, var=u)) & \
-      (TransientTerm(var=u) + CentralDifferenceConvectionTerm(coeff=1. / rho, var=p))
 
-vi = Viewer((p, u))
+Ax = ((0, K), (1 / rho, 0))
+
+eqn = TransientTerm() + ConvectionTerm((Ax,)) == 0
+
+##eqn = (TransientTerm(var=p) + CentralDifferenceConvectionTerm(coeff=K, var=u)) & \
+##      (TransientTerm(var=u) + CentralDifferenceConvectionTerm(coeff=1. / rho, var=p))
+
+vi = Viewer(q)
 vi.plot()
 raw_input('stopped')
 for step in range(100000):
-    eqn.solve(dt=cfl * dx / 2.)
+    eqn.solve(q, dt=cfl * dx / 2.)
     print 'step',step
     if step % 10 ==  0:
         vi.plot()
