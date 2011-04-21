20b88455f1e88ef40822e0f99daeed667a60d6db
James O'Beirne <james.obeirne@gmail.com>
2011-04-21 20:19:22 +0000

    Stripping out cruft.
    git-svn-id: svn+ssh://code.matforge.org/fipy/trunk@4478 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/meshes/cylindricalGrid2D.py
+++ fipy/meshes/cylindricalGrid2D.py
@@ -56,16 +56,11 @@ class CylindricalGrid2D(Grid2D):
         self.origin /= scale
 
         Grid2D.__init__(self, dx=dx, dy=dy, nx=nx, ny=ny, overlap=overlap, communicator=communicator)
-                                     
-        """
-        This is an unfortunate block of code, but it's better than the
-        alternatives.
-        """
-        oldFaceAreas = self._faceAreas
+        
         self._faceAreas *= self.faceCenters[0]
 
         self._scaledFaceAreas = self._scale['area'] * self._faceAreas
-        self._areaProjections = self._faceNormals * oldFaceAreas
+        self._areaProjections = self._faceNormals * self._faceAreas
         self._orientedAreaProjections = self._calcOrientedAreaProjections()
         self._faceAspectRatios = self._calcFaceAspectRatios()
                                        
