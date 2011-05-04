4cdabdf37aab1d91d97f81d3036ca06b4f97cf59
Daniel Wheeler <daniel.wheeler@nist.gov>
2011-05-04 19:58:08 +0000

    Bumping version number after release.
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/version-2_1@4527 d80e17d7-ff13-0410-a124-85740d801063

--- setup.py
+++ setup.py
@@ -546,7 +546,7 @@ def getVersion(version, release=False):
     return version
 
 dist = setup(	name = "FiPy",
-        version = getVersion(version='2.1.2', release=True), 
+        version = getVersion(version='2.1.3', release=False), 
         author = "Jonathan Guyer, Daniel Wheeler, & Jim Warren",
         author_email = "fipy@nist.gov",
         url = "http://www.ctcms.nist.gov/fipy/",
