97df494e883b588c54042af9713c874dba520478
Andrew A. Acquaviva <no@email>
2011-06-14 18:37:14 +0000

    Rough sketch of how to generalize efficiency_test for any example
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/efficiency_test@4601 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/tools/generator.py
@@ -0,0 +1,23 @@
+import string
+import time
+import os
+from ../../ import setup
+
+##call some function in copy_script class
+
+readin = open('../../examples/diffusion/mesh1D.py','r')
+readinstr = readin.read()
+
+##times = []
+##time += time.time()
+##find "mesh =", then find the next \n after that
+##put in a times += time.time()
+##find "for", then find the next \n after that
+##put in a \t times += time.time()
+##do the same for any "while" loops
+##put in a times += time.time() at eof
+
+## write new file to place for setup.py efficiency_test to find and \
+##run for benchmarks, uploading results to Codespeed.
+
+
