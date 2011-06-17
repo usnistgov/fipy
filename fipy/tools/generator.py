78530e8dc86e53617f30675ab2aaf98ffecd94ca
Andrew A. Acquaviva <no@email>
2011-06-17 13:05:13 +0000

    times += time.time() doesn't work. Switched to times.append(time.time())
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/efficiency_test@4611 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/tools/generator.py
+++ fipy/tools/generator.py
@@ -7,7 +7,7 @@ from copy_script import Copy_script
 if os.path.exists('mesh1D.py'):
     os.remove('mesh1D.py')
 
-DocProg = Copy_script(To='mesh1D.py', From='../../examples/diffusion/mesh1D.py')
+DocProg = Copy_script(To='mesh1D.py', From='examples/diffusion/mesh1D.py')
 DocProg.finalize_options()
 DocProg.run()
 f = open('mesh1D.py','r+w')
@@ -17,24 +17,24 @@ flist = f.readlines()
 for index, line in enumerate(flist):
     whitespaces = len(line) - len(line.lstrip())
     if 'from fipy import *' in line:
-        flist.insert(index + 1, 'import time \ntimes = [] \ntimes += time.time()\n')
+        flist.insert(index + 1, 'import time \ntimes = [] \ntimes.append(time.time())\n')
     elif 'mesh =' in line and not '#' in line:
-        flist.insert(index + 1, 'times += time.time()\n')
+        flist.insert(index + 1, 'times.append(time.time())\n')
     elif ('.sweep' in line or '.solve' in line) and not '#' in line:
          if whitespaces != 0:
              if '(' in line and not ')' in line:
-                 flist.insert(index + 2, whitespaces * ' ' + 'times += time.time()\n')
+                 flist.insert(index + 2, whitespaces * ' ' + 'times.append(time.time())\n')
              else:
-                 flist.insert(index + 1, whitespaces * ' ' + 'times += time.time()\n')
+                 flist.insert(index + 1, whitespaces * ' ' + 'times.append(time.time())\n')
          elif '(' in line and not ')' in line:
-             flist.insert(index + 2, 'times += time.time()\n')
+             flist.insert(index + 2, 'times.append(time.time())\n')
          else:
-             flist.insert(index + 1, 'times += time.time()\n')
+             flist.insert(index + 1, 'times.append(time.time())\n')
     elif 'while' in line and not '#' in line:
         if whitespaces != 0:
-            flist.insert(index + 1, whitespaces * ' ' + 'times += time.time()\n')
+            flist.insert(index + 1, whitespaces * ' ' + 'times.append(time.time())\n')
         else:
-            flist.insert(index + 1, 4 * ' ' + 'times += time.time()\n')
+            flist.insert(index + 1, 4 * ' ' + 'times.append(time.time())\n')
     elif "__name__ == '__main__':" in line and not '#' in line:
         whitespaces = len(line) - len(line.lstrip())
         line = line.lstrip()
@@ -43,7 +43,7 @@ for index, line in enumerate(flist):
         flist.insert(index, commentedline)
         del flist[index+1]
 
-flist.append('\ntimes += time.time()\n')
+flist.append('\ntimes.append(time.time())\n')
 flist.append('\nruntime = times[len(times)-1]-time[0]')
 flist.append("\nprint 'runtime:', runtime") 
 
