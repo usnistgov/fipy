db69b4c70fc055cee7adee4b42d593a8876fc539
Andrew A. Acquaviva <no@email>
2011-06-20 18:22:10 +0000

    Generator.py can also run by itself now
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/efficiency_test@4626 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/tools/generator.py
+++ fipy/tools/generator.py
@@ -1,3 +1,39 @@
+#!/usr/bin/env python
+
+## -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "serialCommWrapper.py"
+ #
+ #  Author: Jonathan Guyer <guyer@nist.gov>
+ #  Author: Daniel Wheeler <daniel.wheeler@nist.gov>
+ #  Author: James Warren   <jwarren@nist.gov>
+ #  Author: Andrew Acquaviva <andrewa@nist.gov>
+ #    mail: NIST
+ #     www: http://www.ctcms.nist.gov/fipy/
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  FiPy is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  See the file "license.terms" for information on usage and  redistribution
+ #  of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ #  
+ # ###################################################################
+ ##
 
 import string
 import time
@@ -72,6 +108,6 @@ def run(extra_examples):
         g.write("".join(flist))
         g.close()
 
-## if __name__ == "__main__":
-##    run(empty=[])
+if __name__ == "__main__":
+    run([])
  
