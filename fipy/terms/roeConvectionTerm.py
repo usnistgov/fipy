55699e398e02ee179bc2843d63c18a2e1e86ad17
Daniel Wheeler <daniel.wheeler@nist.gov>
2011-06-30 19:26:53 +0000

    * First steps in creating a Roe convecton term.
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/riemann@4653 d80e17d7-ff13-0410-a124-85740d801063

---
+++ fipy/terms/roeConvectionTerm.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+## -*-Pyth-*-
+ # ###################################################################
+ #  FiPy - Python-based finite volume PDE solver
+ # 
+ #  FILE: "roeConvectionTerm.py"
+ #
+ #  Author: Jonathan Guyer <guyer@nist.gov>
+ #  Author: Daniel Wheeler <daniel.wheeler@nist.gov>
+ #  Author: James Warren   <jwarren@nist.gov>
+ #    mail: NIST
+ #     www: http://www.ctcms.nist.gov/fipy/
+ #  
+ # ========================================================================
+ # This software was developed at the National Institute of Standards
+ # and Technology by employees of the Federal Government in the course
+ # of their official duties.  Pursuant to title 17 Section 105 of the
+ # United States Code this software is not subject to copyright
+ # protection and is in the public domain.  FiPy is an experimental
+ # system.  NIST assumes no responsibility whatsoever for its use by
+ # other parties, and makes no guarantees, expressed or implied, about
+ # its quality, reliability, or any other characteristic.  We would
+ # appreciate acknowledgement if the software is used.
+ # 
+ # This software can be redistributed and/or modified freely
+ # provided that any derivative works bear some notice that they are
+ # derived from it, and any modified versions bear some notice that
+ # they have been modified.
+ # ========================================================================
+ #  
+ # ###################################################################
+ ##
+
+__docformat__ = 'restructuredtext'
+
+from fipy.terms.baseConvectionTerm import _BaseConvectionTerm
+
+class RoeConvectionTerm(_BaseConvectionTerm):
+    r"""
+    A convection term implementing the Roe approximate Riemann flux update of the form
+
+    .. math::
+
+        F_i = \frac{1}{2} \left[ n_j u^-_{jik} \phi_k^- + n_j u^+_{jik} \phi_k^+ \right] -
+        \frac{1}{2} n_j |\hat{A}_{jik}| \left( \phi_k^+ - \phi_k^- \right)
+
+    where the flux is across a face between cell $C^-$ and cell $C^+$ and
+    the normal $n_j$ points from $C^-$ to $C^+$. The question when
+    implementing Roe solvers is how to approximate $\hat{A}_{jik}$ from the
+    local Riemann problem
+
+    .. math::
+
+        \partial_t \phi_i + A_{jik} \partial_j \phi_k = 0
+
+    where
+
+    .. math::
+
+        A_{jik} = \frac{ \partial \left( u_{jil} \phi_l \right) }{ \partial \phi_k }
+
+    """
+
+    def __init__(self, coeff=1.0, var=None):
+        """
+        :Parameters:
+          - `coeff` : The `Term`'s coefficient value.
+
+        """
+
+        self.stencil = None
+        
+        if isinstance(coeff, _MeshVariable) and coeff.rank < 1:
+            raise VectorCoeffError
+
+        FaceTerm.__init__(self, coeff=coeff, var=var)
+        
+    def _getCoeffMatrix_(self, var, weight):
+        coeff = self._getGeomCoeff(var)
+
+        if self.coeffMatrix is None:
+            self.coeffMatrix = {'cell 1 diag' : coeff[0],
+                                'cell 1 offdiag': coeff[1],
+                                'cell 2 diag': -coeff[1],
+                                'cell 2 offdiag': -coeff[0]}
+        return self.coeffMatrix
+    
+    def _calcGeomCoeff(self, var):
+        from fipy.variables.roeVariable import _RoeVariable
+        return _RoeVariable(var, self.coeff)
