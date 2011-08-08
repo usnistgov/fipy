5eb10c7342397379170b4b48b73b73f16844e3f0
Jonathan E. Guyer <guyer@nist.gov>
2011-08-08 18:09:33 +0000

    moved labeling test up to be documentation for Gmsh2D
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/Gmshing@4708 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/meshes/gmshImport.py
+++ fipy/meshes/gmshImport.py
@@ -612,6 +612,122 @@ def _makeMapVariables(mesh, physicalCellMap, geometricalCellMap, physicalFaceMap
             physicalFaceMap, geometricalFaceMap, physicalFaces)
 
 class Gmsh2D(Mesh2D):
+    """Construct a 2D Mesh using Gmsh
+    
+    >>> radius = 5.
+    >>> side = 4.
+    >>> squaredCircle = Gmsh2D('''
+    ... // A mesh consisting of a square inside a circle inside a circle
+    ...                        
+    ... // define the basic dimensions of the mesh
+    ...                        
+    ... cellSize = 1;
+    ... radius = %(radius)g;
+    ... side = %(side)g;
+    ...                        
+    ... // define the compass points of the inner circle
+    ...                        
+    ... Point(1) = {0, 0, 0, cellSize};
+    ... Point(2) = {-radius, 0, 0, cellSize};
+    ... Point(3) = {0, radius, 0, cellSize};
+    ... Point(4) = {radius, 0, 0, cellSize};
+    ... Point(5) = {0, -radius, 0, cellSize};
+    ...                        
+    ... // define the compass points of the outer circle
+    ... 
+    ... Point(6) = {-2*radius, 0, 0, cellSize};
+    ... Point(7) = {0, 2*radius, 0, cellSize};
+    ... Point(8) = {2*radius, 0, 0, cellSize};
+    ... Point(9) = {0, -2*radius, 0, cellSize};
+    ... 
+    ... // define the corners of the square
+    ... 
+    ... Point(10) = {side/2, side/2, 0, cellSize/2};
+    ... Point(11) = {-side/2, side/2, 0, cellSize/2};
+    ... Point(12) = {-side/2, -side/2, 0, cellSize/2};
+    ... Point(13) = {side/2, -side/2, 0, cellSize/2};
+    ... 
+    ... // define the inner circle
+    ... 
+    ... Circle(1) = {2, 1, 3};
+    ... Circle(2) = {3, 1, 4};
+    ... Circle(3) = {4, 1, 5};
+    ... Circle(4) = {5, 1, 2};
+    ... 
+    ... // define the outer circle
+    ... 
+    ... Circle(5) = {6, 1, 7};
+    ... Circle(6) = {7, 1, 8};
+    ... Circle(7) = {8, 1, 9};
+    ... Circle(8) = {9, 1, 6};
+    ... 
+    ... // define the square
+    ... 
+    ... Line(9) = {10, 13};
+    ... Line(10) = {13, 12};
+    ... Line(11) = {12, 11};
+    ... Line(12) = {11, 10};
+    ... 
+    ... // define the three boundaries
+    ... 
+    ... Line Loop(1) = {1, 2, 3, 4};
+    ... Line Loop(2) = {5, 6, 7, 8};
+    ... Line Loop(3) = {9, 10, 11, 12};
+    ... 
+    ... // define the three domains
+    ... 
+    ... Plane Surface(1) = {2, 1};
+    ... Plane Surface(2) = {1, 3};
+    ... Plane Surface(3) = {3};
+    ... 
+    ... // label the three domains
+    ... 
+    ... // attention: if you use any "Physical" labels, you *must* label 
+    ... // all elements that correspond to FiPy Cells (Physical Surace in 2D 
+    ... // and Physical Volume in 3D) or Gmsh will not include them and FiPy
+    ... // will not be able to include them in the Mesh. 
+    ... 
+    ... // note: if you do not use any labels, all Cells will be included.
+    ... 
+    ... Physical Surface("Outer") = {1};
+    ... Physical Surface("Middle") = {2};
+    ... Physical Surface("Inner") = {3};
+    ... 
+    ... // label the "north-west" part of the exterior boundary
+    ... 
+    ... // note: you only need to label the Face elements 
+    ... // (Physical Line in 2D and Physical Surface in 3D) that correspond
+    ... // to boundaries you are interested in. FiPy does not need them to
+    ... // construct the Mesh.
+    ... 
+    ... Physical Line("NW") = {5};
+    ... ''' % locals())
+
+    It can be easier to specify certain domains and boundaries within Gmsh 
+    than it is to define the same domains and boundaries with FiPy expressions.
+    
+    Here we compare obtaining the same Cells and Faces using FiPy's 
+    parametric descriptions and Gmsh's labels.
+    
+    >>> x, y = squaredCircle.cellCenters
+
+    >>> middle = ((x**2 + y**2 <= radius**2) 
+    ...           & ~((x > -side/2) & (x < side/2)
+    ...               & (y > -side/2) & (y < side/2)))
+
+    >>> print (middle == squaredCircle.physicalCells["Middle"]).all()
+    True
+    
+    >>> X, Y = squaredCircle.faceCenters
+
+    >>> NW = ((X**2 + Y**2 > (1.99*radius)**2) 
+    ...       & (X**2 + Y**2 < (2.01*radius)**2)
+    ...       & (X <= 0) & (Y >= 0))
+
+    >>> print (NW == squaredCircle.physicalFaces["NW"]).all()
+    True
+    """
+    
     def __init__(self, 
                  arg, 
                  coordDimensions=2, 
@@ -841,115 +957,6 @@ class Gmsh2D(Mesh2D):
         Traceback (most recent call last):
             ...
         GmshException: Gmsh hasn't produced any cells! Check your Gmsh code.
-        
-        Testing element labeling
-        
-        >>> radius = 5.
-        >>> side = 4.
-        >>> squaredCircle = Gmsh2D('''
-        ... // A mesh consisting of a square inside a circle inside a circle
-        ...                        
-        ... // define the basic dimensions of the mesh
-        ...                        
-        ... cellSize = 1;
-        ... radius = %(radius)g;
-        ... side = %(side)g;
-        ...                        
-        ... // define the compass points of the inner circle
-        ...                        
-        ... Point(1) = {0, 0, 0, cellSize};
-        ... Point(2) = {-radius, 0, 0, cellSize};
-        ... Point(3) = {0, radius, 0, cellSize};
-        ... Point(4) = {radius, 0, 0, cellSize};
-        ... Point(5) = {0, -radius, 0, cellSize};
-        ...                        
-        ... // define the compass points of the outer circle
-        ... 
-        ... Point(6) = {-2*radius, 0, 0, cellSize};
-        ... Point(7) = {0, 2*radius, 0, cellSize};
-        ... Point(8) = {2*radius, 0, 0, cellSize};
-        ... Point(9) = {0, -2*radius, 0, cellSize};
-        ... 
-        ... // define the corners of the square
-        ... 
-        ... Point(10) = {side/2, side/2, 0, cellSize/2};
-        ... Point(11) = {-side/2, side/2, 0, cellSize/2};
-        ... Point(12) = {-side/2, -side/2, 0, cellSize/2};
-        ... Point(13) = {side/2, -side/2, 0, cellSize/2};
-        ... 
-        ... // define the inner circle
-        ... 
-        ... Circle(1) = {2, 1, 3};
-        ... Circle(2) = {3, 1, 4};
-        ... Circle(3) = {4, 1, 5};
-        ... Circle(4) = {5, 1, 2};
-        ... 
-        ... // define the outer circle
-        ... 
-        ... Circle(5) = {6, 1, 7};
-        ... Circle(6) = {7, 1, 8};
-        ... Circle(7) = {8, 1, 9};
-        ... Circle(8) = {9, 1, 6};
-        ... 
-        ... // define the square
-        ... 
-        ... Line(9) = {10, 13};
-        ... Line(10) = {13, 12};
-        ... Line(11) = {12, 11};
-        ... Line(12) = {11, 10};
-        ... 
-        ... // define the three boundaries
-        ... 
-        ... Line Loop(1) = {1, 2, 3, 4};
-        ... Line Loop(2) = {5, 6, 7, 8};
-        ... Line Loop(3) = {9, 10, 11, 12};
-        ... 
-        ... // define the three domains
-        ... 
-        ... Plane Surface(1) = {2, 1};
-        ... Plane Surface(2) = {1, 3};
-        ... Plane Surface(3) = {3};
-        ... 
-        ... // label the three domains
-        ... 
-        ... // attention: if you use any "Physical" labels, you *must* label 
-        ... // all elements that correspond to FiPy Cells (Physical Surace in 2D 
-        ... // and Physical Volume in 3D) or Gmsh will not include them and FiPy
-        ... // will not be able to include them in the Mesh. 
-        ... 
-        ... // note: if you do not use any labels, all Cells will be included.
-        ... 
-        ... Physical Surface("Outer") = {1};
-        ... Physical Surface("Middle") = {2};
-        ... Physical Surface("Inner") = {3};
-        ... 
-        ... // label the "north-west" part of the exterior boundary
-        ... 
-        ... // note: you only need to label the Face elements 
-        ... // (Physical Line in 2D and Physical Surface in 3D) that correspond
-        ... // to boundaries you are interested in. FiPy does not need them to
-        ... // construct the Mesh.
-        ... 
-        ... Physical Line("NW") = {5};
-        ... ''' % locals())
-
-        >>> x, y = squaredCircle.cellCenters
-
-        >>> middle = ((x**2 + y**2 <= radius**2) 
-        ...           & ~((x > -side/2) & (x < side/2)
-        ...               & (y > -side/2) & (y < side/2)))
-
-        >>> print (middle == squaredCircle.physicalCells["Middle"]).all()
-        True
-        
-        >>> X, Y = squaredCircle.faceCenters
-
-        >>> NW = ((X**2 + Y**2 > (1.99*radius)**2) 
-        ...       & (X**2 + Y**2 < (2.01*radius)**2)
-        ...       & (X <= 0) & (Y >= 0))
-
-        >>> print (NW == squaredCircle.physicalFaces["NW"]).all()
-        True
         """
 
 class Gmsh2DIn3DSpace(Gmsh2D):
