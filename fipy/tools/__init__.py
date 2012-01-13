0f051be1e7b7c52ac1a0ab70e5f48e21f340d273
Daniel Wheeler <daniel.wheeler@nist.gov>
2012-01-13 20:57:51 +0000

    Forgot to include ImportError exception.
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/version-2_1@5109 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/tools/__init__.py
+++ fipy/tools/__init__.py
@@ -48,7 +48,7 @@ try:
         if '10.0.4' in PyTrilinos.version():
             try:
                 from mpi4py import MPI
-            except:
+            except ImportError:
                 raise Exception("Could not import mpi4py. The package mpi4py is a required package if you are using Trilinos on a Debian platform with Trilinos version 10.0.4 due to a Trilinos bug (see <http://matforge.org/fipy/ticket/420>). Try installing using 'easy_install mpi4py'.")
 
     from fipy.tools.commWrapper import CommWrapper
