0fa4137f3d929ab2a0849699da0f897732336992
Daniel Wheeler <daniel.wheeler@nist.gov>
2012-04-05 19:36:26 +0000

    Removing old imports
    git-svn-id: svn+ssh://code.matforge.org/fipy/branches/lsmlib@5197 d80e17d7-ff13-0410-a124-85740d801063

--- fipy/models/levelSet/surfactant/__init__.py
+++
@@ -1,6 +0,0 @@
-from fipy.models.levelSet.surfactant.surfactantVariable import *
-from fipy.models.levelSet.surfactant.matplotlibSurfactantViewer import *
-
-__all__ = []
-__all__.extend(surfactantVariable.__all__)
-__all__.extend(matplotlibSurfactantViewer.__all__)
