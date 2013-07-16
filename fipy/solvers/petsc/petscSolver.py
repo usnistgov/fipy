262edef58802fe5abf5c60966d306cc0b87b7ece
Jonathan Guyer <guyer@nist.gov>
2013-07-16 07:28:55 -0400

    Introduce maps to deal with parallel solves
    Incomplete transition to parallel. Works for some
    (`examples/diffusion/mesh1D.py`, `examples/diffusion/mesh20x20.py`), but
    not others (`examples/diffusion/circle.py`).
    
    Addresses #644

--- fipy/solvers/petsc/petscSolver.py
+++ fipy/solvers/petsc/petscSolver.py
@@ -35,6 +35,8 @@
 
 __docformat__ = 'restructuredtext'
 
+from petsc4py import PETSc
+
 from fipy.solvers.solver import Solver
 from fipy.tools import numerix
 
@@ -50,23 +52,75 @@ class PETScSolver(Solver):
         else:
             Solver.__init__(self, *args, **kwargs)
 
-    def _solve(self):
-        array = self.var.numericValue.ravel()
+    @property
+    def _globalMatrixAndVectors(self):
+        if not hasattr(self, 'globalVectors'):
+            globalMatrix = self.matrix
 
+            mesh = self.var.mesh
+            localNonOverlappingCellIDs = mesh._localNonOverlappingCellIDs
+            
+            ## The following conditional is required because empty indexing is not altogether functional.
+            ## This numpy.empty((0,))[[]] and this numpy.empty((0,))[...,[]] both work, but this
+            ## numpy.empty((3, 0))[...,[]] is broken.
+            if self.var.shape[-1] != 0:
+                s = (Ellipsis, localNonOverlappingCellIDs)
+            else:
+                s = (localNonOverlappingCellIDs,)
+                
+            nonOverlappingVector = PETSc.Vec().createWithArray(self.var[s].ravel(), comm=PETSc.COMM_WORLD)
+            
+            from fipy.variables.coupledCellVariable import _CoupledCellVariable
+
+            if isinstance(self.RHSvector, _CoupledCellVariable):
+                RHSvector = self.RHSvector[localNonOverlappingCellIDs]
+            else:
+                RHSvector = numerix.reshape(numerix.array(self.RHSvector), self.var.shape)[s].ravel()
+                
+                    
+            nonOverlappingRHSvector = PETSc.Vec().createWithArray(RHSvector)
+
+            del RHSvector
+            
+            overlappingVector = PETSc.Vec().createWithArray(self.var.value, comm=PETSc.COMM_SELF)
+
+            self.globalVectors = (globalMatrix, nonOverlappingVector, nonOverlappingRHSvector, overlappingVector)
+
+        return self.globalVectors
+
+    def _deleteGlobalMatrixAndVectors(self):
+        self.matrix.flush()
+        del self.globalVectors
+
+    def _solve(self):
         from fipy.terms import SolutionVariableNumberError
         
+        globalMatrix, nonOverlappingVector, nonOverlappingRHSvector, overlappingVector = self._globalMatrixAndVectors
+
         if ((self.matrix == 0)
-            or (self.matrix._shape[0] != self.matrix._shape[1])
-            or (self.matrix._shape[0] != len(array))):
+            or (self.matrix.matrix.sizes[0][1] != self.matrix.matrix.sizes[1][1])
+            or (self.matrix.matrix.sizes[0][1] != nonOverlappingVector.size)):
 
             raise SolutionVariableNumberError
-        
-        self._solve_(self.matrix, array, self.RHSvector)
-        factor = self.var.unit.factor
-        if factor != 1:
-            array /= self.var.unit.factor
 
-        self.var[:] = array.reshape(self.var.shape)
+        self._solve_(globalMatrix.matrix, 
+                     nonOverlappingVector, 
+                     nonOverlappingRHSvector)
+
+#         globalOverlappingCellIDs = self.var.mesh._globalOverlappingCellIDs
+#         self.var.value = numerix.reshape(nonOverlappingVector[globalOverlappingCellIDs.astype('int32')], self.var.shape)
+# #         with nonOverlappingVector.localForm() as overlappingVector:
+# #             self.var.value = numerix.reshape(overlappingVector.array, self.var.shape)
+
+	fr = PETSc.IS().createGeneral(self.var.mesh._globalOverlappingCellIDs.astype('int32'), PETSc.COMM_WORLD)
+        to = PETSc.IS().createGeneral(self.var.mesh._localOverlappingCellIDs.astype('int32'), PETSc.COMM_SELF)
+        scatter = PETSc.Scatter().create(nonOverlappingVector, fr, overlappingVector, to)
+        scatter.scatter(nonOverlappingVector, overlappingVector) #, mode='reverse')
+        self.var.value = numerix.reshape(numerix.array(overlappingVector), self.var.shape)
+        
+        self._deleteGlobalMatrixAndVectors()
+        del self.var
+        del self.RHSvector
             
     @property
     def _matrixClass(self):
