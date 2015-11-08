5857986cac12ef37b8f0f67baf4f119fbcd664ba
Mike Mueller <mmueller@python-academy.de>
2015-11-08 10:07:57 +0100

    Modify doctest. Return value is `True`  not `1` for numpy 1.10.1.

--- examples/diffusion/steadyState/otherMeshes/grid3Dinput.py
+++ examples/diffusion/steadyState/otherMeshes/grid3Dinput.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-## 
+##
  # ###################################################################
  #  FiPy - Python-based finite volume PDE solver
- # 
+ #
  #  FILE: "grid3Dinput.py"
  #
  #  Author: Jonathan Guyer <guyer@nist.gov>
@@ -11,7 +11,7 @@
  #  Author: James Warren   <jwarren@nist.gov>
  #    mail: NIST
  #     www: http://www.ctcms.nist.gov/fipy/
- #  
+ #
  # ========================================================================
  # This software was developed at the National Institute of Standards
  # and Technology by employees of the Federal Government in the course
@@ -22,13 +22,13 @@
  # other parties, and makes no guarantees, expressed or implied, about
  # its quality, reliability, or any other characteristic.  We would
  # appreciate acknowledgement if the software is used.
- # 
+ #
  # This software can be redistributed and/or modified freely
  # provided that any derivative works bear some notice that they are
  # derived from it, and any modified versions bear some notice that
  # they have been modified.
  # ========================================================================
- #  
+ #
  # ###################################################################
  ##
 
@@ -37,15 +37,15 @@
 
 """
 Test case for the Grid3D.
- 
+
    >>> DiffusionTerm().solve(var)
    >>> DiffusionTerm().solve(var2)
    >>> a = numerix.array(var.globalValue)
    >>> b = numerix.array(var2.globalValue)
    >>> c = numerix.ravel(numerix.array((b, b, b)))
    >>> print numerix.allclose(a, c)
-   1
-   
+   True
+
 """
 
 from fipy import *
@@ -91,7 +91,7 @@ var2.constrain(valueLeftRight, mesh2.facesRight)
 var2.constrain(valueBottomTop, mesh2.facesTop)
 var2.constrain(valueBottomTop, mesh2.facesBottom)
 
-eqn = DiffusionTerm()  
+eqn = DiffusionTerm()
 
 if __name__ == '__main__':
     eqn.solve(var2)
