915c963b4045ebb33f2c93a575a1fe09f00490c0
Jonathan Guyer <guyer@nist.gov>
2017-01-28 11:58:59 -0500

    Fixed name of script

--- examples/benchmarking/solvers/dispatch.sh
+++ examples/benchmarking/solvers/dispatch.sh
@@ -4,6 +4,6 @@ for solver in trilinos scipy pysparse
 do
     for size in 100 1000 10000 100000 1000000 10000000
     do
-	qsub -cwd pcgscript.sh "--$solver" --numberOfElements=$size
+	qsub -cwd script.sh "--$solver" --numberOfElements=$size
     done
 done
