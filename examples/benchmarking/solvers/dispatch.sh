2d978ec8e205aa024008c56147d1f943af84ae1f
Jonathan Guyer <guyer@nist.gov>
2017-01-28 11:57:11 -0500

    Move solver benchmarking scripts into their own directory

---
+++ examples/benchmarking/solvers/dispatch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for solver in trilinos scipy pysparse
+do
+    for size in 100 1000 10000 100000 1000000 10000000
+    do
+	qsub -cwd pcgscript.sh "--$solver" --numberOfElements=$size
+    done
+done
