6247b6ad0b2ccb3056817caa1062a49b1bd56320
Jonathan Guyer <guyer@nist.gov>
2017-02-09 16:30:37 -0500

    Switch to argparse from fipy.tools.parser

--- examples/benchmarking/solvers/solvers.py
+++ examples/benchmarking/solvers/solvers.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 import uuid 
 
@@ -5,15 +6,27 @@ import datreant.core as dtr
 
 import fipy as fp
 from fipy.tools import numerix
-from fipy.tools.parser import parse
 from fipy.tools import parallelComm
 
-output = parse('--output', action='store',
-               type='string', default=str(uuid.uuid4()))
+parser = argparse.ArgumentParser()
+parser.add_argument("--output", help="directory to store results in",
+                    default=str(uuid.uuid4()))
+parser.add_argument("--numberOfElements", help="number of total cells in a Grid2D",
+                    type=int, default=10000)
+parser.add_argument("--sweeps", help="number of nonlinear sweeps to take",
+                    type=int, default=10)
+parser.add_argument("--iterations", help="maximum number of linear iterations to take for each sweep",
+                    type=int, default=1000)
+parser.add_argument("--tolerance", help="linear solver tolerance",
+                    type=float, default=1e-10)
+parser.add_argument("--writeFiles", help="whether to write solution values and matrix to OUTPUT",
+                    action='store_true')
+
+args, unknowns = parser.parse_known_args()
 
 if parallelComm.procID == 0:
-    print "storing results in {0}".format(output)
-    data = dtr.Treant(output)
+    print "storing results in {0}".format(args.output)
+    data = dtr.Treant(args.output)
 else:
     class dummyTreant(object):
         categories = dict()
@@ -21,29 +34,14 @@ else:
     data = dummyTreant()
 
 data.categories['processes'] = parallelComm.Nproc
-
-numberOfElements = parse('--numberOfElements', action='store',
-                         type='int', default=10000)
-data.categories['numberOfElements'] = numberOfElements
-
-tolerance =  parse('--tolerance', action='store',
-                   type='float', default=1e-10)
-data.categories['tolerance'] = tolerance
-
-sweeps =  parse('--sweeps', action='store',
-                   type='int', default=10)
-data.categories['sweeps'] = sweeps
-
-iterations =  parse('--iterations', action='store',
-                   type='int', default=1000)
-data.categories['iterations'] = iterations
-
-writeFiles = parse('--writeFiles', action='store_true', default=False)
-
+data.categories['numberOfElements'] = args.numberOfElements
+data.categories['sweeps'] = args.sweeps
+data.categories['iterations'] = args.iterations
+data.categories['tolerance'] = args.tolerance
 data.categories['solver'] = fp.solvers.solver
+data.categories['script'] = __file__
 
-
-N = int(numerix.sqrt(numberOfElements))
+N = int(numerix.sqrt(args.numberOfElements))
 mesh = fp.Grid2D(nx=N, Lx=1., ny=N, Ly=1.)
 
 var = fp.CellVariable(mesh=mesh, value=1., hasOld=True)
@@ -52,21 +50,21 @@ var.constrain(0., where=mesh.facesRight)
 
 eq = fp.TransientTerm() == fp.DiffusionTerm(coeff=var)
 
-solver = fp.LinearPCGSolver(tolerance=tolerance, iterations=iterations, precon=None)
+solver = fp.LinearPCGSolver(tolerance=args.tolerance, iterations=args.iterations, precon=None)
 if fp.solvers.solver == "trilinos":
     # PySparse does b-normalization for (P)CG
     solver.convergenceCheck = solver.AZ_rhs
 
 start = time.clock()
 
-for sweep in range(sweeps):
+for sweep in range(args.sweeps):
     eq.cacheMatrix()
     eq.cacheRHSvector()
     res = eq.sweep(var=var, dt=1., solver=solver)
     
     data.categories['sweep {0} - iterations'.format(sweep)] = solver.status['iterations']
     
-    if writeFiles and parallelComm.procID == 0:
+    if args.writeFiles and parallelComm.procID == 0:
         eq.matrix.exportMmf(data["sweep{0}.mtx".format(sweep)].make().abspath)
         fp.tools.dump.write((var, eq.RHSvector), 
                             filename=data["sweep{0}.tar.gz".format(sweep)].make().abspath)
