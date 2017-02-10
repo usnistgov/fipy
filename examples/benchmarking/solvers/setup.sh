de21740ecfc7f9c3312ff129d85bee177b68d459
Jonathan Guyer <guyer@nist.gov>
2017-02-09 22:03:38 -0500

    Shift workload from setup.sh to dispatch.sh

--- examples/benchmarking/solvers/setup.sh
+++ examples/benchmarking/solvers/setup.sh
@@ -2,20 +2,14 @@
 
 USAGE="usage: $0 [-h] [--env ENV] [--np NP] [--] SCRIPT [ARGS]
 
-activates the appropriate conda environment and calls python on SCRIPT
-
-positional arguments:
-  SCRIPT      Python script to launch (expected to be in same
-              directory as $0)
-  ARGS        Arguments to pass to SCRIPT
+activates the appropriate conda environment and calls python on SCRIPT 
+with ARGS
 
 optional arguments:
+  -h, --help  show this help message and exit
   --env ENV   Conda environment to activate before invoking SCRIPT
-              (default: fipy)
-  --np NP     Number of processes to invoke SCRIPT with (default: 1)
-  -h, --help  show this help message and exit"
+              (default: fipy)"
 
-NP=1
 ENV=fipy
 
 while [[ $# > 0 ]] && [[ $1 == -* ]]
@@ -23,10 +17,6 @@ do
     case "$1" in
         --env)
             ENV="$2"
-            shift # option has parameter
-            ;;
-        --np)
-            NP="$2"
             shift # option has parameter
             ;;
         -h|--help)
@@ -47,21 +37,10 @@ do
     shift # option(s) fully processed, proceed to next input argument
 done
 
-echo "$@"
-
 if [[ "$#" < 1 ]]; then
     echo "$USAGE"
     exit 1
 fi
 
-SCRIPT=$1
-shift
-
-if [[ $NP > 1 ]]; then
-    MPI="mpirun -np ${NP}"
-else
-    MPI=""
-fi
-
 source activate $ENV
-${MPI} python "${BASH_SOURCE%/*}/${SCRIPT}" "$@"
+$@
