2d6bf47ba5cfaf68b3f798ade0149bf535c0ee54
Jonathan Guyer <guyer@nist.gov>
2018-01-12 14:47:27 -0500

    Factor out instruction parsing
    This needs to be handled differently in Python 2.x and Py3k.
    First step is to factor out Py2k variant.
    
    Addresses #545

--- fipy/variables/operatorVariable.py
+++ fipy/variables/operatorVariable.py
@@ -34,6 +34,7 @@ __docformat__ = 'restructuredtext'
 
 __all__ = []
 
+import dis
 import sys
 
 from fipy.variables.variable import Variable
@@ -106,57 +107,59 @@ def _OperatorVariableClass(baseClass=object):
               - `style`: one of `'__repr__'`, `'name'`, `'TeX'`, `'C'`
 
             """
-            import opcode
-
-            def __var(i):
-                v = self.var[i]
-                if style == "__repr__":
-                    result = repr(v)
-                elif style == "name":
-                    if isinstance(v, Variable):
-                        result = v.name
-                        if len(result) == 0:
-                            # The string form of a variable
-                            # would probably be too long and messy.
-                            # Just give shorthand.
-                            result = "%s(...)" % v.__class__.__name__
-                    elif type(v) in (type(1), type(1.)):
-                        result = repr(v)
-                    else:
-                        # The string form of anything but a
-                        # number would be too long and messy.
-                        # Just give shorthand.
-                        result = "<...>"
-
-                elif style == "TeX":
-                    raise Exception, "TeX style not yet implemented"
-                elif style == "C":
-                    if not v._isCached():
-                        result = v._getCstring(argDict, id=id + str(i), freshen=freshen)
-                        if isinstance(v, Variable):
-                            v._value = None
-                        else:
-                            v.value = None
-                    else:
-                        result = v._variableClass._getCstring(v, argDict,
-                                                                   id=id + str(i),
-                                                                   freshen=False)
-                else:
-                    raise SyntaxError, "Unknown style: %s" % style
-
-                return result
-
             if isinstance(self.op, numerix.ufunc):
-                return "%s(%s)" % (self.op.__name__, ", ".join([__var(i) for i in range(len(self.var))]))
+                return "%s(%s)" % (self.op.__name__, ", ".join([self.__var(i) for i in range(len(self.var))]))
 
-            def _getByteCodes():
-                if sys.version_info < (3,0):
-                    return [ord(byte) for byte in self.op.func_code.co_code]
+            try:
+                instructions = dis.get_instructions(self.op.func_code)
+                parseInstructions = self._py3kInstructions
+            except AttributeError:
+                instructions = [ord(byte) for byte in self.op.func_code.co_code]
+                parseInstructions = self._py2kInstructions
+
+            return parseInstructions(instructions, style=style)
+
+        def __var(self, i, style):
+            v = self.var[i]
+            if style == "__repr__":
+                result = repr(v)
+            elif style == "name":
+                if isinstance(v, Variable):
+                    result = v.name
+                    if len(result) == 0:
+                        # The string form of a variable
+                        # would probably be too long and messy.
+                        # Just give shorthand.
+                        result = "%s(...)" % v.__class__.__name__
+                elif type(v) in (type(1), type(1.)):
+                    result = repr(v)
                 else:
-                    return list(self.op.__code__.co_code)
+                    # The string form of anything but a
+                    # number would be too long and messy.
+                    # Just give shorthand.
+                    result = "<...>"
 
-            bytecodes = _getByteCodes()
+            elif style == "TeX":
+                raise Exception, "TeX style not yet implemented"
+            elif style == "C":
+                if not v._isCached():
+                    result = v._getCstring(argDict, id=id + str(i), freshen=freshen)
+                    if isinstance(v, Variable):
+                        v._value = None
+                    else:
+                        v.value = None
+                else:
+                    result = v._variableClass._getCstring(v, argDict,
+                                                               id=id + str(i),
+                                                               freshen=False)
+            else:
+                raise SyntaxError, "Unknown style: %s" % style
 
+            return result
+
+        def _py2kInstructions(self, bytecodes, style):
+            import opcode
+            
             def _popIndex():
                 return bytecodes.pop(0) + bytecodes.pop(0) * 256
 
@@ -193,7 +196,7 @@ def _OperatorVariableClass(baseClass=object):
                     counter = _popIndex()
                     stack.append(self.op.func_code.co_names[counter])
                 elif opcode.opname[bytecode] == 'LOAD_FAST':
-                    stack.append(__var(_popIndex()))
+                    stack.append(self.__var(_popIndex(), style=style))
                 elif opcode.opname[bytecode] == 'CALL_FUNCTION':
                     args = []
                     for j in range(bytecodes.pop(1)):
@@ -214,7 +217,7 @@ def _OperatorVariableClass(baseClass=object):
                     raise SyntaxError("Unknown bytecode: %s in %s of %s" % (
                        repr(bytecode),
                        repr([bytecode] + bytecodes),
-                       repr(_getByteCode()))
+                       repr(_getByteCode())))
 
         @property
         def _varProxy(self):
