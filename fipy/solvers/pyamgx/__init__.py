d902bbd11e36eeb01d8ecb09c7acb3175bc83963
Ashwin Srinath <atrikut@clemson.edu>
2018-05-03 12:44:58 -0400

    Import pyamgx linear solvers in __init__.py and set DefaultSolvers, DummySolver, and GeneralSolver

--- fipy/solvers/pyamgx/__init__.py
+++ fipy/solvers/pyamgx/__init__.py
@@ -3,16 +3,24 @@ import atexit
 import pyamgx
 
 from fipy.solvers.pyamgx.pyAMGXSolver import *
-from fipy.solvers.pyamgx.linearCGSSolver import *
+from fipy.solvers.pyamgx.linearLUSolver import *
+from fipy.solvers.pyamgx.linearPCGSolver import *
 from fipy.solvers.pyamgx.linearGMRESSolver import *
 
 pyamgx.initialize()
 atexit.register(pyamgx.finalize)
 
-DefaultSolver = LinearGMRESSolver
+DefaultSolver = LinearPCGSolver
+DefaultAsymmetricSolver = LinearGMRESSolver
+DummySolver = LinearGMRESSolver
+GeneralSolver = LinearGMRESSolver
 
 __all__ = ["DefaultSolver",
+           "DummySolver",
+           "DefaultAsymmetricSolver",
+           "GeneralSolver"
           ]
 
-__all__.extend(linearCGSSolver.__all__)
+__all__.extend(linearPCGSolver.__all__)
 __all__.extend(linearGMRESSolver.__all__)
+__all__.extend(linearLUSolver.__all__)
