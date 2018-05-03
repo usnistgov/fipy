601dbbcf9af9ac3f3bf92fbb049e53fed0cf1cb5
Ashwin Srinath <atrikut@clemson.edu>
2018-05-03 12:41:50 -0400

    Fixes to pyAMGXSolver

--- fipy/solvers/pyamgx/pyAMGXSolver.py
+++ fipy/solvers/pyamgx/pyAMGXSolver.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import atexit
 
 import numpy
 from scipy.sparse import csr_matrix
@@ -12,29 +13,21 @@ from fipy.tools import numerix
 __all__ = ["PyAMGXSolver"]
 
 class PyAMGXSolver(Solver):
-    """
-    The `PyAMGXSolver` is a general interface to pyamgx.
-    """
-    def __init__(self, config_dict, *args, **kwargs):
-        """
-        Parameters
-        ----------
-        config_dict : dict
-            Dictionary specifying AMGX configuration options
-        """
-        self.config_dict = config_dict
-        self.cfg = pyamgx.Config().create_from_dict(self.config_dict)
+
+    def __init__(self, config_dict, tolerance=1e-10, iterations=2000):
+        self.cfg = pyamgx.Config().create_from_dict(config_dict)
         self.resources = pyamgx.Resources().create_simple(self.cfg)
         self.x_gpu = pyamgx.Vector().create(self.resources)
         self.b_gpu = pyamgx.Vector().create(self.resources)
         self.A_gpu = pyamgx.Matrix().create(self.resources)
         self.solver = pyamgx.Solver().create(self.resources, self.cfg)
+        super(PyAMGXSolver, self).__init__(tolerance=tolerance, iterations=iterations)
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.solver.destroy()
+    def __exit__(self):
         self.A_gpu.destroy()
         self.b_gpu.destroy()
         self.x_gpu.destroy()
+        self.solver.destroy()
         self.resources.destroy()
         self.cfg.destroy()
 
@@ -46,7 +39,6 @@ class PyAMGXSolver(Solver):
         self.var = var
         self.matrix = matrix
         self.RHSvector = RHSvector
-
         self.A_gpu.upload_CSR(self.matrix.matrix)
         self.solver.setup(self.A_gpu)
 
@@ -60,9 +52,10 @@ class PyAMGXSolver(Solver):
 
         # download values from GPU to CPU
         self.x_gpu.download(x)
+        return x
 
     def _solve(self):
-        self._solve_(self.matrix, self.var.ravel(), numerix.array(self.RHSvector))
-            
-    def _canSolveAsymmetric(self):
-        return False
+         if self.var.mesh.communicator.Nproc > 1:
+             raise Exception("SciPy solvers cannot be used with multiple processors")
+
+         self.var[:] = numerix.reshape(self._solve_(self.matrix, self.var.ravel(), numerix.array(self.RHSvector)), self.var.shape)
