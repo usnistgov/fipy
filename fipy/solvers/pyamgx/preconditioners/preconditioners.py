d4ba97074b629161adc86a5283f18577a0bbcc8b
Ashwin Srinath <atrikut@clemson.edu>
2018-05-11 14:44:55 -0400

    pyamgx: Expose more AMGX preconditioners

--- fipy/solvers/pyamgx/preconditioners/preconditioners.py
+++ fipy/solvers/pyamgx/preconditioners/preconditioners.py
@@ -1,18 +1,22 @@
 import copy
 
-__all__ = ["AMGPreconditioner",
+__all__ = ["AggregationAMGPreconditioner",
+           "ClassicalAMGPreconditioner",
            "CGPreconditioner",
            "BiCGStabPreconditioner",
            "FGMRESPreconditioner",
            "BlockJacobiPreconditioner",
-           "MultiColorDILUPreconditioner"]
+           "MultiColorDILUPreconditioner",
+           "PolynomialPreconditioner",
+           "MultiColorGSPreconditioner"]
 
 class Preconditioner:
-    def __init__(self, preconditioner_type):
+    def __init__(self, preconditioner_type, **kwargs):
         self.config_dict = {
             "solver": preconditioner_type,
             "max_iters": 1
         }
+        self.config_dict.update(kwargs)
     def __call__(self, **kwargs):
         """
         :Parameters:
@@ -21,9 +25,12 @@ class Preconditioner:
         self.config_dict.update(kwargs)
         return copy.copy(self.config_dict)
 
-AMGPreconditioner = Preconditioner("AMG")
+AggregationAMGPreconditioner = Preconditioner("AMG", algorithm="AGGREGATION", selector="SIZE_2")
+ClassicalAMGPreconditioner = Preconditioner("AMG")
 CGPreconditioner = Preconditioner("PCG")
 BiCGStabPreconditioner = Preconditioner("PCIBCGSTAB")
 FGMRESPreconditioner = Preconditioner("FGMRES")
 BlockJacobiPreconditioner = Preconditioner("BLOCK_JACOBI")
 MultiColorDILUPreconditioner = Preconditioner("MULTICOLOR_DILU")
+PolynomialPreconditioner = Preconditioner("POLYNOMIAL")
+MultiColorGSPreconditioner = Preconditioner("MULTICOLOR_GS")
