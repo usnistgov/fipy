3353d07e598fab950d180fe4b84c926e0716282f
Jonathan Guyer <guyer@nist.gov>
2019-04-12 20:29:55 -0400

    Add docstring

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+""" FiPy is an object oriented, partial differential equation (PDE) solver
+
+FiPy is based on a standard finite volume (FV) approach.  The framework has
+been developed in the Materials Science and Engineering Division (MSED) and
+Center for Theoretical and Computational Materials Science (CTCMS), in the
+Material Measurement Laboratory (MML) at the National Institute of
+Standards and Technology (NIST).
+"""
+
 from setuptools import setup, find_packages
 from setuptools.command.test import test as _test
 
