5df6135249bfbf3a3cd015f5a65c5f0c107994f8
Jonathan Guyer <guyer@nist.gov>
2019-04-21 08:47:24 -0400

    Reformat by black

--- _setup/release.py
+++ _setup/release.py
@@ -19,8 +19,7 @@ class release(Command):
 
     # List of option tuples: long name, short name (None if no short
     # name), and help string.
-    user_options = [('version=', None,
-                     "Version string to tag and assign to release")]
+    user_options = [("version=", None, "Version string to tag and assign to release")]
 
     def initialize_options(self):
         import versioneer
@@ -33,28 +32,28 @@ class release(Command):
     def _build_unix_archive(self):
         os.remove("MANIFEST")
         shutil.copyfile("MANIFEST-UNIX.in", "MANIFEST.in")
-        run_setup('setup.py', ['sdist'])
+        run_setup("setup.py", ["sdist"])
         os.remove("MANIFEST.in")
 
     def _build_windows_archive(self):
         os.remove("MANIFEST")
-        run_setup('setup.py', ['bdist', '--formats=wininst'])
+        run_setup("setup.py", ["bdist", "--formats=wininst"])
 
         os.remove("MANIFEST")
         fname = "FiPy-{}.win32.exe".format(self.version)
         os.symlink(os.path.join("dist", fname), ".")
         shutil.copyfile("MANIFEST-WINDOWS.in", "MANIFEST.in")
-        run_setup('setup.py', ['sdist', '--dist-dir=dist-windows', '--formats=zip'])
+        run_setup("setup.py", ["sdist", "--dist-dir=dist-windows", "--formats=zip"])
         os.unlink(fname)
-        shutil.move(os.path.join("dist-windows",
-                                 "FiPy-{}.zip".format(self.version)),
-                    os.path.join("dist",
-                                 "FiPy-{}.win32.zip".format(self.version)))
+        shutil.move(
+            os.path.join("dist-windows", "FiPy-{}.zip".format(self.version)),
+            os.path.join("dist", "FiPy-{}.win32.zip".format(self.version)),
+        )
         os.remove("MANIFEST.in")
 
     def run(self):
-        run_setup('setup.py', ['bdist_egg'])
-        run_setup('setup.py', ['build_docs', '--pdf', '--html', '--cathartic'])
+        run_setup("setup.py", ["bdist_egg"])
+        run_setup("setup.py", ["build_docs", "--pdf", "--html", "--cathartic"])
 
         self._build_unix_archive()
         self._build_windows_archive()
