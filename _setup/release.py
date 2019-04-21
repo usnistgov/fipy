7470af47d6c783c75c10d2c928c3085e5edc1adc
Jonathan Guyer <guyer@nist.gov>
2019-04-21 08:44:46 -0400

    Fix pylint errors

--- _setup/release.py
+++ _setup/release.py
@@ -1,20 +1,26 @@
+"""setuptools command to prepare FiPy for release"""
+
 from distutils.core import Command
 import os
-from setuptools.sandbox import run_setup
 import shutil
+
+from setuptools.sandbox import run_setup
 
 
 __all__ = ["release"]
 
 
 class release(Command):
+    """Prepare FiPy for release
 
-    description = "prepare the FiPy release artifacts"
+    Generates tarball and excutable Windows installer"""
+
+    description = "Prepare the FiPy release artifacts"
 
     # List of option tuples: long name, short name (None if no short
     # name), and help string.
     user_options = [('version=', None,
-                    "Version string to tag and assign to release")]
+                     "Version string to tag and assign to release")]
 
     def initialize_options(self):
         import versioneer
@@ -24,13 +30,13 @@ class release(Command):
     def finalize_options(self):
         pass
 
-    def _buildUNIXArchive(self):
+    def _build_unix_archive(self):
         os.remove("MANIFEST")
         shutil.copyfile("MANIFEST-UNIX.in", "MANIFEST.in")
         run_setup('setup.py', ['sdist'])
         os.remove("MANIFEST.in")
 
-    def _buildWindowsArchive(self):
+    def _build_windows_archive(self):
         os.remove("MANIFEST")
         run_setup('setup.py', ['bdist', '--formats=wininst'])
 
@@ -41,7 +47,7 @@ class release(Command):
         run_setup('setup.py', ['sdist', '--dist-dir=dist-windows', '--formats=zip'])
         os.unlink(fname)
         shutil.move(os.path.join("dist-windows",
-                                 "FiPy-{}.zip".format(self.version)).
+                                 "FiPy-{}.zip".format(self.version)),
                     os.path.join("dist",
                                  "FiPy-{}.win32.zip".format(self.version)))
         os.remove("MANIFEST.in")
@@ -50,5 +56,5 @@ class release(Command):
         run_setup('setup.py', ['bdist_egg'])
         run_setup('setup.py', ['build_docs', '--pdf', '--html', '--cathartic'])
 
-        self._buildUNIXArchive()
-        self._buildWindowsArchive()
+        self._build_unix_archive()
+        self._build_windows_archive()
