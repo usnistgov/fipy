9bfc31f2118e15b7b1d94be639370f67ab2d1e4c
Jonathan Guyer <guyer@nist.gov>
2019-04-21 08:27:54 -0400

    Add release script to automate steps from ADMINISTRATA

---
+++ _setup/release.py
@@ -0,0 +1,55 @@
+from distutils.core import Command
+from setuptools.sandbox import run_setup
+import shutil
+
+import versioneer
+
+
+__all__ = ["release"]
+
+class release(Command):
+
+    description = "prepare the FiPy release artifacts"
+
+    # List of option tuples: long name, short name (None if no short
+    # name), and help string.
+    user_options = [('version=', None,
+                    "Version string to tag and assign to release")]
+
+    def initialize_options (self):
+        import versioneer
+        
+        self.version = versioneer.get_version()
+
+    def finalize_options (self):
+        pass
+
+    def _buildUNIXArchive(self):
+        os.remove("MANIFEST")
+        shutil.copyfile("MANIFEST-UNIX.in", "MANIFEST.in")
+        run_setup('setup.py', script_args=['sdist'])
+        os.remove("MANIFEST.in")
+        
+    def _buildWindowsArchive(self):
+        os.remove("MANIFEST")
+        run_setup('setup.py', script_args=['bdist', '--formats=wininst'])
+        
+        os.remove("MANIFEST")
+        fname = "FiPy-{}.win32.exe".format(self.version)
+        os.symlink(os.path.join("dist", fname), ".")
+        shutil.copyfile("MANIFEST-WINDOWS.in", "MANIFEST.in")
+        run_setup('setup.py', script_args=['sdist', '--dist-dir=dist-windows', '--formats=zip'])
+        os.unlink(fname)
+        shutil.move(os.path.join("dist-windows", 
+                                 "FiPy-{}.zip".format(self.version)).
+                    os.path.join("dist",
+                                 "FiPy-{}.win32.zip".format(self.version))) 
+        os.remove("MANIFEST.in")
+        
+    def run (self):
+        run_setup('setup.py', script_args=['bdist_egg'])
+        run_setup('setup.py', script_args=['build_docs', '--pdf'. '--html', '--cathartic'])
+
+        self._buildUNIXArchive()
+        self._buildWindowsArchive()
+        
