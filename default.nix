b8b673508e29df8d6cbe6255a2ce19bda26409eb
Daniel Wheeler <daniel.wheeler2@gmail.com>
2019-06-25 16:34:25 -0400

    Move contents of nix/build.nix to default.nix
    Simplify Nix recipes using a command line argument to nix-shell for
    the Python version.

--- default.nix
+++ default.nix
@@ -1,2 +1,65 @@
-{ nixpkgs ? import ./nix/nixpkgs_version.nix }:
-import ./nix/py2.nix { inherit nixpkgs; }
+# Usage:
+#
+#     $ nix-shell --pure --argstr python_version 36
+#
+# to use Python version 3.6.
+
+
+{ nixpkgs ? (import ./nix/nixpkgs_version.nix), python_version ? "27" }:
+let
+  name = "python" + python_version + "Packages";
+  pypkgs = nixpkgs.${name};
+  pysparse = if (python_version == "27") then
+    (import ./nix/pysparse.nix { inherit nixpkgs pypkgs; })
+  else
+    null;
+  gmsh = import ./nix/gmsh.nix { inherit nixpkgs; };
+  skfmm = import ./nix/skfmm.nix { inherit nixpkgs pypkgs; };
+in
+  pypkgs.buildPythonPackage rec {
+    pname = "fipy";
+    version = "3.1.3.dev";
+    env = nixpkgs.buildEnv { name=pname; paths=buildInputs; };
+    buildInputs = [
+      pypkgs.pip
+      pypkgs.python
+      pypkgs.numpy
+      pypkgs.scipy
+      gmsh
+      skfmm
+      pysparse
+      pypkgs.matplotlib
+      pypkgs.tkinter
+      nixpkgs.pkgs.git
+      nixpkgs.imagemagick
+      pypkgs.future
+    ];
+    src=builtins.filterSource (path: type: type != "directory" || baseNameOf path != ".git") ./.;
+    # src=./..;
+    doCheck=false;
+    meta = {
+      homepage = "https://www.ctcms.nist.gov/fipy/";
+      description = "A Finite Volume PDE Solver Using Python";
+      version = version;
+      license = nixpkgs.stdenv.lib.licenses.free;
+    };
+    catchConflicts=false;
+    postShellHook = ''
+      SOURCE_DATE_EPOCH=$(date +%s)
+      export PYTHONUSERBASE=$PWD/.local
+      export USER_SITE=`python -c "import site; print(site.USER_SITE)"`
+      export PYTHONPATH=$PYTHONPATH:$USER_SITE
+      export PATH=$PATH:$PYTHONUSERBASE/bin
+
+      ## To build the docs
+      # pip install --user sphinx
+      # pip install --user sphinxcontrib-bibtex
+      # pip install --user git+https://github.com/thewtex/sphinx-contrib.git#subdirectory=traclinks
+      # required for embedded plots in documentation
+      # pip install --user pandas
+      # python setup.py build_docs --html
+
+      ## To build PyAMG add nixpkgs.gcc to buildInputs and then
+      # pip install --user pyamg
+    '';
+  }
