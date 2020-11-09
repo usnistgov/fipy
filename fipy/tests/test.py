e2fa921e292397469b80a49cc7e03ea3b70e2e27
Jonathan Guyer <guyer@nist.gov>
2020-11-09 18:08:42 -0500

    Tabulate versions of FiPy dependencies (#763)
    Add python interpreter and indicate which solver suite is in use

--- fipy/tests/test.py
+++ fipy/tests/test.py
@@ -6,6 +6,7 @@ from future.utils import text_to_native_str
 from future.utils import string_types
 import unittest
 import warnings
+import sys
 
 __all__ = [text_to_native_str("test")]
 
@@ -146,57 +147,70 @@ class test(_test):
             yield "examples.test._suite"
 
     def printPackageInfo(self):
+        packages = ['python']
+        versions = [sys.version.replace('\n', '| ')]
 
         for pkg in ['fipy', 'numpy', 'pysparse', 'scipy', 'matplotlib', 'mpi4py', 'petsc4py', 'pyamgx']:
+            packages.append(pkg)
 
             try:
                 mod = __import__(pkg)
 
-                if hasattr(mod, '__version__'):
-                    print(pkg, 'version', mod.__version__)
-                else:
-                    print(pkg, 'version not available')
-
+                versions.append(mod.__version__)
             except ImportError as e:
-                print(pkg, 'is not installed')
+                versions.append('not installed')
 
             except Exception as e:
-                print(pkg, 'version check failed:', e)
+                versions.append('version check failed: {}'.format(e))
 
         ## PyTrilinos
+        packages.append('PyTrilinos')
         try:
             import PyTrilinos
-            print(PyTrilinos.version())
+            versions.append(PyTrilinos.version())
         except ImportError as e:
-            print('PyTrilinos is not installed')
+            versions.append('not installed')
         except Exception as e:
-            print('PyTrilinos version check failed:', e)
+            versions.append('version check failed: {}'.format(e))
 
         ## Mayavi uses a non-standard approach for storing its version number.
+        packages.append('mayavi')
         try:
             from mayavi.__version__ import __version__ as mayaviversion
-            print('mayavi version', mayaviversion)
+            versions.append(mayaviversion)
         except ImportError as e:
             try:
                 from enthought.mayavi.__version__ import __version__ as mayaviversion
-                print('enthought.mayavi version', mayaviversion)
+                versions.append(mayaviversion)
             except ImportError as e:
-                print('enthought.mayavi is not installed')
-            except Exception as e:
-                print('enthought.mayavi version check failed:', e)
+                versions.append('not installed')
         except Exception as e:
-            print('mayavi version check failed:', e)
+            versions.append('version check failed: {}'.format(e))
 
         ## Gmsh version
+        packages.append('gmsh')
         try:
             from fipy.meshes.gmshMesh import gmshVersion
             gmshversion = gmshVersion()
             if gmshversion is None:
-                print('gmsh is not installed')
+                versions.append('not installed')
             else:
-                print('gmsh version', gmshversion)
+                versions.append(gmshversion)
         except Exception as e:
-            print('gmsh version check failed:', e)
+            versions.append('version check failed: {}'.format(e))
+
+        packages.append("solver")
+        try:
+            from fipy.solvers import solver
+            versions.append(solver)
+        except Exception as e:
+            versions.append(str(e))
+
+        print()
+        package_width = max(len(word) for word in packages)
+        for package, version in zip(packages, versions):
+            print("  ".join((package.ljust(package_width), version)))
+        print()
 
     def run_tests(self):
         import sys
