005ebe3463d61aac354ee4e1c846b1339c27e0a6
Jonathan Guyer <guyer@nist.gov>
2022-07-08 14:03:30 -0400

    Add classes to pyamgx namespace

--- fipy/solvers/pyamgx/__init__.py
+++ fipy/solvers/pyamgx/__init__.py
@@ -36,7 +36,9 @@ from future.utils import text_to_native_str
 __all__ = [text_to_native_str(n) for n in __all__]
 
 __all__.extend(linearCGSolver.__all__)
+__all__.extend(linearGMRESSolver.__all__)
 __all__.extend(linearFGMRESSolver.__all__)
 __all__.extend(linearBiCGStabSolver.__all__)
 __all__.extend(linearLUSolver.__all__)
 __all__.extend(aggregationAMGSolver.__all__)
+__all__.extend(classicalAMGSolver.__all__)
