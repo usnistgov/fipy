3a201a7c0eaa74dfc2506ad6a40a380397d8eb3e
Jonathan Guyer <guyer@nist.gov>
2022-07-12 11:12:28 -0500

    Fix inheritance for PyAMG preconditioners

--- fipy/solvers/pyAMG/preconditioners/smoothedAggregationPreconditioner.py
+++ fipy/solvers/pyAMG/preconditioners/smoothedAggregationPreconditioner.py
@@ -1,14 +1,15 @@
 from __future__ import unicode_literals
 from builtins import object
+
 from pyamg import smoothed_aggregation_solver
+
+from ...scipy.preconditioners.preconditioner import Preconditioner
 
 __all__ = ["SmoothedAggregationPreconditioner"]
 from future.utils import text_to_native_str
 __all__ = [text_to_native_str(n) for n in __all__]
 
-class SmoothedAggregationPreconditioner(object):
-    def __init__(self):
-        pass
+class SmoothedAggregationPreconditioner(Preconditioner):
 
     def _applyToMatrix(self, A):
         return smoothed_aggregation_solver(A).aspreconditioner(cycle='V')
