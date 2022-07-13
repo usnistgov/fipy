d215fc12ba797ce3ece51ae94cb2559ee3e7d420
Jonathan Guyer <guyer@nist.gov>
2022-07-13 11:28:21 -0500

    Fix names

--- fipy/solvers/pysparse/linearPCGSolver.py
+++ fipy/solvers/pysparse/linearPCGSolver.py
@@ -30,7 +30,7 @@ class LinearPCGSolver(LinearRHSSolver):
     solveFnc = staticmethod(krylov.pcg)
 
     def __init__(self, tolerance=1e-10, criterion="default",
-                 iterations=1000, precon=SsorPreconditioner()):
+                 iterations=1000, precon=SSORPreconditioner()):
         """
         Create a `LinearPCGSolver` object.
 
