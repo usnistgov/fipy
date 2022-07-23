ed86463c11d8aa780876a29a40a1239dc7618e1f
Jonathan Guyer <guyer@nist.gov>
2022-07-23 09:52:28 -0400

    Strip out notebookisms and Viewers

--- examples/benchmarking/solvers/nucleation.py
+++ examples/benchmarking/solvers/nucleation.py
@@ -7,15 +7,7 @@
 # 
 # Based on problem 2.3 in *Benchmark problems for nucleation*, Tamás Pusztai, September 25, 2019
 
-# **Do not edit `benchmark8c.py`**. Generate the batch-runnable file from the notebook with
-# ```bash
-# jupyter nbconvert benchmark8c.ipynb --to python
-# ```
-
 # ## Import Python modules
-
-# In[ ]:
-
 
 import os
 import re
@@ -29,50 +21,8 @@ from fipy.tools import parallelComm
 from fipy.meshes.factoryMeshes import _dnl
 
 
-# Jupyter notebook handles some things differently than from the commandline
-
-# In[ ]:
-
-
-try:
-    from IPython import get_ipython
-    isnotebook = get_ipython() is not None
-except:
-    isnotebook = False
-
-
 # ## Initialize
 # ### Load parameters
-
-# In[ ]:
-
-
-if isnotebook:
-    yamlfile = "params8c.yaml"
-else:
-    yamlfile = sys.argv[1]
-
-with open(yamlfile, 'r') as f:
-    if hasattr(yaml, "FullLoader"):
-        # PyYAML 5.1 deprecated the plain yaml.load(input) function
-        # https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation
-        params = yaml.load(f, Loader=yaml.FullLoader)
-    else:
-        params = yaml.load(f)
-
-
-# ### Set any parameters for interactive notebook
-
-# In[ ]:
-
-
-if isnotebook:
-    params['Lx'] = 100.
-    params['Ly'] = 100.
-    params['checkpoint_interval'] = 1.5 * params['dt']
-    params['savetime'] = 4 * params['dt'] 
-    params['totaltime'] = 100 * params['dt']
-#    params['restart'] = "t=0.0.tar.gz"
 
 
 # ### Initialize mesh and solution variables
@@ -83,9 +33,6 @@ if isnotebook:
 # 
 # Create a mesh based on parameters. Set
 # >  the domain size to 1000 × 1000... the spatial and temporal resolution by setting $\Delta x = \Delta y = 0.8$ and $\Delta t = 0.04$
-
-# In[ ]:
-
 
 checkpoint_interval = params['checkpoint_interval']
 savetime = params['savetime']
@@ -124,30 +71,16 @@ x, y = mesh.cellCenters[0], mesh.cellCenters[1]
 X, Y = mesh.faceCenters[0], mesh.faceCenters[1]
 
 
-# In[ ]:
-
-
-if isnotebook:
-    viewer = fp.Viewer(vars=phi, datamin=0., datamax=1.)
-    viewer.plot()
-
-
 # ## Define governing equation
 
 # > use only the nondimensional forms of the phase-field and nucleation equations, but without the tildes, for simplicity
 # 
 # > [Set] the driving force to $\Delta f = 1 / (6\sqrt{2})$
 
-# In[ ]:
-
-
 Delta_f = 1. / (6 * fp.numerix.sqrt(2.))
 
 
 # > $$r_c = \frac{1}{3\sqrt{2}}\frac{1}{\Delta f} = 2.0$$
-
-# In[ ]:
-
 
 rc = 2.0
 
@@ -187,8 +120,6 @@ rc = 2.0
 # \notag
 # \end{align}
 
-# In[ ]:
-
 
 mPhi = -2 * (1 - 2 * phi) + 30 * phi * (1 - phi) * Delta_f
 dmPhidPhi = 4 + 30 * (1 - 2 * phi) * Delta_f
@@ -204,8 +135,6 @@ eq = (fp.TransientTerm() ==
 # > \begin{align}
 # F[\phi] = \int\left[\frac{1}{2}(\nabla\phi)^2 + g(\phi) - \Delta f p(\phi)\right]\,dV \tag{6}
 # \end{align}
-
-# In[ ]:
 
 
 ftot = (0.5 * phi.grad.mag**2
@@ -223,8 +152,6 @@ F = ftot.cellVolumeAverage * volumes.sum()
 # \end{align}
 # to the $\phi$ values already in the domain, and handle the possible overlaps by setting $\phi = 1$ for all cells where $\phi > 1.$
 
-# In[ ]:
-
 
 def nucleus(x0, y0, r0):
     r = fp.numerix.sqrt((x - x0)**2 + (y - y0)**2)
@@ -238,8 +165,6 @@ def nucleus(x0, y0, r0):
 # or
 # 
 # > generate 100 random nucleation times in the range $t=0\dots100$ for adding the 100 seeds to the simulation domain.
-
-# In[ ]:
 
 
 if parallelComm.procID == 0:
@@ -259,11 +184,6 @@ nucleii = parallelComm.bcast(nucleii, root=0)
 
 # ## Setup output
 
-# ### Setup ouput storage
-
-# In[ ]:
-
-
 try:
     from sumatra.projects import load_project
     project = load_project(os.getcwd())
@@ -282,95 +202,6 @@ else:
         categories = dict()
 
     data = dummyTreant()
-
-
-# ### Create particle counter
-
-# In[ ]:
-
-
-from scipy import ndimage
-
-class LabelVariable(fp.CellVariable):
-    """Label features in `var` using scipy.ndimage.label
-    
-    Parameters
-    ----------
-    var : ~fipy.variables.cellVariable.CellVariable
-        Field to be labeled. Any values equal to or greater than `threshold`
-        are counted as features and values below are considered the background.
-        
-        .. important:
-           Only sensible if `var` is defined on a `...Grid...` Mesh.
-    structure : array_like, optional
-        A structuring element that defines feature connections.
-        `structure` must be centrosymmetric
-        (see ```scipy.ndimage.label`` Notes
-        <https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.label.html#scipy.ndimage.label>`_).
-        If no structuring element is provided,
-        one is automatically generated with a squared connectivity equal to
-        one.  That is, for a 2-D `input` array, the default structuring element
-        is::
-            [[0,1,0],
-             [1,1,1],
-             [0,1,0]]
-    threshold : float, optional
-        Boundary value between features (inclusive) and background.
-    dtype : date-type, optional
-        The desired data-type for the labels. Note that the type must be able
-        to store the largest label, or this Variable will raise an Exception.
-        Default: int.
-    """
-    def __init__(self, var, structure=None, threshold=0.5, dtype=int):
-        # We want our value to hold dtype,
-        # but if we pass an array, the CellVariable
-        # will probably be wonky
-        value = fp.numerix.array(0.).astype(dtype).item()
-        fp.CellVariable.__init__(self,
-                                 mesh=var.mesh,
-                                 value=value,
-                                 elementshape=var.shape[:-1])
-        self.var = self._requires(var)
-        self.structure = structure
-        self.threshold = threshold
-        self.dtype = dtype
-        self._num_features = None
-    
-    def _calcValue(self):
-        """Label features of `var`
-        
-        Side-effect: sets self._num_features
-        """
-        arr = (self.var.globalValue > self.threshold).astype(self.dtype)
-        shape = (self.var.mesh.args['nx'], self.var.mesh.args['ny'])
-        arr = arr.reshape(shape)
-        self._num_features = ndimage.label(input=arr,
-                                           structure=self.structure,
-                                           output=arr)
-        return arr.flat
-        
-    @property
-    def num_features(self):
-        """How many objects were found
-        """
-        if self.stale or not self._isCached() or self._num_features is None:
-            self._getValue()
-
-        return self._num_features
-
-
-# In[ ]:
-
-
-labels = LabelVariable(phi, threshold=0.5)
-
-
-# In[ ]:
-
-
-if isnotebook:
-    labelViewer = fp.Viewer(vars=labels, datamin=0, datamax=25)
-    labelViewer.plot()
 
 
 # ### Define output routines
@@ -479,14 +310,3 @@ for until in times:
               
     if elapsed in checkpoints:
         checkpoint(elapsed)
-              
-    if isnotebook:
-        viewer.plot()
-        labelViewer.plot()
-
-
-# In[ ]:
-
-
-
-
