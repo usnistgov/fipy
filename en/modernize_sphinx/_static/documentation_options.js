c48f441c18c95a5c04a64bcd130b01105ad8152c
GitHub Action <action@github.com>
2023-09-05 20:08:20 +0000

    Update documentation for modernize_sphinx@bcc35fe

--- en/modernize_sphinx/_static/documentation_options.js
+++ en/modernize_sphinx/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.4+277.g61d8667d',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.4.4+302.gbcc35fec',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
