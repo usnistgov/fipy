0daf00a2727b095f6487b954e4c0a00fb83ac3e8
GitHub Action <action@github.com>
2023-09-07 14:53:34 +0000

    Update documentation for modernize_sphinx@a9cacdd

--- en/modernize_sphinx/_static/documentation_options.js
+++ en/modernize_sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.4+330.g18ccfce9c',
+    VERSION: '3.4.4+331.ga9cacddb4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
