a48d14937bcf62b6cdbdab13ad1d262f2a84ab91
GitHub Action <action@github.com>
2023-12-04 16:18:42 +0000

    Update documentation for issue963-PETSc_3_20_0_broke_the_world@c68bea8

--- en/issue963-PETSc_3_20_0_broke_the_world/_static/documentation_options.js
+++ en/issue963-PETSc_3_20_0_broke_the_world/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.4+11.g0b2b295de',
+    VERSION: '3.4.4+12.gc68bea8cf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
