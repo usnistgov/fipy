298728f0eb717eab055cb1c0b4bf74b70a8b006f
GitHub Action <action@github.com>
2023-12-10 15:49:49 +0000

    Update documentation for environment_pinning@292b2b7

--- en/environment_pinning/_static/documentation_options.js
+++ en/environment_pinning/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.4+36.g24930878b',
+    VERSION: '3.4.4+37.g292b2b7fa',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
