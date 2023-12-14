b52a0cdbf5090aac8125034cadf0abee79c0f526
GitHub Action <action@github.com>
2023-12-14 03:14:37 +0000

    Update documentation for Fix_CONDA_ENVIRONMENT_in_Sphinx@31bb5b0

--- en/Fix_CONDA_ENVIRONMENT_in_Sphinx/_static/documentation_options.js
+++ en/Fix_CONDA_ENVIRONMENT_in_Sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.4+8.gdc930dc69',
+    VERSION: '3.4.4+9.g31bb5b0fe',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
