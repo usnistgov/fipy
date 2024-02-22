04d7a0075fb7e98b538f1217587bc47017588b9f
GitHub Action <action@github.com>
2024-02-22 15:49:24 +0000

    Update documentation for issue997-incompatible_with_Python_3_12@f9cd267

--- en/issue997-incompatible_with_Python_3_12/_static/documentation_options.js
+++ en/issue997-incompatible_with_Python_3_12/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.4+17.g1f3cf3c0f',
+    VERSION: '3.4.4+22.gf9cd26767',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
