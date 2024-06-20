usnistgov/fipy

commit 2eed4180e3a3a8d7b0899d708844be1ab0ee9b07

Replace `.getsctype()` with `.dtype`

fipy/terms/unaryTerm.py
@@ -106,7 +106,7 @@ class _UnaryTerm(Term):
 
     def _checkVar(self, var):
         if ((var is not None)
-            and (numerix.sctype2char(var.getsctype()) not in numerix.typecodes['Float'])):
+            and issubclass(var.dtype.type, numerix.floating)):
             import warnings
             warnings.warn("""sweep() or solve() are likely to produce erroneous results when `var` does not contain floats.""",
                           UserWarning, stacklevel=4)

fipy/tools/dimensions/physicalField.py
@@ -859,24 +859,21 @@ class PhysicalField(object):
                 unit = units[i]
             return tuple(result)
 
-    def getsctype(self, default=None):
+    @property
+    def dtype(self):
         """
 
         Returns the NumPy sctype of the underlying array.
 
-            >>> PhysicalField(1, 'm').getsctype() == numerix.array(1).dtype
+            >>> issubclass(PhysicalField(1, 'm').dtype.type, numerix.integer)
             True
-            >>> PhysicalField(1., 'm').getsctype() == numerix.array(1.).dtype
+            >>> issubclass(PhysicalField(1., 'm').dtype.type, numerix.floating)
             True
-            >>> PhysicalField((1, 1.), 'm').getsctype() == numerix.array((1., 1.)).dtype
+            >>> issubclass(PhysicalField((1, 1.), 'm').dtype.type, numerix.floating)
             True
 
         """
-
-        if not hasattr(self, 'typecode'):
-            self.typecode = numerix.asarray(self.numericValue).dtype
-
-        return self.typecode
+        return numerix.asarray(self.numericValue).dtype
 
     def _getUnit(self):
         """

fipy/variables/cellVariable.py
@@ -55,8 +55,7 @@ class CellVariable(MeshVariable):
             >>> b = c.old + 3
             >>> print(b)
             [2]
-            >>> from builtins import str
-            >>> print(str(b.getsctype()) == str(numerix.array(1).dtype)
+            >>> issubclass(b.dtype.type, numerix.integer)
             True
 
         replacing with the same thing is no problem

fipy/variables/coupledCellVariable.py
@@ -80,7 +80,7 @@ class _CoupledCellVariable(object):
         [6 7]
         >>> print(v2)
         [8 9]
-        >>> v.getsctype() == numerix.array(1).dtype
+        >>> issubclass(v.dtype.type, numerix.integer)
         True
 
         """
@@ -95,7 +95,8 @@ class _CoupledCellVariable(object):
     def __iter__(self):
         return iter(self.value)
 
-    def getsctype(self, default=None):
+    @property
+    def dtype(self):
         return self.numericValue.dtype
 
     def ravel(self):

fipy/variables/operatorVariable.py
@@ -386,11 +386,11 @@ def _testBinOp(self):
 
         >>> a = -(numerix.sin(Variable() * Variable()))
 
-    Check that `getTypeCode()` works as expected.
+    Check that `dtype` works as expected.
 
         >>> a = Variable(1.) * Variable(1)
-        >>> print(a.getsctype() == numerix.float64)
-        1
+        >>> issubclass(a.dtype.type, numerix.floating)
+        True
 
     The following test is to correct an `--inline` bug that was
     being thrown by the Cahn-Hilliard example. The fix for this

fipy/variables/variable.py
@@ -703,7 +703,7 @@ class Variable(object):
 
         """
         if where is not None:
-            tmp = numerix.empty(numerix.getShape(where), self.getsctype())
+            tmp = numerix.empty(numerix.getShape(where), self.dtype)
             tmp[:] = value
             tmp = numerix.where(where, tmp, self.value)
         else:
@@ -767,24 +767,21 @@ class Variable(object):
 
     shape = property(_getShape)
 
-    def getsctype(self, default=None):
+    @property
+    def dtype(self):
         """
 
-        Returns the Numpy `sctype` of the underlying array.
+        Returns the Numpy `dtype` of the underlying array.
 
-            >>> Variable(1).getsctype() == numerix.array(1).dtype
+            >>> issubclass(Variable(1).dtype.type, numerix.integer)
             True
-            >>> Variable(1.).getsctype() == numerix.array(1.).dtype
+            >>> issubclass(Variable(1.).dtype.type, numerix.floating)
             True
-            >>> Variable((1, 1.)).getsctype() == numerix.array((1., 1.)))
+            >>> issubclass(Variable((1, 1.)).dtype.type, numerix.floating)
             True
 
         """
-
-        if not hasattr(self, 'typecode'):
-            self.typecode = numerix.asarray(self.numericValue).dtypes
-
-        return self.typecode
+        return numerix.asarray(self.numericValue).dtype
 
     @property
     def itemsize(self):
@@ -926,10 +923,10 @@ class Variable(object):
             self.typecode = argDict['result'].dtype
         else:
             if self._value is None:
-                if self.getsctype() == numerix.bool_:
+                if self.dtype is numerix.dtype(bool):
                     argDict['result'] = numerix.empty(dim, numerix.int8)
                 else:
-                    argDict['result'] = numerix.empty(dim, self.getsctype())
+                    argDict['result'] = numerix.empty(dim, self.dtype)
             else:
                 argDict['result'] = self._value
 
@@ -943,8 +940,8 @@ class Variable(object):
             if resultShape == ():
                 argDict['result'] = numerix.reshape(argDict['result'], resultShape)
 
-            if self.getsctype() == numerix.bool_:
-                argDict['result'] = numerix.asarray(argDict['result'], dtype=self.getsctype())
+            if issubclass(self.dtype.type, numerix.bool):
+                argDict['result'] = numerix.asarray(argDict['result'], dtype=self.dtype)
 
         return argDict['result']
 

commit 729e2c5952e556149544dc2ff68fca529166a1a3

Fix polarity of floating CellVariable check
Broken in 2eed418

fipy/terms/unaryTerm.py
@@ -106,7 +106,7 @@ class _UnaryTerm(Term):
 
     def _checkVar(self, var):
         if ((var is not None)
-            and issubclass(var.dtype.type, numerix.floating)):
+            and not issubclass(var.dtype.type, numerix.floating)):
             import warnings
             warnings.warn("""sweep() or solve() are likely to produce erroneous results when `var` does not contain floats.""",
                           UserWarning, stacklevel=4)