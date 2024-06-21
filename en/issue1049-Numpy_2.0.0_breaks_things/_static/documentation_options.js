9d0f232403962865d41cb68c4e36d2f1e3efa5f3
GitHub Action <action@github.com>
2024-06-21 02:29:03 +0000

    Update documentation for issue1049-Numpy_2.0.0_breaks_things@0ce2a46

--- en/issue1049-Numpy_2.0.0_breaks_things/_static/documentation_options.js
+++ en/issue1049-Numpy_2.0.0_breaks_things/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.4+34.g3a1e3169d',
+    VERSION: '3.4.4+35.g0ce2a4603',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
