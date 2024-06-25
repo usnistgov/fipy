9e54ed0e347a616214eeb4cd5e69f3854c648d39
GitHub Action <action@github.com>
2024-06-25 22:04:32 +0000

    Update documentation for master@66774ea

--- en/master/_static/documentation_options.js
+++ en/master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.4+15.g62360760e',
+    VERSION: '3.4.4+16.g66774ea82',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
