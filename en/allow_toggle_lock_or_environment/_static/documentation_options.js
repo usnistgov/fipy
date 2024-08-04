459ba0b6fa51523b4496d9dcd070b0a3231f6a8b
GitHub Action <action@github.com>
2024-08-04 17:47:11 +0000

    Update documentation for allow_toggle_lock_or_environment@563cfd9

--- en/allow_toggle_lock_or_environment/_static/documentation_options.js
+++ en/allow_toggle_lock_or_environment/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.5+11.g9d3adabc0',
+    VERSION: '3.4.5+12.g563cfd9ca',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
