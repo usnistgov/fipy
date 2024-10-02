00ed39f4a995755f8038b95c3dc5b39ef5a371b7
GitHub Action <action@github.com>
2024-10-02 19:54:34 +0000

    Update documentation for issue1069-Cannot_dot_CellVariables@4a72b06

--- en/issue1069-Cannot_dot_CellVariables/_static/documentation_options.js
+++ en/issue1069-Cannot_dot_CellVariables/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.5+13.ge259b2e0b',
+    VERSION: '3.4.5+14.g4a72b0657',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
