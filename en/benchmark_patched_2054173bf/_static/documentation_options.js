addb3a1d2a53b3a408493721d32f24d7c7f9d78c
GitHub Action <action@github.com>
2025-02-07 14:22:24 +0000

    Update documentation for benchmark_patched_2054173bf@21c82d4

--- en/benchmark_patched_2054173bf/_static/documentation_options.js
+++ en/benchmark_patched_2054173bf/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.4+256.g53298da97',
+    VERSION: '3.4.4+257.g21c82d4c3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
