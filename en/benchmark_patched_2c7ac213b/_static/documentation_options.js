758c4f8be3de114cfd38498583e5efdb180b7753
GitHub Action <action@github.com>
2025-02-10 12:52:24 +0000

    Update documentation for benchmark_patched_2c7ac213b@6bf1eae

--- en/benchmark_patched_2c7ac213b/_static/documentation_options.js
+++ en/benchmark_patched_2c7ac213b/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.5+310.g62d38a3c6',
+    VERSION: '3.4.5+311.g6bf1eae87',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
