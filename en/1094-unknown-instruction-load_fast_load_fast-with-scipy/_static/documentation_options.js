078b09fef24326a639bd936c5c25a05f4bf3f7e3
GitHub Action <action@github.com>
2025-02-12 23:38:31 +0000

    Update documentation for 1094-unknown-instruction-load_fast_load_fast-with-scipy@eccda75

--- en/1094-unknown-instruction-load_fast_load_fast-with-scipy/_static/documentation_options.js
+++ en/1094-unknown-instruction-load_fast_load_fast-with-scipy/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.5+17.gfadab55ad',
+    VERSION: '3.4.5+18.geccda75ef',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
