a76520eb0d2badf2addaa904633dc602cc941a67
GitHub Action <action@github.com>
2025-05-23 16:34:08 +0000

    Update documentation for debug-conda-timeout@44ba2a2

--- en/debug-conda-timeout/_static/documentation_options.js
+++ en/debug-conda-timeout/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.5+27.g00946c08f',
+    VERSION: '3.4.5+28.g44ba2a228',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
