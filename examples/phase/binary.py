50d8f493807262486a7be1f5efdedf54afe7839d
Jonathan Guyer <guyer@nist.gov>
2025-06-05 15:18:51 -0400

    Factor out PhaseViewer

--- examples/phase/binary.py
+++ examples/phase/binary.py
@@ -476,59 +476,12 @@ We plot the result against the sharp interface solution
 
 >>> if __name__ == '__main__':
 ...     try:
-...         from fipy import Matplotlib1DViewer
-...         from scipy.optimize import leastsq # doctest: +SCIPY
-...         from matplotlib import pyplot as plt
-...
-...         class PhaseViewer(Matplotlib1DViewer):
-...             def __init__(self, phase, C, sharp, elapsed, title=None,
-...                          tmin=None, tmax=None, **kwlimits):
-...                 self.phase = phase
-...                 self.x = self.phase.mesh.cellCenters[0]
-...                 self.elapsed = elapsed
-...
-...                 fig = plt.figure(constrained_layout=True, figsize=[4, 6])
-...                 gs = fig.add_gridspec(ncols=1, nrows=3)
-...                 self.viewax = fig.add_subplot(gs[1:, 0])
-...                 self.viewax.set_xlabel(r"$x / \mathrm{cm}$")
-...                 self.posax = fig.add_subplot(gs[0, 0], sharex=self.viewax)
-...                 self.posax.set_ylabel(r"$t / \mathrm{s}$")
-...                 self.posax.label_outer()
-...                 self.posax.set_ylim(tmin, tmax)
-...
-...                 self.times = []
-...                 self.positions = []
-...                 self.line, = self.posax.semilogy([0.], [0.],
-...                                                  color="blue")
-...                 self.bug, = self.posax.semilogy([], [],
-...                                                 linestyle="", marker="o", color="red")
-...
-...                 super(PhaseViewer, self).__init__(vars=(phase, C, sharp),
-...                                                   axes=self.viewax,
-...                                                   **kwlimits)
-...
-...                 self.timer = self.viewax.text(0.00025, 0.2, "t = 0")
-...
-...             @staticmethod
-...             def tanhResiduals(p, y, x):
-...                 x0, d = p
-...                 return y - 0.5 * (1 - numerix.tanh((x - x0) / (2*d)))
-...
-...             def _plot(self):
-...                 (x0_fit, d_fit), msg = leastsq(self.tanhResiduals, [L/2., deltaA],
-...                                                args=(self.phase.globalValue,
-...                                                      self.x.globalValue))
-...                 self.positions.append(x0_fit)
-...                 self.times.append(float(self.elapsed))
-...                 self.line.set_data(self.positions, self.times)
-...                 self.bug.set_data([self.positions[-1]], [self.times[-1]])
-...
-...                 self.timer.set_text(r"$t = {:f}\\,\\mathrm{{s}}$".format(float(self.elapsed)))
-...
-...                 super(PhaseViewer, self)._plot()
+...         from examples.phase.phaseViewer import PhaseViewer
 ...
 ...         viewer = PhaseViewer(phase=phase, C=C, sharp=sharp,
-...                              elapsed=elapsed, tmin=1e-5, tmax=300 * 3600,
+...                              elapsed=elapsed,
+...                              L=L, deltaA=deltaA,
+...                              tmin=1e-5, tmax=300 * 3600,
 ...                              datamin=0., datamax=1.)
 ...     except ImportError:
 ...         viewer = Viewer(vars=(phase, C, sharp),
