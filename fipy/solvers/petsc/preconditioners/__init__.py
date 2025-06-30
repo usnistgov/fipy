7342620b4483beb2b239632b278175503187708b
Jonathan Guyer <guyer@nist.gov>
2025-06-30 17:59:00 -0400

    Add HYPRE preconditioner

--- fipy/solvers/petsc/preconditioners/__init__.py
+++ fipy/solvers/petsc/preconditioners/__init__.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from .defaultPreconditioner import *
 from .icPreconditioner import *
 from .iluPreconditioner import *
+from .hyprePreconditioner import *
 from .jacobiPreconditioner import *
 from .luPreconditioner import *
 from .ssorPreconditioner import *
@@ -10,6 +11,7 @@ __all__ = []
 __all__.extend(defaultPreconditioner.__all__)
 __all__.extend(icPreconditioner.__all__)
 __all__.extend(iluPreconditioner.__all__)
+__all__.extend(hyprePreconditioner.__all__)
 __all__.extend(jacobiPreconditioner.__all__)
 __all__.extend(luPreconditioner.__all__)
 __all__.extend(ssorPreconditioner.__all__)
