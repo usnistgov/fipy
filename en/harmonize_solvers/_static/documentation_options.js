29803c3cdbb1990c0a33fc512462b219dbb42951
GitHub Action <action@github.com>
2025-07-03 19:36:36 +0000

    Update documentation for harmonize_solvers@b584d66

--- en/harmonize_solvers/_static/documentation_options.js
+++ en/harmonize_solvers/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.4.5+358.g54b175539',
+    VERSION: '3.4.5+359.gb584d661b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
