854bd96ec4d88cb5b948d7e60d29556cfa0bc364
GitHub Action <action@github.com>
2025-09-04 22:08:59 +0000

    Update documentation for drop_py27@241beb5

--- en/drop_py27/_static/documentation_options.js
+++ en/drop_py27/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.99+32.g11ea72261',
+    VERSION: '3.99+33.g241beb54f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
