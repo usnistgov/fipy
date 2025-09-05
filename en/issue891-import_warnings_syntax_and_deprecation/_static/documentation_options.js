27d6294a31e32c4f0f428b031c0aab07e8f5ee09
GitHub Action <action@github.com>
2025-09-05 17:41:10 +0000

    Update documentation for issue891-import_warnings_syntax_and_deprecation@8ef330b

--- en/issue891-import_warnings_syntax_and_deprecation/_static/documentation_options.js
+++ en/issue891-import_warnings_syntax_and_deprecation/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.99+6.g8a2de8d02',
+    VERSION: '3.99+8.g8ef330bce',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
