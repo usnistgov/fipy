d810ffa4102f1d8110376d6ffb08a876d98e3358
GitHub Action <action@github.com>
2025-09-05 20:06:59 +0000

    Update documentation for fix_links@247d36c

--- en/fix_links/_static/documentation_options.js
+++ en/fix_links/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.99+6.g70bc481de',
+    VERSION: '3.99+7.g247d36ce6',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
