a677911ceff6a1000e63a34e39af957e32198ea7
GitHub Action <action@github.com>
2025-09-13 14:50:56 +0000

    Update documentation for Migrate_to_pyproject_toml@f31669d

--- en/Migrate_to_pyproject_toml/_static/documentation_options.js
+++ en/Migrate_to_pyproject_toml/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.99+26.gc11d13128',
+    VERSION: '3.99+27.gf31669d3e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
