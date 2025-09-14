15c0848d35112852d226a9cff439c35bfd15abb4
Jonathan Guyer <guyer@nist.gov>
2025-09-13 22:10:13 -0400

    Convert test command to a class

--- fipy/tests/test.py
+++ fipy/tests/test.py
@@ -1,4 +1,5 @@
 import contextlib
+import numpy
 import os
 from pathlib import Path
 import sys
@@ -8,50 +9,13 @@ from typing_extensions import Annotated
 import unittest
 import warnings
 
+from fipy.tests.doctestPlus import _DocTestTimes, report_skips
+from fipy.tools.logging.environment import package_info
+from fipy.tools import numerix
+
+
 __all__ = ["app"]
 
-## cribbed from obsolete setuptools.command.test
-
-@contextlib.contextmanager
-def project_on_sys_path(include_dists=[]):
-    old_path = sys.path[:]
-    old_modules = sys.modules.copy()
-
-    try:
-        project_path = Path(".").absolute().as_posix()
-        sys.path.insert(0, project_path)
-        with paths_on_pythonpath([project_path]):
-            yield
-    finally:
-        sys.path[:] = old_path
-        sys.modules.clear()
-        sys.modules.update(old_modules)
-
-@contextlib.contextmanager
-def paths_on_pythonpath(paths):
-    """
-    Add the indicated paths to the head of the :envvar:`PYTHONPATH`
-    environment variable so that subprocesses will also see the packages at
-    these paths.
-
-    Do this in a context that restores the value on exit.
-    """
-    nothing = object()
-    orig_pythonpath = os.environ.get('PYTHONPATH', nothing)
-    current_pythonpath = os.environ.get('PYTHONPATH', '')
-    try:
-        prefix = os.pathsep.join(paths)
-        to_join = filter(None, [prefix, current_pythonpath])
-        new_path = os.pathsep.join(to_join)
-        if new_path:
-            os.environ['PYTHONPATH'] = new_path
-
-        yield
-    finally:
-        if orig_pythonpath is nothing:
-            os.environ.pop('PYTHONPATH', None)
-        else:
-            os.environ['PYTHONPATH'] = orig_pythonpath
 
 class DeprecationErroringTestProgram(unittest.TestProgram):
     """`TestProgram` that overrides inability of standard
@@ -84,147 +48,303 @@ class DeprecationErroringTestProgram(unittest.TestProgram):
 
             super(DeprecationErroringTestProgram, self).runTests()
 
-def _test_args(verbose, module_or_suite, viewers, modules, examples):
-    # can't seem to delegate a generator until Python 3.3
 
-    if verbose:
-        yield '--verbose'
-    if module_or_suite:
-        yield module_or_suite
+class TestCommand(object):
+    def __init__(
+        self,
+        module_or_suite,
+        inline,
+        pythoncompiled,
+        trilinos,
+        scipy,
+        petsc,
+        pyamgx,
+        all,
+        really_all,
+        examples,
+        modules,
+        viewers,
+        cache,
+        timetests,
+        skfmm,
+        lsmlib,
+        deprecation_errors,
+        verbose
+    ):
+        self.module_or_suite = module_or_suite
+        self.inline = inline
+        self.pythoncompiled = pythoncompiled
+        self.trilinos = trilinos
+        self.scipy = scipy
+        self.petsc = petsc
+        self.pyamgx = pyamgx
+        self.all = all
+        self.really_all = really_all
+        self.examples = examples
+        self.modules = modules
+        self.viewers = viewers
+        self.cache = cache
+        self.timetests = timetests
+        self.skfmm = skfmm
+        self.lsmlib = lsmlib
+        self.deprecation_errors = deprecation_errors
+        self.verbose = verbose
 
-    if viewers:
-        print("*" * 60)
-        print("*" + "".center(58) + "*")
-        print("*" + "ATTENTION".center(58) + "*")
-        print("*" + "".center(58) + "*")
-        print("*" + "Some of the following tests require user interaction".center(58) + "*")
-        print("*" + "".center(58) + "*")
-        print("*" * 60)
+        self.initialize()
 
-        yield "fipy.viewers.testinteractive._suite"
-    if modules:
-        yield "fipy.testFiPy._suite"
-    if examples:
-        yield "examples.test._suite"
+        self.set_modules()
 
-def _printPackageInfo():
-    from fipy.tools.logging.environment import package_info
+        self.test_args = list(self._test_args())
 
-    packages = package_info()
 
-    print()
-    package_width = max(len(word) for word in packages.keys())
-    for package, version in packages.items():
-        print("  ".join((package.ljust(package_width), version)))
-    print()
+    def initialize(self):
+        if self.trilinos:
+            self.initialize_trilinos()
 
-def _initialize_trilinos():
-    try:
-        ## The import scipy statement is added to allow
-        ## the --Trilinos tests to run without throwing a
-        ## segmentation fault. This is caused by weird
-        ## behavior in scipy and PyTrilinos depending on
-        ## the order in which modules are imported
+        if self.pyamgx:
+            self.initialize_pyamgx()
+
+        if self.inline:
+            self.initialize_weave()
+
+        if self.pythoncompiled is not None:
+            pythoncompiled = self.pythoncompiled.absolute().as_posix()
+            os.environ['PYTHONCOMPILED'] = pythoncompiled
+
+
+    def set_modules(self):
+        if not (self.examples
+                or self.modules
+                or self.viewers
+                or self.module_or_suite):
+            self.all = True
+        if self.all or self.really_all:
+            self.examples = True
+            self.modules = True
+        if self.really_all:
+            self.viewers = True
+
+
+    @staticmethod
+    @contextlib.contextmanager
+    def project_on_sys_path():
+        """Add this project to the head of :envvar:`PYTHONPATH`
+
+        Do this in a context that restores the value on exit.
+
+        Cribbed from obsolete :mod:`setuptools.command.test`
+        """
+        old_path = sys.path[:]
+        old_modules = sys.modules.copy()
+
         try:
-            import scipy
+            project_path = Path(".").absolute().as_posix()
+            sys.path.insert(0, project_path)
+            with TestCommand.paths_on_pythonpath([project_path]):
+                yield
+        finally:
+            sys.path[:] = old_path
+            sys.modules.clear()
+            sys.modules.update(old_modules)
+
+
+    @staticmethod
+    @contextlib.contextmanager
+    def paths_on_pythonpath(paths):
+        """Add the indicated paths to the head of :envvar:`PYTHONPATH`
+
+        Subprocesses will also see the packages at these paths.
+
+        Do this in a context that restores the value on exit.
+
+        Cribbed from obsolete :mod:`setuptools.command.test`
+        """
+        nothing = object()
+        orig_pythonpath = os.environ.get('PYTHONPATH', nothing)
+        current_pythonpath = os.environ.get('PYTHONPATH', '')
+        try:
+            prefix = os.pathsep.join(paths)
+            to_join = filter(None, [prefix, current_pythonpath])
+            new_path = os.pathsep.join(to_join)
+            if new_path:
+                os.environ['PYTHONPATH'] = new_path
+
+            yield
+        finally:
+            if orig_pythonpath is nothing:
+                os.environ.pop('PYTHONPATH', None)
+            else:
+                os.environ['PYTHONPATH'] = orig_pythonpath
+
+
+    def _test_args(self):
+        # can't seem to delegate a generator until Python 3.3
+
+        if self.verbose:
+            yield '--verbose'
+        if self.module_or_suite:
+            yield self.module_or_suite
+
+        if self.viewers:
+            print("*" * 60)
+            print("*" + "".center(58) + "*")
+            print("*" + "ATTENTION".center(58) + "*")
+            print("*" + "".center(58) + "*")
+            print("*" + "Some of the following tests require user interaction".center(58) + "*")
+            print("*" + "".center(58) + "*")
+            print("*" * 60)
+
+            yield "fipy.viewers.testinteractive._suite"
+        if self.modules:
+            yield "fipy.testFiPy._suite"
+        if self.examples:
+            yield "examples.test._suite"
+
+
+    @staticmethod
+    def printPackageInfo():
+        packages = package_info()
+
+        print()
+        package_width = max(len(word) for word in packages.keys())
+        for package, version in packages.items():
+            print("  ".join((package.ljust(package_width), version)))
+        print()
+
+
+    @staticmethod
+    def initialize_trilinos():
+        try:
+            ## The import scipy statement is added to allow
+            ## the --Trilinos tests to run without throwing a
+            ## segmentation fault. This is caused by weird
+            ## behavior in scipy and PyTrilinos depending on
+            ## the order in which modules are imported
+            try:
+                import scipy
+            except:
+                pass
+            import PyTrilinos
+        except ImportError as a:
+            print("!!! Trilinos library is not installed", file=sys.stderr)
+            return
+
+
+    @staticmethod
+    def initialize_pyamgx():
+        try:
+            ## Unregister the function pyamgx.finalize
+            ## from atexit. This prevents
+            ## pyamgx from printing an error message
+            ## about memory leaks and a dump of leaked memory.
+            ## The memory leaks happen because
+            ## the tests do not use the pyamgx solvers
+            ## "cleanly", i.e., they do not use the
+            ## `with` statement.
+            import pyamgx
+            import atexit
+            if hasattr(atexit, 'unregister'):
+                atexit.unregister(pyamgx.finalize)
+            else:
+                atexit._exithandlers.remove(
+                    (pyamgx.finalize, (), {}))
+        except ImportError as e:
+            print("!!! pyamgx package is not installed", file=sys.stederr)
+            return
+
+
+    @staticmethod
+    def initialize_weave():
+        try:
+            import weave
+        except ImportError as a:
+            print("!!! weave library is not installed", file=sys.stderr)
+            return
+
+
+    @staticmethod
+    @contextlib.contextmanager
+    def legacy_printoptions():
+        printoptions = numerix.get_printoptions()
+
+        try:
+            if "legacy" in printoptions:
+                numerix.set_printoptions(legacy="1.13")
+
+            yield
+        finally:
+            if "legacy" in printoptions:
+                numerix.set_printoptions(legacy=printoptions["legacy"])
+
+
+    def save_testtimes(self):
+        if self.timetests is None:
+            return
+
+        _DocTestTimes = numpy.rec.fromrecords(_DocTestTimes,
+                                formats='f8,S255',
+                                names='time,test')
+        _DocTestTimes.sort(order=('time', 'test'))
+
+        # Only write on proc 0.
+        # Doesn't use FiPy comms because this command can
+        # be run outside of FiPy (`fipy_test`)
+        try:
+            from mpi4py import MPI
+            procID = MPI.COMM_WORLD.rank
+            barrier = MPI.COMM_WORLD.barrier
         except:
-            pass
-        import PyTrilinos
-    except ImportError as a:
-        print("!!! Trilinos library is not installed", file=sys.stderr)
-        return
+            procID = 0
+            def barrier(*args):
+                pass
 
-def _initialize_pyamgx():
-    try:
-        ## Unregister the function pyamgx.finalize
-        ## from atexit. This prevents
-        ## pyamgx from printing an error message
-        ## about memory leaks and a dump of leaked memory.
-        ## The memory leaks happen because
-        ## the tests do not use the pyamgx solvers
-        ## "cleanly", i.e., they do not use the
-        ## `with` statement.
-        import pyamgx
-        import atexit
-        if hasattr(atexit, 'unregister'):
-            atexit.unregister(pyamgx.finalize)
+        if procID == 0:
+            numpy.savetxt(self.timetests,
+                          _DocTestTimes[::-1],
+                          delimiter='\t',
+                          header="time\tmodule",
+                          comments='',
+                          fmt=("%.18e", "%s"))
+
+        barrier()
+
+
+    @property
+    def test_program_class(self):
+        if self.deprecation_errors:
+            test_program_class = DeprecationErroringTestProgram
         else:
-            atexit._exithandlers.remove(
-                (pyamgx.finalize, (), {}))
-    except ImportError as e:
-        print("!!! pyamgx package is not installed", file=sys.stederr)
-        return
+            test_program_class = unittest.TestProgram
 
-def _initialize_weave():
-    try:
-        import weave
-    except ImportError as a:
-        print("!!! weave library is not installed", file=sys.stderr)
-        return
+        return test_program_class
 
-@contextlib.contextmanager
-def legacy_printoptions():
-    from fipy.tools import numerix
-    printoptions = numerix.get_printoptions()
 
-    try:
-        if "legacy" in printoptions:
-            numerix.set_printoptions(legacy="1.13")
+    def run_tests(self):
+        self.printPackageInfo()
 
-        yield
-    finally:
-        if "legacy" in printoptions:
-            numerix.set_printoptions(legacy=printoptions["legacy"])
-
-def save_testtimes(timetests):
-    from fipy.tests.doctestPlus import _DocTestTimes
-    import numpy
-    _DocTestTimes = numpy.rec.fromrecords(_DocTestTimes, formats='f8,S255', names='time,test')
-    _DocTestTimes.sort(order=('time', 'test'))
-
-    # Only write on proc 0.
-    # Doesn't use FiPy comms because this command can
-    # be run outside of FiPy (`fipy_test`)
-    try:
-        from mpi4py import MPI
-        procID = MPI.COMM_WORLD.rank
-        barrier = MPI.COMM_WORLD.barrier
-    except:
-        procID = 0
-        def barrier(*args):
-            pass
-
-    if procID == 0:
-        numpy.savetxt(timetests, _DocTestTimes[::-1],
-                      delimiter='\t',
-                      header="time\tmodule", comments='',
-                      fmt=("%.18e", "%s"))
-
-    barrier()
-
-def run_tests(test_program_class, test_args, timetests=None):
-    _printPackageInfo()
-
-    with legacy_printoptions():
         try:
-            test_program_class(
-                None, None, [unittest.__file__]+test_args,
-                testLoader = unittest.TestLoader()
-                )
+            # Run tests with current working directory on path
+            # so that examples can be found
+            with self.project_on_sys_path():
+                with self.legacy_printoptions():
+                    self.test_program_class(
+                        None,
+                        None,
+                        [unittest.__file__]+self.test_args,
+                        testLoader=unittest.TestLoader()
+                    )
         except SystemExit as exitErr:
             # unittest.main(..., exit=...) not available until Python 2.7
-            from fipy.tests.doctestPlus import report_skips
             report_skips()
-            if timetests is not None:
+            if self.timetests is not None:
                 pass
             else:
                 raise
 
-    if timetests is not None:
-        save_testtimes(timetests)
+        self.save_testtimes()
 
-    if 'exitErr' in locals():
-        raise exitErr
+        if 'exitErr' in locals():
+            raise exitErr
 
 def main(
     module_or_suite: Annotated[
@@ -279,10 +399,6 @@ def main(
         bool,
         Option(help="run FiPy with Variable caching")
     ] = False,
-    no_cache: Annotated[
-        bool,
-        Option(help="run FiPy without Variable caching")
-    ] = True,
     timetests: Annotated[
         Path,
         Option(help="file in which to put time spent on each test")
@@ -304,39 +420,29 @@ def main(
         Option(help="blah blah verbose")
     ] = True
 ):
-    if trilinos:
-        _initialize_trilinos()
+    test = TestCommand(
+        module_or_suite,
+        inline,
+        pythoncompiled,
+        trilinos,
+        scipy,
+        petsc,
+        pyamgx,
+        all,
+        really_all,
+        examples,
+        modules,
+        viewers,
+        cache,
+        timetests,
+        skfmm,
+        lsmlib,
+        deprecation_errors,
+        verbose
+    )
 
-    if pyamgx:
-        _initialize_pyamgx()
+    test.run_tests()
 
-    if inline:
-        _initialize_weave()
-
-    if pythoncompiled is not None:
-        os.environ['PYTHONCOMPILED'] = pythoncompiled.absolute().as_posix()
-
-    if not (examples or modules or viewers or module_or_suite):
-        all = True
-    if all or really_all:
-        examples = True
-        modules = True
-    if really_all:
-        viewers = True
-
-    test_args = list(_test_args(verbose, module_or_suite, viewers, modules, examples))
-
-    if deprecation_errors:
-        test_program_class = DeprecationErroringTestProgram
-    else:
-        test_program_class = unittest.TestProgram
-
-    # Run tests with current working directory on path
-    # so that examples can be found
-    with project_on_sys_path():
-        run_tests(test_program_class=test_program_class,
-                  test_args=test_args,
-                  timetests=timetests)
 
 app = typer.Typer()
 app.command()(main)
