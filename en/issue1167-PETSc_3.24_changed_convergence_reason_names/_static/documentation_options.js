fb7c954d22cf5e46628f9e15781f9e86eff23e21
GitHub Action <action@github.com>
2025-11-19 17:05:20 +0000

    Update documentation for issue1167-PETSc_3.24_changed_convergence_reason_names@378b076

--- en/issue1167-PETSc_3.24_changed_convergence_reason_names/_static/documentation_options.js
+++ en/issue1167-PETSc_3.24_changed_convergence_reason_names/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.0.1+3.g9483afd13',
+    VERSION: '4.0.1+4.g378b076be',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
