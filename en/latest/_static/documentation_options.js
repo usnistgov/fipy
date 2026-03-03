66b64df4076c84703898e0f46d002b925d411a5b
GitHub Action <action@github.com>
2026-03-03 12:55:44 +0000

    Update documentation for master@11e5c6a

--- en/latest/_static/documentation_options.js
+++ en/latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.0.2+1.gff73147b1',
+    VERSION: '4.0.2+2.g11e5c6ae2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
