b0a35a03236a5703db25afab6fbcbc311cddf6be
GitHub Action <action@github.com>
2026-03-27 22:29:11 +0000

    Update documentation for issue1190-Grid3D_with_array_as_dx_not_working_anymore@5ea22a4

--- en/issue1190-Grid3D_with_array_as_dx_not_working_anymore/_static/documentation_options.js
+++ en/issue1190-Grid3D_with_array_as_dx_not_working_anymore/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.0.2+9.g0e32245d7',
+    VERSION: '4.0.2+10.g5ea22a47a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
