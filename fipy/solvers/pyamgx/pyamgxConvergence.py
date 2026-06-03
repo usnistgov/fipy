48ef29f94db2c1df84727c1504f2d8ea732f1717
bandrews <b9andrews@gmail.com>
2026-06-03 11:12:36 -0400

    add pyamgx not_converged to ConvergenceBased code registry

--- fipy/solvers/pyamgx/pyamgxConvergence.py
+++ fipy/solvers/pyamgx/pyamgxConvergence.py
@@ -1,23 +1,38 @@
-from ..convergence import (Convergence, IterationDivergence,
-                           BreakdownDivergence)
+from ..convergence import (
+    BreakdownDivergence,
+    Convergence,
+    Divergence,
+    IterationDivergence,
+)
+
 
 class pyamgx_Convergence(Convergence):
-    """
-    """
+    """ """
+
     status_code = "success"
     status_name = "AMGX_SOLVE_SUCCESS"
     suite = "pyamgx"
 
+
 class pyamgx_BreakdownDivergence(BreakdownDivergence):
-    """
-    """
+    """ """
+
     status_code = "failed"
     status_name = "AMGX_SOLVE_FAILED"
     suite = "pyamgx"
 
+
 class pyamgx_IterationDivergence(IterationDivergence):
-    """
-    """
+    """ """
+
     status_code = "diverged"
     status_name = "AMGX_SOLVE_DIVERGED"
+    suite = "pyamgx"
+
+
+class pyamgx_Divergence(Divergence):
+    """ """
+
+    status_code = "not_converged"
+    status_name = "AMGX_SOLVE_NOT_CONVERGED"
     suite = "pyamgx"
