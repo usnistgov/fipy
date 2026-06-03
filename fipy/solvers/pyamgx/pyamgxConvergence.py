d6abbed33043079390a2ec432623daab9c298d1e
bandrews <b9andrews@gmail.com>
2026-06-03 11:19:50 -0400

    remove auto formatted changes

--- fipy/solvers/pyamgx/pyamgxConvergence.py
+++ fipy/solvers/pyamgx/pyamgxConvergence.py
@@ -7,32 +7,29 @@ from ..convergence import (
 
 
 class pyamgx_Convergence(Convergence):
-    """ """
-
+    """
+    """
     status_code = "success"
     status_name = "AMGX_SOLVE_SUCCESS"
     suite = "pyamgx"
 
-
 class pyamgx_BreakdownDivergence(BreakdownDivergence):
-    """ """
-
+    """
+    """
     status_code = "failed"
     status_name = "AMGX_SOLVE_FAILED"
     suite = "pyamgx"
 
-
 class pyamgx_IterationDivergence(IterationDivergence):
-    """ """
-
+    """
+    """
     status_code = "diverged"
     status_name = "AMGX_SOLVE_DIVERGED"
     suite = "pyamgx"
 
-
 class pyamgx_Divergence(Divergence):
-    """ """
-
+    """
+    """
     status_code = "not_converged"
     status_name = "AMGX_SOLVE_NOT_CONVERGED"
     suite = "pyamgx"
